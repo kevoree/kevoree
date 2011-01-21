6a0ed97a3d0eca981b795c2b4c889ed2a528a03e
Fouquet François <fouquet.f@gmail.com>
2011-01-21 14:17:05 +0100

    ART2 Merge

---
+++ org.kevoree.kompare/src/main/scala/org/kevoree/kompare/sub/InitNodeKompare.scala
@@ -0,0 +1,93 @@
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.kevoree.kompare.sub
+
+import org.kevoree._
+import org.kevoreeAdaptation.AddDeployUnit
+import org.kevoreeAdaptation._
+import org.slf4j.LoggerFactory
+import scala.collection.JavaConversions._
+import org.kevoree.framework.aspects.Art2Aspects._
+
+trait InitNodeKompare extends AbstractKompare {
+
+  def getInitNodeAdaptationModel(node:ContainerNode):AdaptationModel= {
+    var adaptationModel = org.kevoreeAdaptation.KevoreeAdaptationFactory.eINSTANCE.createAdaptationModel
+    logger.info("INIT NODE v2 "+node.getName)
+    //UPDATE ALL COMPONENT TYPE
+
+    var root = node.eContainer.asInstanceOf[ContainerRoot]
+    
+    /* add type */
+    node.getUsedTypeDefinition.foreach{ct=>
+      var typecmd = KevoreeAdaptationFactory.eINSTANCE.createAddType
+      typecmd.setRef(ct)
+      adaptationModel.getAdaptations.add(typecmd)
+
+      /* add all reLib */
+      ct.getRequiredLibs.foreach{rLib =>
+        var addcttp = KevoreeAdaptationFactory.eINSTANCE.createAddThirdParty
+        addcttp.setRef(rLib)
+        adaptationModel.getAdaptations.add(addcttp)
+      }
+
+      /* add deploy unit if necessary */
+      adaptationModel.getAdaptations.filter(adaptation => adaptation.isInstanceOf[AddDeployUnit]).find(adaptation=> adaptation.asInstanceOf[AddDeployUnit].getRef.isModelEquals(ct.getDeployUnit) ) match {
+        case None => {
+            var ctcmd = KevoreeAdaptationFactory.eINSTANCE.createAddDeployUnit
+            ctcmd.setRef(ct.getDeployUnit)
+            adaptationModel.getAdaptations.add(ctcmd)
+          }
+        case Some(e)=> //SIMILAR DEPLOY UNIT PRIMITIVE ALREADY REGISTERED
+      }
+
+
+
+    }
+    
+    /* add component */
+    node.getInstances.foreach({c =>
+        var addccmd = KevoreeAdaptationFactory.eINSTANCE.createAddInstance
+        addccmd.setRef(c)
+        adaptationModel.getAdaptations.add(addccmd)
+      })
+    
+
+    /* add FRAGMENT binding */
+    root.getHubs.foreach{channel =>
+      channel.getOtherFragment(node.getName).foreach{remoteName =>
+        var addccmd = KevoreeAdaptationFactory.eINSTANCE.createAddFragmentBinding
+        addccmd.setRef(channel)
+        addccmd.setTargetNodeName(remoteName)
+        adaptationModel.getAdaptations.add(addccmd)
+      }
+    }
+    
+    /* add mbinding */
+    root.getMBindings.foreach{b=>
+      if(b.getPort.eContainer.eContainer == node){
+        var addcmd = KevoreeAdaptationFactory.eINSTANCE.createAddBinding
+        addcmd.setRef(b)
+        adaptationModel.getAdaptations.add(addcmd)
+      }
+    }
+
+    adaptationModel
+  }
+}
---
+++ org.kevoree.kompare/src/test/scala/org/kevoree/kompare/tests/type/TypeInstallAndRemoveTest.scala
@@ -0,0 +1,90 @@
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.kevoree.kompare.tests.`type`
+
+import org.kevoree.api.service.core.kompare.ModelKompareService
+import org.kevoree.kompare.Art2KompareBean
+import org.kevoree.kompare.tests.KompareSuite
+import org.junit._
+import org.kevoreeAdaptation._
+
+
+class TypeInstallAndRemoveTest extends KompareSuite {
+
+  var component : ModelKompareService = null
+
+  @Before def initialize() {
+    component = new Art2KompareBean
+  }
+
+  @Test def testNoTypeInstall()={
+    var kompareModel = component.kompare(emptyModel, model("test_type/noTypeInstall.art2"), "duke")
+    kompareModel verifySize 0
+  }
+
+  @Test def testOnlyOneDeployUnitInstall_INITNODE()={
+    var kompareModel = component.kompare(emptyModel, model("test_type/onlyOneDeployUnitInstall.art2"), "duke")
+    kompareModel shouldContainSize(classOf[AddDeployUnit],1)
+  }
+
+  @Test def testOnlyOneDeployUnitInstall_UPDATENODE()={
+    var kompareModel = component.kompare(model("test_type/noTypeInstall.art2"), model("test_type/onlyOneDeployUnitInstall.art2"), "duke")
+    kompareModel shouldContainSize(classOf[AddDeployUnit],1)
+    kompareModel shouldContain(classOf[AddType],"ComponentPrimitiveTypeService")
+    kompareModel shouldContain(classOf[AddType],"ComponentB")
+    kompareModel shouldContain(classOf[AddInstance],"ComponentB--10313997")
+    kompareModel shouldContain(classOf[AddInstance],"ComponentPrimitiveTypeService--791402174")
+  }
+
+  @Test def testnoTypeInstall_UPDATENODE()={
+    var kompareModel = component.kompare(model("test_type/onlyOneDeployUnitInstall.art2"), model("test_type/onlyOneDeployUnitInstall.art2"), "duke")
+    kompareModel verifySize 0
+  }
+
+  @Test def testnoTypeDeployUnitUninstall_UPDATENODE()={
+    var kompareModel = component.kompare(model("test_type/onlyOneDeployUnitInstall.art2"), model("test_type/noTypeDeployUnitUninstall.art2"), "duke")
+    kompareModel shouldContainSize(classOf[RemoveDeployUnit],0)
+  }
+
+
+  @Test def testOnlyOneDeployUnitUninstall_UPDATENODE()={
+    var kompareModel = component.kompare(model("test_type/onlyOneDeployUnitInstall.art2"), model("test_type/onlyOneDeployUnitUninstall.art2"), "duke")
+    kompareModel shouldContainSize(classOf[RemoveDeployUnit],1)
+    kompareModel shouldContain(classOf[RemoveType],"ComponentPrimitiveTypeService")
+    kompareModel shouldContain(classOf[RemoveType],"ComponentB")
+    kompareModel shouldContain(classOf[RemoveInstance],"ComponentB--10313997")
+    kompareModel shouldContain(classOf[RemoveInstance],"ComponentPrimitiveTypeService--791402174")
+  }
+
+  @Test def testOnlyOneDeployUnitUninstall_STOPNODE()={
+    var kompareModel = component.kompare(model("test_type/onlyOneDeployUnitInstall.art2"),emptyModel, "duke")
+    kompareModel shouldContainSize(classOf[RemoveDeployUnit],1)
+  }
+
+  @Test def testuninstall_STOPNODE()={
+    var kompareModel = component.kompare(model("test_type/onlyOneDeployUnitInstall.art2"),emptyModel, "duke")
+    kompareModel shouldContainSize(classOf[RemoveDeployUnit],1)
+    kompareModel shouldContain(classOf[RemoveType],"ComponentPrimitiveTypeService")
+    kompareModel shouldContain(classOf[RemoveType],"ComponentB")
+    kompareModel shouldContain(classOf[RemoveInstance],"ComponentB--10313997")
+    kompareModel shouldContain(classOf[RemoveInstance],"ComponentPrimitiveTypeService--791402174")
+  }
+
+}
+
