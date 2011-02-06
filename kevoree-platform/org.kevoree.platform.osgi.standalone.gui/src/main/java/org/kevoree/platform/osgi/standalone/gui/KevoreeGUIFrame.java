e614d793d9b24bd5501b71e5c487f018026639a0
Fouquet François <fouquet.f@gmail.com>
2011-02-06 19:06:57 +0100

    Add Icon to JDialog frame
    Former-commit-id: bd6ee8c9354c96dfcb96318fb5bc089aecd6ce7d

--- kevoree-platform/org.kevoree.platform.osgi.standalone.gui/src/main/java/org/kevoree/platform/osgi/standalone/gui/KevoreeGUIFrame.java
+++ kevoree-platform/org.kevoree.platform.osgi.standalone.gui/src/main/java/org/kevoree/platform/osgi/standalone/gui/KevoreeGUIFrame.java
@@ -26,21 +26,24 @@ public class KevoreeGUIFrame extends JFrame {
         this.setBackground(new Color(63, 128, 187));
 
         URL urlIcon = getClass().getClassLoader().getResource("kevoree-logo-full.png");
+        URL urlSmallIcon = getClass().getClassLoader().getResource("kev-logo-full.png");
         ImageIcon topIIcon = new ImageIcon(urlIcon);
+        ImageIcon smallIcon = new ImageIcon(urlSmallIcon);
+        this.setIconImage(smallIcon.getImage());
         JLabel topImage = new JLabel(topIIcon);
         this.add(topImage, BorderLayout.NORTH);
 
-        String response = JOptionPane.showInputDialog(this,
+        String response = (String) JOptionPane.showInputDialog(this,
                 "Kevoree node name ?",
                 "Kevoree node runtime",
-                JOptionPane.QUESTION_MESSAGE);
+                JOptionPane.QUESTION_MESSAGE, smallIcon, null, "");
 
         if (response == null) {
             System.exit(0);
         }
 
         System.setProperty("node.name",response);
-
+        this.setTitle(response +" : KevoreeNode");
         singleton.setVisible(true);
 
 
