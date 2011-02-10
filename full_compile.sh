45a11160be54b0f24291a3be0f97de4fe3dc00e6
Fouquet François <fouquet.f@gmail.com>
2011-02-10 18:11:44 +0100

    cleanup

--- full_compile.sh
+++ full_compile.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 export MAVEN_OPTS="-Xms512m -Xmx1024m -XX:PermSize=256m -XX:MaxPermSize=512m"
-mvn clean install deploy
+BASE_RELEASE_DIR=`pwd`
+cd kevoree-extra/org.kevoree.extra.root
+mvn clean install deploy -Dkevoree.version=1.0.0-RC1
+cd $BASE_RELEASE_DIR
+echo "current dir "
+echo `pwd`
+mvn clean install deploy -Dkevoree.version=1.0.0-RC1
