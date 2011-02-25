5e23741a84dc8934bbff1b63d39785aa9460f9cc
edaubert <Erwan.Daubert@inria.fr>
2011-02-25 10:19:11 +0100

    fix module name of baseChecker in root pom.xml fix node and component cycle checker

--- kevoree-core/org.kevoree.deploy.osgi/src/main/scala/org/kevoree/adaptation/deploy/osgi/scheduling/SchedulingWithTopologicalOrderAlgo.scala
+++ kevoree-core/org.kevoree.deploy.osgi/src/main/scala/org/kevoree/adaptation/deploy/osgi/scheduling/SchedulingWithTopologicalOrderAlgo.scala
@@ -28,123 +28,123 @@ import org.kevoree.adaptation.deploy.osgi.command.LifeCycleCommand
 
 class SchedulingWithTopologicalOrderAlgo {
 
-  def schedule(commands: List[LifeCycleCommand], start: Boolean): List[LifeCycleCommand] = {
-    if (commands.size > 1) {
-      var graph : DefaultDirectedGraph[LifeCycleCommand,(LifeCycleCommand,LifeCycleCommand)] = buildGraph(commands, start)
-      var topologicAlgorithm : TopologicalOrderIterator[LifeCycleCommand,(LifeCycleCommand,LifeCycleCommand)] = new TopologicalOrderIterator(graph)
+	def schedule(commands: List[LifeCycleCommand], start: Boolean): List[LifeCycleCommand] = {
+		if (commands.size > 1) {
+			var graph: DefaultDirectedGraph[LifeCycleCommand, (LifeCycleCommand, LifeCycleCommand)] = buildGraph(commands, start)
+			var topologicAlgorithm: TopologicalOrderIterator[LifeCycleCommand, (LifeCycleCommand, LifeCycleCommand)] = new TopologicalOrderIterator(graph)
 
-      var listCommands = List[LifeCycleCommand]()
-      while (topologicAlgorithm.hasNext) {
-	listCommands = listCommands ++ List(topologicAlgorithm.next)
-      }
-      if (listCommands.size == commands.size) {
-	return listCommands
-      }
-    } 
-    commands
-  }
+			var listCommands = List[LifeCycleCommand]()
+			while (topologicAlgorithm.hasNext) {
+				listCommands = listCommands ++ List(topologicAlgorithm.next)
+			}
+			if (listCommands.size == commands.size) {
+				return listCommands
+			}
+		}
+		commands
+	}
 
-  /**
-   * each command is a vertex and edges represent dependency order between commands
-   */
-  private def buildGraph(commands: List[LifeCycleCommand], start: Boolean) : DefaultDirectedGraph[LifeCycleCommand,(LifeCycleCommand,LifeCycleCommand)] ={
-    var graph = new DefaultDirectedGraph[LifeCycleCommand,(LifeCycleCommand,LifeCycleCommand)](classOf[(LifeCycleCommand,LifeCycleCommand)])
+	/**
+	 * each command is a vertex and edges represent dependency order between commands
+	 */
+	private def buildGraph(commands: List[LifeCycleCommand], start: Boolean): DefaultDirectedGraph[LifeCycleCommand, (LifeCycleCommand, LifeCycleCommand)] = {
+		var graph = new DefaultDirectedGraph[LifeCycleCommand, (LifeCycleCommand, LifeCycleCommand)](classOf[(LifeCycleCommand, LifeCycleCommand)])
 
-    var map = lookForPotentialConstraints(commands)
+		var map = lookForPotentialConstraints(commands)
 
-    for (command <- commands) {
-      for (command2 <- commands) {
-	graph.addVertex(command2)
-	if (!command.equals(command2)) {
-	  (map.get(command2.getInstance)) match {
-	    case Some(cmdDep) => {
-		if (cmdDep.contains(command.getInstance)){
-		  if (start) {
-		    graph.addEdge(command2, command, (command2, command))
-		    //constraint1 = Choco.lt(variables.get(command2).get, variables.get(command).get)
-		    //println(command2.getInstance.getName+"<"+command.getInstance.getName)
-		  }
-		  else {
-		    graph.addEdge(command, command2, (command, command2))
-		    //constraint1 = Choco.lt(variables.get(command).get, variables.get(command2).get)
-		  }
+		for (command <- commands) {
+			for (command2 <- commands) {
+				graph.addVertex(command2)
+				if (!command.equals(command2)) {
+					(map.get(command2.getInstance)) match {
+						case Some(cmdDep) => {
+							if (cmdDep.contains(command.getInstance)) {
+								if (start) {
+									graph.addEdge(command2, command, (command2, command))
+									//constraint1 = Choco.lt(variables.get(command2).get, variables.get(command).get)
+									//println(command2.getInstance.getName+"<"+command.getInstance.getName)
+								}
+								else {
+									graph.addEdge(command, command2, (command, command2))
+									//constraint1 = Choco.lt(variables.get(command).get, variables.get(command2).get)
+								}
 
-		}/* else {
+							} /* else {
 		  //println(command.getInstance.getName + "!="+command2.getInstance.getName)
 		  constraint1 = Choco.neq(variables.get(command).get, variables.get(command2).get)
 		  }*/
-		//model.addConstraint(constraint1)
-	      }
-	    case _ => {
-		//println(command.getInstance.getName + "!="+command2.getInstance.getName)
-		/*var constraint1: Constraint = Choco.neq(variables.get(command).get, variables.get(command2).get)
-		 model.addConstraint(constraint1)*/
-	      }
-	  }
+							//model.addConstraint(constraint1)
+						}
+						case _ => {
+							//println(command.getInstance.getName + "!="+command2.getInstance.getName)
+							/*var constraint1: Constraint = Choco.neq(variables.get(command).get, variables.get(command2).get)
+																 model.addConstraint(constraint1)*/
+						}
+					}
+				}
+
+			}
+		}
+		graph
 	}
 
-      }
-    }
-    graph
-  }
+	/*
+			 * Return Map
+			 *
+			 * Instance Map key is a dependency of all List instances return by key
+			 *
+			 *
+			 * [i:Instance,li:List[Instance]]
+			 * li depends i
+			 *
+			 *
+			 * */
+	private def lookForPotentialConstraints(commands: List[LifeCycleCommand]): scala.collection.mutable.Map[Instance, java.util.List[Instance]] = {
+		val instanceDependencies: scala.collection.mutable.Map[Instance, java.util.List[Instance]] = scala.collection.mutable.Map[Instance, java.util.List[Instance]]()
 
-  /*
-   * Return Map
-   *
-   * Instance Map key is a dependency of all List instances return by key
-   *
-   *
-   * [i:Instance,li:List[Instance]]
-   * li depends i
-   *
-   *
-   * */
-  private def lookForPotentialConstraints(commands: List[LifeCycleCommand]): scala.collection.mutable.Map[Instance, java.util.List[Instance]] = {
-    val instanceDependencies: scala.collection.mutable.Map[Instance, java.util.List[Instance]] = scala.collection.mutable.Map[Instance, java.util.List[Instance]]()
-
-    var rootContainer : ContainerRoot = null
-    var firstCommand= (commands(0)).getInstance
-    firstCommand match {
-      case c : Channel => rootContainer = c.eContainer.asInstanceOf[ContainerRoot]
-      case c : ComponentInstance => rootContainer = c.eContainer.eContainer.asInstanceOf[ContainerRoot]
-    }
-
-
-    if (rootContainer.getMBindings != null) {
-    var bindingIterator = rootContainer.getMBindings.iterator
-    while(bindingIterator.hasNext){
-      var binding = bindingIterator.next
-      for (command <- commands) {
-	command.getInstance match {
-	  case instance: ComponentInstance => {
-	      // test all provided port
-	      // the instance of the provided port must be stopped before those which are connected to him
-	      var pit = instance.getProvided.iterator
-	      while(pit.hasNext){
-		var port = pit.next
-		if (binding.getPort.equals(port)) {
-		  var newL = instanceDependencies.get(instance).getOrElse(new java.util.ArrayList())
-		  newL.add(binding.getHub)
-		  instanceDependencies.update(instance, newL)
+		var rootContainer: ContainerRoot = null
+		var firstCommand = (commands(0)).getInstance
+		firstCommand match {
+			case c: Channel => rootContainer = c.eContainer.asInstanceOf[ContainerRoot]
+			case c: ComponentInstance => rootContainer = c.eContainer.eContainer.asInstanceOf[ContainerRoot]
 		}
-	      }
-	      // test all required port
-	      // the instance wait stops of all of those which are connected to him
-	      var rit = instance.getRequired.iterator
-	      while(rit.hasNext){
-		var port = rit.next
-		if (binding.getPort.equals(port)) {
-		  var newL = instanceDependencies.get(binding.getHub).getOrElse(new java.util.ArrayList())
-		  newL.add(instance)
-		  instanceDependencies.update(binding.getHub, newL)
+
+
+		if (rootContainer.getMBindings != null) {
+			var bindingIterator = rootContainer.getMBindings.iterator
+			while (bindingIterator.hasNext) {
+				var binding = bindingIterator.next
+				for (command <- commands) {
+					command.getInstance match {
+						case instance: ComponentInstance => {
+							// test all provided port
+							// the instance of the provided port must be stopped before those which are connected to him
+							var pit = instance.getProvided.iterator
+							while (pit.hasNext) {
+								var port = pit.next
+								if (binding.getPort.equals(port)) {
+									var newL = instanceDependencies.get(instance).getOrElse(new java.util.ArrayList())
+									newL.add(binding.getHub)
+									instanceDependencies.update(instance, newL)
+								}
+							}
+							// test all required port
+							// the instance wait stops of all of those which are connected to him
+							var rit = instance.getRequired.iterator
+							while (rit.hasNext) {
+								var port = rit.next
+								if (binding.getPort.equals(port)) {
+									var newL = instanceDependencies.get(binding.getHub).getOrElse(new java.util.ArrayList())
+									newL.add(instance)
+									instanceDependencies.update(binding.getHub, newL)
+								}
+							}
+						}
+						case _ =>
+					}
+				}
+			}
 		}
-	      }
-	    }
-	  case _ =>
+		instanceDependencies
 	}
-      }
-    }
-  }
-  instanceDependencies
-}
 }
