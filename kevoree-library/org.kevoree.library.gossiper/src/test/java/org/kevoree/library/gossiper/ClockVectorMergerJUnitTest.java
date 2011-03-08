82f33b19c1fef412f57216a67d560696c49c0086
Fouquet François <fouquet.f@gmail.com>
2011-03-08 17:50:19 +0100

    ClockVector

--- kevoree-library/org.kevoree.library.gossiper/src/test/java/org/kevoree/library/gossiper/ClockVectorMergerJUnitTest.java
+++ kevoree-library/org.kevoree.library.gossiper/src/test/java/org/kevoree/library/gossiper/ClockVectorMergerJUnitTest.java
@@ -2,23 +2,48 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.kevoree.library.gossiper.version;
+package org.kevoree.library.gossiper;
 
 import org.kevoree.library.gossiper.version.GossiperMessages.ClockEntry;
+import org.kevoree.library.gossiper.version.VersionUtils;
 import org.kevoree.library.gossiper.version.GossiperMessages.VectorClock;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  *
  * @author ffouquet
  */
-public class NewTester {
+public class ClockVectorMergerJUnitTest {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public ClockVectorMergerJUnitTest() {
+    }
 
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void checkMerger() {
         VectorClock v1 = org.kevoree.library.gossiper.version.GossiperMessages.VectorClock.newBuilder().
                 setTimestamp(System.currentTimeMillis()).
                 addEnties(org.kevoree.library.gossiper.version.GossiperMessages.ClockEntry.newBuilder().setNodeID("A").setVersion(2).setTimestamp(System.currentTimeMillis())).
@@ -38,10 +63,23 @@ public class NewTester {
 
         VectorClock v3 = VersionUtils.merge(v1, v2);
 
-       for(ClockEntry ce : v3.getEntiesList()){
-           System.out.println(ce.getNodeID()+":"+ce.getVersion()+"-"+ce.getTimestamp());
-       }
+        assert (v3.getEnties(0).getNodeID().equals("A"));
+        assert (v3.getEnties(0).getVersion() == 2);
 
+        assert (v3.getEnties(1).getNodeID().equals("E"));
+        assert (v3.getEnties(1).getVersion() == 4);
+
+        assert (v3.getEnties(2).getNodeID().equals("B"));
+        assert (v3.getEnties(2).getVersion() == 4);
+
+        assert (v3.getEnties(3).getNodeID().equals("C"));
+        assert (v3.getEnties(3).getVersion() == 5);
+
+        assert (v3.getEnties(4).getNodeID().equals("D"));
+        assert (v3.getEnties(4).getVersion() == 5);
+
+        assert (v3.getEnties(5).getNodeID().equals("F"));
+        assert (v3.getEnties(5).getVersion() == 6);
 
     }
 }
