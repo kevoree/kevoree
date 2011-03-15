89bbd3a6cd1a861556163582b3cb6f0fd175271e
edaubert <Erwan.Daubert@inria.fr>
2011-03-15 09:27:01 +0100

    add GossiperChanellActor class

---
+++ kevoree-library/org.kevoree.library.gossiper/src/main/scala/org/kevoree/library/gossiper/GossiperChannelActor.scala
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.kevoree.library.gossiper
+
+class GossiperChannelActor extends actors.DaemonActor {
+
+  
+  /*
+   * si newValue => creer un uuid et on stocke
+   * on push à tout ceux connu
+   * 
+   * on attend que les autres viennent chercher
+   * 
+   * selectPeer => il est orienté
+   *  on va faire du gossip avec ceux dont on ne sait pas si ils sont à jour
+   */
+}
