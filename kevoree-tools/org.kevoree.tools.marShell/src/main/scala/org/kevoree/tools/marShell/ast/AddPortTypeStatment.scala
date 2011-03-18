2bce1c2705ca39a7fdcc53383f263763689dd384
Fouquet François <fouquet.f@gmail.com>
2011-03-18 08:21:36 +0100

    Improve Kevs
    Former-commit-id: 97ce52f24270a49b9850bc909278cd22246167e7

---
+++ kevoree-tools/org.kevoree.tools.marShell/src/main/scala/org/kevoree/tools/marShell/ast/AddPortTypeStatment.scala
@@ -0,0 +1,11 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.kevoree.tools.marShell.ast
+
+/* None className assume Message Port Type */
+class AddPortTypeStatment(portTypeName : String,componentTypeName : String,className : Option[String]) {
+
+}
