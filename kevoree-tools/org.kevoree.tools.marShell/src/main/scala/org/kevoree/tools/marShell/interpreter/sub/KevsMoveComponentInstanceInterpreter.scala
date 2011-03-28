2045d2964ee3b038cd45459904e68e2fac6cff22
Fouquet François <fouquet.f@gmail.com>
2011-03-28 09:08:22 +0200

    KevS
    Former-commit-id: cb832b046c097d3f8b0755ea6a13538b07a7c9ca

---
+++ kevoree-tools/org.kevoree.tools.marShell/src/main/scala/org/kevoree/tools/marShell/interpreter/sub/KevsMoveComponentInstanceInterpreter.scala
@@ -0,0 +1,10 @@
+package org.kevoree.tools.marShell.interpreter.sub
+
+import org.kevoree.tools.marShell.interpreter.{KevsInterpreterContext, KevsAbstractInterpreter}
+
+class KevsMoveComponentInstanceInterpreter extends KevsAbstractInterpreter {
+  def interpret(context: KevsInterpreterContext): Boolean = {
+    //TODO
+        true
+  }
+}
