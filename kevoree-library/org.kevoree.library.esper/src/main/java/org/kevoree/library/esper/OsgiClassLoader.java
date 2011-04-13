ddf4dd89f9031bbedd4937740ac5d7d16f61d0e1
Olivier Barais <olivier.barais@gmail.com>
2011-04-13 17:44:02 +0200

    add patched esper

---
+++ kevoree-library/org.kevoree.library.esper/src/main/java/org/kevoree/library/esper/OsgiClassLoader.java
@@ -0,0 +1,17 @@
+package org.kevoree.library.esper;
+
+import org.kevoree.ClassLoaderInterface;
+import org.osgi.framework.Bundle;
+
+public class OsgiClassLoader implements ClassLoaderInterface{
+	Bundle b;
+	public OsgiClassLoader(Bundle b) {
+		this.b = b;
+	}
+
+	@Override
+	public Class loadClass(String name) throws ClassNotFoundException {
+		return b.loadClass(name);
+	}
+
+}
