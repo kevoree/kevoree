0ef01db2f7f048ba51c198c7573a7c6bd0008b2b
Fouquet François <fouquet.f@gmail.com>
2011-04-18 17:30:14 +0200

    add RxTx channel Type
    Former-commit-id: 748fddef66a6f8cc1cbce33d4c892f57112f4fad

--- kevoree-core/org.kevoree.deploy.osgi/src/main/scala/org/kevoree/adaptation/deploy/osgi/command/AddDeployUnitCommand.scala
+++ kevoree-core/org.kevoree.deploy.osgi/src/main/scala/org/kevoree/adaptation/deploy/osgi/command/AddDeployUnitCommand.scala
@@ -23,66 +23,97 @@ import org.kevoree.DeployUnit
 import org.kevoree.adaptation.deploy.osgi.context.KevoreeDeployManager
 import org.kevoree.adaptation.deploy.osgi.context.KevoreeOSGiBundle
 import scala.collection.JavaConversions._
-import org.osgi.framework.BundleException
 import org.osgi.service.packageadmin.PackageAdmin
 import org.slf4j.LoggerFactory
+import org.osgi.framework.{FrameworkUtil, BundleException}
 
-case class AddDeployUnitCommand(deployUnit : DeployUnit, ctx : KevoreeDeployManager)  extends PrimitiveCommand{
+case class AddDeployUnitCommand(deployUnit: DeployUnit, ctx: KevoreeDeployManager) extends PrimitiveCommand {
 
   var logger = LoggerFactory.getLogger(this.getClass);
-	
-  def execute() : Boolean= {
+
+  def execute(): Boolean = {
     logger.info("CMD ADD DEPLOY UNIT EXECUTION ");
 
-    CommandHelper.buildAllQuery(deployUnit).exists{query=>
-      try{
-        logger.info("Try to install from URI, "+query)
-        lastExecutionBundle = Some(ctx.bundleContext.installBundle(query));
-        var symbolicName : String = lastExecutionBundle.get.getSymbolicName
+    CommandHelper.buildAllQuery(deployUnit).exists {
+      query =>
+        try {
+          logger.info("Try to install from URI, " + query)
+          lastExecutionBundle = Some(ctx.bundleContext.installBundle(query));
+          var symbolicName: String = lastExecutionBundle.get.getSymbolicName
 
-        //FOR DEPLOY UNIT DO NOT USE ONLY NAME
-        ctx.bundleMapping.append(KevoreeOSGiBundle(CommandHelper.buildKEY(deployUnit),deployUnit.getClass.getName,lastExecutionBundle.get))
-        //lastExecutionBundle.get.start
-				mustBeStarted = true
-		
-        true
-      } catch {
-        case e : BundleException if(e.getType == BundleException.DUPLICATE_BUNDLE_ERROR) => {
-            logger.warn("DeployUnit conflict ! ",e)
-            mustBeStarted = false
-            true
+          //FOR DEPLOY UNIT DO NOT USE ONLY NAME
+          ctx.bundleMapping.append(KevoreeOSGiBundle(CommandHelper.buildKEY(deployUnit), deployUnit.getClass.getName, lastExecutionBundle.get))
+          //lastExecutionBundle.get.start
+          mustBeStarted = true
+
+          true
+        } catch {
+          case e: BundleException if (e.getType == BundleException.DUPLICATE_BUNDLE_ERROR) => {
+            logger.warn("DeployUnit conflict ! ", e)
+
+            //try to found a valide candidate anyway
+
+            //found type definition
+            val optTypeDef = deployUnit.eContainer.asInstanceOf[ContainerRoot].getTypeDefinitions.find(typeDef => typeDef.getDeployUnits.contains(deployUnit))
+            optTypeDef match {
+              case Some(typDef) => {
+                lastExecutionBundle = ctx.bundleContext.getBundles.find {
+                  bundle =>
+                    try {
+                      bundle.loadClass(typDef.getBean)
+                      true
+                    } catch {
+                      case _@e => false
+                    }
+                }
+
+                lastExecutionBundle match {
+                  case Some(bundle) => {
+                    ctx.bundleMapping.append(KevoreeOSGiBundle(CommandHelper.buildKEY(deployUnit), deployUnit.getClass.getName, bundle))
+                    mustBeStarted = false
+                    true
+                  }
+                  case None => {
+                    mustBeStarted = false
+                    false
+                  }
+                }
+              }
+              case None => {
+                false
+              }
+            }
+
+
           }
-        case _ @ e =>{
-            try{
+          case _@e => {
+            try {
               lastExecutionBundle match {
                 case None => logger.error("failed to perform CMD ADD CT EXECUTION")
-                case Some(bundle) => logger.error("failed to perform CMD ADD CT EXECUTION on " +bundle.getSymbolicName,e);
+                case Some(bundle) => logger.error("failed to perform CMD ADD CT EXECUTION on " + bundle.getSymbolicName, e);
               }
             } catch {
-              case _=> logger.error("failed to perform CMD ADD CT EXECUTION")
+              case _ => logger.error("failed to perform CMD ADD CT EXECUTION")
             }
             false
           }
-      }
+        }
 
     }
 
-
-
-       
 
   }
 
   def undo() = {
     lastExecutionBundle match {
-      case Some(bundle)=> {
-          bundle.stop;
-          bundle.uninstall
-          var srPackageAdmin = ctx.bundleContext.getServiceReference(classOf[PackageAdmin].getName)
-          var padmin : PackageAdmin = ctx.bundleContext.getService(srPackageAdmin).asInstanceOf[PackageAdmin]
-          padmin.resolveBundles(Array(bundle))
-        }
-      case None=> //NOTHING CAN BE DOING HERE
+      case Some(bundle) => {
+        bundle.stop;
+        bundle.uninstall
+        var srPackageAdmin = ctx.bundleContext.getServiceReference(classOf[PackageAdmin].getName)
+        var padmin: PackageAdmin = ctx.bundleContext.getService(srPackageAdmin).asInstanceOf[PackageAdmin]
+        padmin.resolveBundles(Array(bundle))
+      }
+      case None => //NOTHING CAN BE DOING HERE
     }
     /* TODO CALL refreshPackage */
   }
