fd7cbde7ff82c350654ae8f628b346ceb789c7a0
Fouquet François <fouquet.f@gmail.com>
2011-05-02 15:26:50 +0200

    AppSample viever

--- kevoree-experiment/org.kevoree.experiment.trace.gui/src/main/java/org/kevoree/experiment/trace/gui/AppSample.java
+++ kevoree-experiment/org.kevoree.experiment.trace.gui/src/main/java/org/kevoree/experiment/trace/gui/AppSample.java
@@ -9,13 +9,13 @@ import java.net.URISyntaxException;
 
 public class AppSample {
 
-	public static void main(String[] args) throws IOException, URISyntaxException {
+    public static void main(String[] args) throws IOException, URISyntaxException {
 
-		InputStream input = AppSample.class.getClassLoader().getResourceAsStream("./trace_out");
+        InputStream input = AppSample.class.getClassLoader().getResourceAsStream("./trace_out");
 
-		TraceMessages.Traces traces = TraceMessages.Traces.parseFrom(input);
+        TraceMessages.Traces traces = TraceMessages.Traces.parseFrom(input);
 
-		for (TraceMessages.Trace trace : traces.getTraceList()) {
+        for (TraceMessages.Trace trace : traces.getTraceList()) {
 
             //UNSERIALIZE VECTOR CLOCK
 
@@ -24,57 +24,54 @@ public class AppSample {
                 String[] values = clockEntries[i].split(":");
                 if (values.length >= 2) {
                     System.out.println(values[0] + "=>" + values[1].trim());
-					Integer.parseInt(values[1].trim());
+                    Integer.parseInt(values[1].trim());
                 }
             }
         }
 
-		JFrame frame = new JFrame();
-		frame.setSize(400, 400);
+        JFrame frame = new JFrame();
+        frame.setSize(400, 400);
 
-		VectorClockDisseminationChart chart = new VectorClockDisseminationChart();
-		//chart.loadTrace(traces*//*, true*//*);
-		chart.loadTrace(new File(AppSample.class.getClassLoader().getResource("./trace_out").toURI()).getAbsolutePath());
-
-
-		ChartPanel chartPanel = new ChartPanel(chart.getChart());
-		chartPanel.setOpaque(false);
-		frame.add(chartPanel);
-
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+        VectorClockDisseminationChart chart = new VectorClockDisseminationChart();
+        //chart.loadTrace(traces*//*, true*//*);
+        chart.loadTrace(new File(AppSample.class.getClassLoader().getResource("./trace_out").toURI()).getAbsolutePath());
 
 
-	}
+        ChartPanel chartPanel = new ChartPanel(chart.getChart());
+        chartPanel.setOpaque(false);
+        frame.add(chartPanel);
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
 
 
-	public static String convertStreamToString(InputStream is)
-			throws IOException {
-		/*
-				 * To convert the InputStream to String we use the
-				 * Reader.read(char[] buffer) method. We iterate until the
-				 * Reader return -1 which means there's no more data to
-				 * read. We use the StringWriter class to produce the string.
-				 */
-		if (is != null) {
-			Writer writer = new StringWriter();
+    }
 
-			char[] buffer = new char[1024];
-			try {
-				Reader reader = new BufferedReader(
-						new InputStreamReader(is, "UTF-8"));
-				int n;
-				while ((n = reader.read(buffer)) != -1) {
-					writer.write(buffer, 0, n);
-				}
-			} finally {
-				is.close();
-			}
-			return writer.toString();
-		} else {
-			return "";
-		}
-	}
+    public static String convertStreamToString(InputStream is)
+            throws IOException {
+        /*
+         * To convert the InputStream to String we use the
+         * Reader.read(char[] buffer) method. We iterate until the
+         * Reader return -1 which means there's no more data to
+         * read. We use the StringWriter class to produce the string.
+         */
+        if (is != null) {
+            Writer writer = new StringWriter();
 
-
+            char[] buffer = new char[1024];
+            try {
+                Reader reader = new BufferedReader(
+                        new InputStreamReader(is, "UTF-8"));
+                int n;
+                while ((n = reader.read(buffer)) != -1) {
+                    writer.write(buffer, 0, n);
+                }
+            } finally {
+                is.close();
+            }
+            return writer.toString();
+        } else {
+            return "";
+        }
+    }
 }
