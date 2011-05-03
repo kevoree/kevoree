45c16d7f287df2d780f6b8f511de0fb6df596e84
edaubert <Erwan.Daubert@inria.fr>
2011-05-03 11:40:38 +0200

    first test to use url pax handler

--- kevoree-tools/org.kevoree.tools.agent/src/main/java/org/kevoree/tools/agent/KevoreeNodeRunner.java
+++ kevoree-tools/org.kevoree.tools.agent/src/main/java/org/kevoree/tools/agent/KevoreeNodeRunner.java
@@ -1,17 +1,36 @@
 package org.kevoree.tools.agent;
 
+
+import org.ops4j.pax.url.mvn.Handler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
 public class KevoreeNodeRunner {
 
-    public KevoreeNodeRunner(String nodeName , Integer basePort){
+	private Logger logger = LoggerFactory.getLogger(KevoreeNodeRunner.class);
 
-    }
+	private Process nodePlatformProcess;
 
-    public void startNode(){
+	public KevoreeNodeRunner(String nodeName, Integer basePort) {
 
-    }
+	}
 
-    public void stopKillNode(){
+	public void startNode() {
+		
+	
+	}
 
-    }
+	public void stopKillNode() {
+		try {
+			nodePlatformProcess.getOutputStream().write("stop 0".getBytes());
+			nodePlatformProcess.getOutputStream().flush();
+		} catch (IOException e) {
+			//e.printStackTrace();
+			logger.error("The node cannot be killed. Try to force kill", e.getCause());
+			nodePlatformProcess.destroy();
+		}
+	}
 
 }
