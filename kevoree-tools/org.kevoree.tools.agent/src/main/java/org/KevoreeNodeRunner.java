27c48638b44105233643a94c2817f6df9a893c8a
Fouquet François <fouquet.f@gmail.com>
2011-05-03 11:15:49 +0200

    Add Interface

---
+++ kevoree-tools/org.kevoree.tools.agent/src/main/java/org/KevoreeNodeRunner.java
@@ -0,0 +1,17 @@
+package org;
+
+public class KevoreeNodeRunner {
+
+    public KevoreeNodeRunner(String nodeName , Integer basePort){
+
+    }
+
+    public void startNode(){
+
+    }
+
+    public void stopKillNode(){
+
+    }
+
+}
