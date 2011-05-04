a14dfde62e29a4f3c45aa8c5d78be7122420f8e4
Fouquet François <fouquet.f@gmail.com>
2011-05-04 16:36:37 +0200

    move
    Former-commit-id: f3bc7016a946b2d9abe67644dca2da34f0340117

---
+++ kevoree-platform/org.kevoree.platform.agent/src/main/java/org/kevoree/tools/agent/KevoreeNodeRunner.java
@@ -0,0 +1,136 @@
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kevoree.tools.agent;
+
+import org.ops4j.pax.url.mvn.Handler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLStreamHandler;
+import java.net.URLStreamHandlerFactory;
+
+public class KevoreeNodeRunner {
+
+	private static final String defaultJarFilePath = System.getProperty("java.io.tmpdir") + "org.kevoree.platform.osgi.stanhdalone.jar";
+
+	private Logger logger = LoggerFactory.getLogger(KevoreeNodeRunner.class);
+
+	private Process nodePlatformProcess;
+	private String platformJARPath;
+
+	private String nodeName;
+	private Integer basePort;
+
+	public KevoreeNodeRunner(String nodeName, Integer basePort) {
+
+		//URL url = new URL(null,"cvs://server/project/folder#version", new PaxMvnUrlStreamHandlerFactory());
+		//System.setProperty("org.ops4j.pax.url.mvn.defaultRepositories", "http://maven.kevoree.org/release");
+		URL.setURLStreamHandlerFactory(new PaxMvnUrlStreamHandlerFactory());
+
+	}
+
+	public void startNode() {
+		System.out.println("StartNodeCommand");
+		if (platformJARPath == null) {
+			getJar();
+		}
+		try {
+			nodePlatformProcess = Runtime.getRuntime().exec(new String[]{"java", "-Dnode.name=" + nodeName, "-Dnode.port=" + basePort, "-jar", platformJARPath});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void stopKillNode() {
+		System.out.println("KillNodeCommand");
+		try {
+			nodePlatformProcess.getOutputStream().write("stop 0".getBytes());
+			nodePlatformProcess.getOutputStream().flush();
+		} catch (IOException e) {
+			//e.printStackTrace();
+			logger.error("The node cannot be killed. Try to force kill", e.getCause());
+			nodePlatformProcess.destroy();
+		}
+	}
+
+	/*private void getJar() { // from maven
+		try {
+			URL mvnURL = new URL("mvn:http://maven.kevoree.org/release/!org.kevoree.platform/org.kevoree.platform.osgi.standalone");
+			InputStream stream = mvnURL.openConnection().getInputStream();
+
+			File f = File.createTempFile("org.kevoree.platform.osgi.standalone", ".jar");
+			f.deleteOnExit();
+			OutputStream outputStream = new FileOutputStream(f);
+
+			byte[] bytes = new byte[1024];
+			int length = 0;
+			while ((length = stream.read()) != -1) {
+				outputStream.write(bytes, 0, length);
+			}
+			outputStream.flush();
+			outputStream.close();
+			stream.close();
+
+			platformJARPath = f.getAbsolutePath();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}*/
+
+	private void getJar() { // from the availables resources
+		try {
+			InputStream stream = this.getClass().getClassLoader().getResourceAsStream("org.kevoree.platform.osgi.standalone.jar");
+
+			File f = new File(defaultJarFilePath);
+			f.deleteOnExit();
+			OutputStream outputStream = new FileOutputStream(f);
+
+			byte[] bytes = new byte[1024];
+			int length;
+			while ((length = stream.read()) != -1) {
+				outputStream.write(bytes, 0, length);
+			}
+			outputStream.flush();
+			outputStream.close();
+			stream.close();
+
+			platformJARPath = f.getAbsolutePath();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public class PaxMvnUrlStreamHandlerFactory implements URLStreamHandlerFactory {
+
+		Handler handler;
+
+		public PaxMvnUrlStreamHandlerFactory() {
+			handler = new Handler();
+		}
+
+		@Override
+		public URLStreamHandler createURLStreamHandler(String protocol) {
+			return handler;
+		}
+	}
+}
---
+++ kevoree-platform/org.kevoree.platform.agent/src/main/scala/org/kevoree/tools/agent/KevoreeRuntimeAgent.scala
@@ -0,0 +1,65 @@
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kevoree.tools.agent
+
+import org.kevoree.ContainerRoot
+import scala.collection.JavaConversions._
+import java.net.NetworkInterface
+import org.kevoree.framework.{Constants, KevoreePlatformHelper}
+
+class KevoreeRuntimeAgent {
+
+  def processModel(model:ContainerRoot){
+    KevoreeNodeRunnerHandler.closeAllRunners()
+    detectLocalNodeFromRuntime(model).foreach{ t=>
+        KevoreeNodeRunnerHandler.addRunner(t._1,t._2)
+    }
+  }
+
+
+  private def detectLocalNodeFromRuntime(model: ContainerRoot): List[(String,Int)] = {
+
+    var localIPS: List[String] = List()
+    NetworkInterface.getNetworkInterfaces.foreach {
+      netIT =>
+        netIT.getInetAddresses.foreach {
+          inetAddr =>
+            localIPS = localIPS :+ inetAddr.getHostAddress
+            localIPS = localIPS :+ inetAddr.getCanonicalHostName
+            localIPS = localIPS :+ inetAddr.getHostAddress
+        }
+    }
+
+    var result: List[(String,Int)] = List()
+    model.getNodes.foreach {
+      node =>
+        var IP = KevoreePlatformHelper.getProperty(model, node.getName, Constants.KEVOREE_PLATFORM_REMOTE_NODE_IP);
+        if (IP == "") {
+          IP = "127.0.0.1"
+        }
+        var PORT = KevoreePlatformHelper.getProperty(model, node.getName, Constants.KEVOREE_PLATFORM_REMOTE_NODE_MODELSYNCH_PORT);
+        if (PORT == "") {
+          PORT = "8000"
+        }
+
+        if (localIPS.exists(localIP => localIP.contains(IP))) {
+          result = result :+ (node.getName,Integer.parseInt(PORT))
+        }
+
+    }
+    result
+  }
+
+
+}
