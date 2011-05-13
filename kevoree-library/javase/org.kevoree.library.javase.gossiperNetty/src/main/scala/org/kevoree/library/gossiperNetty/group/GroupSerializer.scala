21af2198c884c5b443af36787070cdcc0540df56
edaubert <Erwan.Daubert@inria.fr>
2011-05-13 16:15:48 +0200

    reduce the number of serialization of model

--- kevoree-library/javase/org.kevoree.library.javase.gossiperNetty/src/main/scala/org/kevoree/library/gossiperNetty/group/GroupSerializer.scala
+++ kevoree-library/javase/org.kevoree.library.javase.gossiperNetty/src/main/scala/org/kevoree/library/gossiperNetty/group/GroupSerializer.scala
@@ -5,6 +5,8 @@ import org.kevoree.framework.KevoreeXmiHelper
 import java.io.{ByteArrayOutputStream, ByteArrayInputStream}
 import org.slf4j.LoggerFactory
 import org.kevoree.library.gossiperNetty.Serializer
+import org.kevoree.api.service.core.handler.KevoreeModelHandlerService
+import java.util.Date
 
 /**
  * User: Erwan Daubert
@@ -12,43 +14,49 @@ import org.kevoree.library.gossiperNetty.Serializer
  * Time: 14:40
  */
 
-class GroupSerializer extends Serializer {
+class GroupSerializer (modelService: KevoreeModelHandlerService) extends Serializer {
 
-	private var logger = LoggerFactory.getLogger(classOf[GroupSerializer])
+  private var logger = LoggerFactory.getLogger (classOf[GroupSerializer])
 
-	def serialize(data: Any): Array[Byte] = {
-		try {
-			stringFromModel(data.asInstanceOf[ContainerRoot])
-		} catch {
-			case e => {
-				logger.error("Model cannot be serialize: ",  e)
-				null
-			}
-		}
-	}
+  private var lastSerialization: Date = null
+  private var bytes: Array[Byte] = null
 
-	def deserialize(data: Array[Byte]): Any = {
-		try {
-			modelFromString(data)
-		} catch {
-			case e => {
-				logger.error("Model cannot be deserialize: ", e)
-				null
-			}
-		}
-	}
+  def serialize (data: Any): Array[Byte] = {
+    try {
+      if (lastSerialization.before (modelService.getLastModification)) {
+        stringFromModel (data.asInstanceOf[ContainerRoot])
+      }
+      bytes
+    } catch {
+      case e => {
+        logger.error ("Model cannot be serialize: ", e)
+        null
+      }
+    }
+  }
 
-	private def modelFromString(model: Array[Byte]): ContainerRoot = {
-		val stream = new ByteArrayInputStream(model)
-		KevoreeXmiHelper.loadStream(stream)
-	}
+  def deserialize (data: Array[Byte]): Any = {
+    try {
+      modelFromString (data)
+    } catch {
+      case e => {
+        logger.error ("Model cannot be deserialize: ", e)
+        null
+      }
+    }
+  }
 
-	private def stringFromModel(model: ContainerRoot) : Array[Byte] = {
-		val out = new ByteArrayOutputStream
-		KevoreeXmiHelper.saveStream(out, model)
-		out.flush()
-		val bytes = out.toByteArray
-		out.close()
-		bytes
-	}
+  private def modelFromString (model: Array[Byte]) {
+    val stream = new ByteArrayInputStream (model)
+    KevoreeXmiHelper.loadStream (stream)
+  }
+
+  private def stringFromModel (model: ContainerRoot): Array[Byte] = {
+    val out = new ByteArrayOutputStream
+    KevoreeXmiHelper.saveStream (out, model)
+    out.flush ()
+    bytes = out.toByteArray
+    out.close ()
+    lastSerialization = System.currentTimeMillis
+  }
 }
