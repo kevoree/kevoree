b849befe11981677072e79afb446ab0dbabc2231
Fouquet François <fouquet.f@gmail.com>
2011-05-17 14:34:57 +0200

    fix bug

--- kevoree-experiment/org.kevoree.experiment.modelScript/src/main/scala/org/kevoree/experiment/modelScript/BootStrapApp.scala
+++ kevoree-experiment/org.kevoree.experiment.modelScript/src/main/scala/org/kevoree/experiment/modelScript/BootStrapApp.scala
@@ -21,12 +21,12 @@ object BootStrapApp extends Application {
   val ips = List(dukeIP, paraisseuxIP)
 
 
-  tscript append generatePhysicalNodeScript("duke", dukeIP, 8000, 10)
-  tscript append generatePhysicalNodeScript("paraisseux", paraisseuxIP, 8000, 10)
+  tscript append generatePhysicalNodeScript("duke", dukeIP, 8000, 4)
+  tscript append generatePhysicalNodeScript("paraisseux", paraisseuxIP, 8000, 4)
 
   //ADD GLOBAL GROUP
   tscript append "addGroup gossipGroup : LogNettyGossiperGroup {"
-  tscript append "port=\"" + generateGroupFragmentPort(List(("duke", 10, 9000), ("paraisseux", 10, 9000))) + "\"\n"
+  tscript append "port=\"" + generateGroupFragmentPort(List(("duke", 4, 9000), ("paraisseux", 4, 9000))) + "\"\n"
   tscript append ",loggerServerIP=\"" + dukeIP + "\""
 
 
--- kevoree-library/javase/org.kevoree.library.javase.gossiperNetty/src/test/scala/org/kevoree/library/gossiperNetty/group/ModelSerializationTest.scala
+++ kevoree-library/javase/org.kevoree.library.javase.gossiperNetty/src/test/scala/org/kevoree/library/gossiperNetty/group/ModelSerializationTest.scala
@@ -7,7 +7,7 @@ import org.kevoree.ContainerRoot
 import java.io.ByteArrayOutputStream
 import java.util.Date
 import org.eclipse.emf.ecore.util.EcoreUtil
-import actors.DaemonActor
+import actors.{Actor, DaemonActor}
 
 /**
  * User: Erwan Daubert - erwan.daubert@gmail.com
@@ -17,179 +17,211 @@ import actors.DaemonActor
 
 class ModelSerializationTest extends AssertionsForJUnit {
 
-  object MyHandler extends DaemonActor {
+  class CMyHandler extends Actor {
+    //start()
 
-    val containerRoot = KevoreeXmiHelper.load ("src/test/resources/concurrentModel.kev")
-    start ()
+    println("deamon start")
+    val containerRoot = KevoreeXmiHelper.load(this.getClass.getClassLoader.getResource("concurrentModel.kev").getFile)
 
-    case class GETTOTO ()
 
-    def get (): ContainerRoot = {
-      (this !? GETTOTO ()).asInstanceOf[ContainerRoot]
+    case class GETTOTO()
+
+    case class STOP()
+
+    def getmodel(): ContainerRoot = {
+      (this !? GETTOTO()).asInstanceOf[ContainerRoot]
     }
 
     /* PRIVATE PROCESS PART */
-    def act () {
+    def act() {
+
       loop {
+
         react {
-          case GETTOTO () => {
-            println("ça va bloquer")
-            reply (EcoreUtil.copy (containerRoot))
-            println("en fait nan")
+
+
+          case GETTOTO() => {
+            try {
+
+                reply(EcoreUtil.copy(containerRoot))
+
+              //reply(containerRoot)
+
+            } catch {
+              case _@e => e.printStackTrace()
+            }
+
 
           }
+          case STOP() => exit()
         }
+
+
       }
     }
   }
 
-  @Test def concurrentSerialization () {
+  @Test def concurrentSerialization() {
 
+    val MyHandler = new CMyHandler
+    MyHandler.start()
 
-    println ("toto")
+    MyHandler.getmodel()
+    println("toto")
 
-    val t1 = new Thread () {
-      override def run () {
-        println ("t1" + System.currentTimeMillis ())
-        serialize (MyHandler.get ())
-        println ("t1" + System.currentTimeMillis ())
+    val t1 = new Thread() {
+      override def run() {
+
+        serialize(MyHandler.getmodel())
+
       }
     }
-    val t2 = new Thread () {
-      override def run () {
+    val t2 = new Thread() {
+      override def run() {
         //println("t2" + System.currentTimeMillis())
-        serialize (MyHandler.get ())
+        serialize(MyHandler.getmodel())
         //println("t2" + System.currentTimeMillis())
       }
     }
-    val t3 = new Thread () {
-      override def run () {
+    val t3 = new Thread() {
+      override def run() {
         //println("t3" + System.currentTimeMillis())
-        serialize (MyHandler.get ())
+        serialize(MyHandler.getmodel())
         //println("t3" + System.currentTimeMillis())
       }
     }
-    val t4 = new Thread () {
-      override def run () {
+    val t4 = new Thread() {
+      override def run() {
         //println("t4" + System.currentTimeMillis())
-        serialize (MyHandler.get ())
+        serialize(MyHandler.getmodel())
         //println("t4" + System.currentTimeMillis())
       }
     }
-    val t5 = new Thread () {
-      override def run () {
+    val t5 = new Thread() {
+      override def run() {
         //println("t5" + System.currentTimeMillis())
-        serialize (MyHandler.get ())
+        serialize(MyHandler.getmodel())
         //println("t5" + System.currentTimeMillis())
       }
     }
-    val t6 = new Thread () {
-      override def run () {
+    val t6 = new Thread() {
+      override def run() {
         //println("t6" + System.currentTimeMillis())
-        serialize (MyHandler.get ())
+        serialize(MyHandler.getmodel())
         //println("t6" + System.currentTimeMillis())
       }
     }
-    val t7 = new Thread () {
-      override def run () {
+    val t7 = new Thread() {
+      override def run() {
         //println("t7" + System.currentTimeMillis())
-        serialize (MyHandler.get ())
+        serialize(MyHandler.getmodel())
         //println("t7" + System.currentTimeMillis())
       }
     }
-    val t8 = new Thread () {
-      override def run () {
+    val t8 = new Thread() {
+      override def run() {
         //println("t8" + System.currentTimeMillis())
-        serialize (MyHandler.get ())
+        serialize(MyHandler.getmodel())
         //println("t8" + System.currentTimeMillis())
       }
     }
-    val t9 = new Thread () {
-      override def run () {
-        println ("t9" + System.currentTimeMillis ())
-        serialize (MyHandler.get ())
-        println ("t9" + System.currentTimeMillis ())
+    val t9 = new Thread() {
+      override def run() {
+        //println("t9" + System.currentTimeMillis())
+        serialize(MyHandler.getmodel())
+        // println("t9" + System.currentTimeMillis())
       }
     }
-    val t10 = new Thread () {
-      override def run () {
-        println ("t10" + System.currentTimeMillis ())
-        serialize (MyHandler.get ())
-        println ("t10" + System.currentTimeMillis ())
+    val t10 = new Thread() {
+      override def run() {
+
+        serialize(MyHandler.getmodel())
+
       }
     }
-    val t11 = new Thread () {
-      override def run () {
+    val t11 = new Thread() {
+      override def run() {
         //println("t11" + System.currentTimeMillis())
-        serialize (MyHandler.get ())
+        serialize(MyHandler.getmodel())
         //println("t11" + System.currentTimeMillis())
       }
     }
-    val t12 = new Thread () {
-      override def run () {
+    val t12 = new Thread() {
+      override def run() {
         //println("t12" + System.currentTimeMillis())
-        serialize (MyHandler.get ())
+        serialize(MyHandler.getmodel())
         //println("t12" + System.currentTimeMillis())
       }
     }
-    val t13 = new Thread () {
-      override def run () {
+    val t13 = new Thread() {
+      override def run() {
         //println("t13" + System.currentTimeMillis())
-        serialize (MyHandler.get ())
+        serialize(MyHandler.getmodel())
         //println("t13" + System.currentTimeMillis())
       }
     }
-    val t14 = new Thread () {
-      override def run () {
+    val t14 = new Thread() {
+      override def run() {
         //println("t14" + System.currentTimeMillis())
-        serialize (MyHandler.get ())
+        serialize(MyHandler.getmodel())
         //println("t14" + System.currentTimeMillis())
       }
     }
-    val t15 = new Thread () {
-      override def run () {
+    val t15 = new Thread() {
+      override def run() {
         //println("t15" + System.currentTimeMillis())
-        serialize (MyHandler.get ())
+        serialize(MyHandler.getmodel())
         //println("t15" + System.currentTimeMillis())
       }
     }
-    val t16 = new Thread () {
-      override def run () {
-        println ("t16 " + System.currentTimeMillis ())
-        serialize (MyHandler.get ())
-        println ("t16 " + System.currentTimeMillis ())
+    val t16 = new Thread() {
+      override def run() {
+        serialize(MyHandler.getmodel())
       }
     }
 
-    t1.start ()
-    t2.start ()
-    t3.start ()
-    t4.start ()
-    t5.start ()
-    t6.start ()
-    t7.start ()
-    t8.start ()
-    t9.start ()
-    t10.start ()
-    t11.start ()
-    t12.start ()
-    t13.start ()
-    t14.start ()
-    t15.start ()
-    t16.start ()
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
+    t5.start()
+    t6.start()
+    t7.start()
+    t8.start()
+    t9.start()
+    t10.start()
+    t11.start()
+    t12.start()
+    t13.start()
+    t14.start()
+    t15.start()
+    t16.start()
+
+
+    Thread.sleep(3000)
 
 
   }
 
-  private def serialize (model: ContainerRoot) {
-    println ("start serilaierr ...")
-    val out = new ByteArrayOutputStream
-    KevoreeXmiHelper.saveStream (out, model)
-    out.flush ()
-    val bytes = out.toByteArray
-    out.close ()
-    val lastSerialization = new Date (System.currentTimeMillis)
-    println ("end serilaierr ...")
+  var i = 0
+
+  private def serialize(model: ContainerRoot) {
+
+    try {
+      println("start " + i)
+      val out = new ByteArrayOutputStream
+      KevoreeXmiHelper.saveStream(out, model)
+      out.flush()
+      val bytes = out.toByteArray
+      out.close()
+      val lastSerialization = new Date(System.currentTimeMillis)
+      println("Stop " + i)
+      i = i + 1
+    } catch {
+      case _@e => e.printStackTrace()
+    }
+
+
   }
 
 }
