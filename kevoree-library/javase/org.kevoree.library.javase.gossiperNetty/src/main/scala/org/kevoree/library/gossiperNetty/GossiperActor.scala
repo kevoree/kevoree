36fbe0824661947cc72db0c060ac35d97a40ab2b
edaubert <Erwan.Daubert@inria.fr>
2011-05-18 14:02:23 +0200

    add two options on Gossiper Component  first: select if we want to send notification  second: select if we want to always ask data

--- kevoree-library/javase/org.kevoree.library.javase.gossiperNetty/src/main/scala/org/kevoree/library/gossiperNetty/GossiperActor.scala
+++ kevoree-library/javase/org.kevoree.library.javase.gossiperNetty/src/main/scala/org/kevoree/library/gossiperNetty/GossiperActor.scala
@@ -5,63 +5,64 @@
 
 package org.kevoree.library.gossiperNetty
 
-import java.security.SecureRandom
 import scala.actors.TIMEOUT
-import scala.collection.JavaConversions._
 
-class GossiperActor(timeout: java.lang.Long, channel: NettyGossipAbstractElement, dataManager: DataManager, port: Int, fullUDP: java.lang.Boolean, garbage: Boolean,serializer : Serializer, selector : PeerSelector) extends actors.DaemonActor {
+class GossiperActor (timeout: java.lang.Long, channel: NettyGossipAbstractElement, dataManager: DataManager, port: Int,
+  fullUDP: java.lang.Boolean, garbage: Boolean, serializer: Serializer, selector: PeerSelector, alwaysAskModel : Boolean)
+  extends actors.DaemonActor {
 
-	private var gossiperRequestSender = new GossiperRequestSender(timeout, channel, dataManager, fullUDP, garbage,serializer)
-	private var notificationRequestSender = new NotificationRequestSender(channel)
-	private var gossiperRequestReceiver = new GossiperRequestReceiver(channel, dataManager, port, gossiperRequestSender, fullUDP, serializer)
+  private val gossiperRequestSender = new
+      GossiperRequestSender (timeout, channel, dataManager, fullUDP, garbage, serializer, alwaysAskModel)
+  private val notificationRequestSender = new NotificationRequestSender (channel)
+  private val gossiperRequestReceiver = new
+      GossiperRequestReceiver (channel, dataManager, port, gossiperRequestSender, fullUDP, serializer)
 
-  this.start()
+  this.start ()
 
-	/* PUBLIC PART */
-	case class STOP_GOSSIPER()
+  /* PUBLIC PART */
+  case class STOP_GOSSIPER ()
 
-	case class DO_GOSSIP(targetNodeName: String)
+  case class DO_GOSSIP (targetNodeName: String)
 
-	case class NOTIFY_PEERS()
+  case class NOTIFY_PEERS ()
 
-	def stop() {
-		this ! STOP_GOSSIPER()
-	}
+  def stop () {
+    this ! STOP_GOSSIPER ()
+  }
 
-	def scheduleGossip(nodeName: String) {
-		this ! DO_GOSSIP(nodeName)
-	}
+  def scheduleGossip (nodeName: String) {
+    this ! DO_GOSSIP (nodeName)
+  }
 
-	def notifyPeers() {
-		this ! NOTIFY_PEERS()
-	}
+  def notifyPeers () {
+    this ! NOTIFY_PEERS ()
+  }
 
-	/* PRIVATE PROCESS PART */
-	def act() {
-		loop {
-			reactWithin(timeout.longValue) {
-				case STOP_GOSSIPER() => {
-					gossiperRequestSender.stop()
-					gossiperRequestReceiver.stop()
-					notificationRequestSender.stop()
-					this.exit()
-				}
-				case DO_GOSSIP(targetNodeName) => doGossip(targetNodeName)
-				case NOTIFY_PEERS() => notificationRequestSender.notifyPeersAction()
-				case TIMEOUT => {
-					val peer = selector.selectPeer(channel.getName)//channel.selectPeer
-					if (!peer.equals("")) {
-						doGossip(peer)
-					}
-				}
-			}
-		}
-	}
+  /* PRIVATE PROCESS PART */
+  def act () {
+    loop {
+      reactWithin (timeout.longValue) {
+        case STOP_GOSSIPER () => {
+          gossiperRequestSender.stop ()
+          gossiperRequestReceiver.stop ()
+          notificationRequestSender.stop ()
+          this.exit ()
+        }
+        case DO_GOSSIP (targetNodeName) => doGossip (targetNodeName)
+        case NOTIFY_PEERS () => notificationRequestSender.notifyPeersAction ()
+        case TIMEOUT => {
+          val peer = selector.selectPeer (channel.getName) //channel.selectPeer
+          if (!peer.equals ("")) {
+            doGossip (peer)
+          }
+        }
+      }
+    }
+  }
 
-	private def doGossip(targetNodeName: String) = {
-		if (targetNodeName != null && targetNodeName != "") {
-			//println("launch Gossip")
-			gossiperRequestSender.initGossipAction(targetNodeName)
-		}
-	}
+  private def doGossip (targetNodeName: String) {
+    if (targetNodeName != null && targetNodeName != "") {
+      gossiperRequestSender.initGossipAction (targetNodeName)
+    }
+  }
 }
