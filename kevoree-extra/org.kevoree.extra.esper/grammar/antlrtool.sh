47c268afc1d4b6301333b2a7d46c534c905ce781
Fouquet François <fouquet.f@gmail.com>
2011-07-18 18:43:42 +0200

    cleanup kevoree extra

--- kevoree-extra/org.kevoree.extra.esper/grammar/antlrtool.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Script to the ANTLR tool parser compiler
-#
-$JAVA_HOME/bin/java -classpath ../lib/antlr-3.2.jar org.antlr.Tool -fo ../src/main/java/com/espertech/esper/epl/generated EsperEPL2Grammar.g EsperEPL2Ast.g -Xmaxinlinedfastates 2000
-$JAVA_HOME/bin/java -classpath ../target/classes com.espertech.esper.util.ParserTool ../src/main/java/com/espertech/esper/epl/generated/EsperEPL2GrammarParser.java
