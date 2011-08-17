95fca26de23f9241daba8ab7530752f84fe8d8e3
Fouquet François <fouquet.f@gmail.com>
2011-08-17 23:14:24 +0200

    fix

--- kevoree-library/arduino/org.kevoree.library.arduino.components/src/main/java/org/kevoree/library/sensors/AutoNormalizer.java
+++ kevoree-library/arduino/org.kevoree.library.arduino.components/src/main/java/org/kevoree/library/sensors/AutoNormalizer.java
@@ -23,13 +23,13 @@ public class AutoNormalizer extends AbstractComponentType {
 
     @Stop
     public void stop() {
-
     }
 
     @Generate("classheader")
     public void generateHeader(StringBuffer context) {
-        context.append("int minValue"+this.getName());
-        context.append("= 1024;\n");
+        context.append("char buf[10];\n");
+        context.append("int minValue;\n");
+        context.append("int maxValue;\n");
         context.append("int ");
         context.append("maxValue" + this.getName());
         context.append("= 0;\n");
@@ -37,44 +37,27 @@ public class AutoNormalizer extends AbstractComponentType {
         context.append("char * value");
     }
 
+    @Generate("classinit")
+    public void generateClassInit(StringBuffer context) {
+        context.append("minValue = 1024;\n");
+        context.append("maxValue = 0;\n");
+    }
 
     @Port(name = "input")
     public void triggerPort(Object o) {
         StringBuffer context = (StringBuffer) o;
-
-        context.append("" +
-                "" +
-                "  char msg[param.length()+10];\n" +
-                "  param.toCharArray(msg, param.length()+1);\n" +
-                "  float value = atof(msg);\n");
-
-        context.append("if(value < " + "minValue" + this.getName() + "){ " + "minValue" + this.getName() + " = value; }\n");
-        context.append("if(value > " + "maxValue" + this.getName() + "){ " + "maxValue" + this.getName() + " = value; }\n");
-            context.append("float result=");
-            context.append(" ((value-" + "minValue" + this.getName() + ")");
-            context.append(" / (" + "maxValue" + this.getName() + "-" + "minValue" + this.getName() + "))*100");
-            context.append(";\n");
-
-        if (this.getDictionary().get("inverted").equals("1")) {
-            context.append(" result = abs(100 - result  );\n");
-        }
-
-
+        context.append("float value = atof(msg->value);\n");
+        context.append("if(value < minValue){ minValue = value; }\n");
+        context.append("if(value > maxValue){ maxValue = value; }\n");
+        context.append("float result=((value-minValue)/ (maxValue-minValue))*100;\n");
+        context.append("if(atoi(pin) == 0){result = abs(100 - result  );}\n");
         context.append("kmessage * msg = (kmessage*) malloc(sizeof(kmessage));");
         context.append("if (msg){memset(msg, 0, sizeof(kmessage));}");
-        context.append("msg->value = \"release\";");
+        context.append("sprintf(buf,\"%d\",int(result));\n");
+        context.append("smsg->value = buf;\n");
         context.append("msg->metric = \"percent\";");
-        context.append("release_rport(msg);");
+        context.append("norm_rport(msg);");
         context.append("free(msg);");
-
-
-
-
-        //GENERATE METHOD CALL
-        //context.append(ArduinoMethodHelper.generateMethodNameFromComponentPort(this.getName(), "norm", PortUsage.required()));
-        //GENERATE PARAMETER
-        //context.append("(String(int(result)));\n");
-
     }
 
 }
