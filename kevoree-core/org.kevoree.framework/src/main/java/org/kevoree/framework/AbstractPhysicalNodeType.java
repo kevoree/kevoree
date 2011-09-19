8bf076aa78b26d8b8c180aa3109d40abfa0aba76
Erwan Daubert <erwan.daubert@gmail.com>
2011-09-19 13:38:28 +0200

    add first sky stuff

---
+++ kevoree-core/org.kevoree.framework/src/main/java/org/kevoree/framework/AbstractPhysicalNodeType.java
@@ -0,0 +1,60 @@
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kevoree.framework;
+
+import org.kevoree.ContainerRoot;
+import org.kevoree.api.service.adaptation.deploy.KevoreeAdaptationDeployService;
+import org.kevoree.api.service.core.handler.KevoreeModelHandlerService;
+import org.osgi.framework.BundleContext;
+
+import java.util.HashMap;
+
+public abstract class AbstractPhysicalNodeType implements KevoreeAdaptationDeployService {
+
+    public abstract void push(String physicalNodeName, ContainerRoot root, BundleContext context);
+
+    private HashMap<String, Object> dictionary = new HashMap<String, Object>();
+
+    public HashMap<String, Object> getDictionary() {
+        return this.dictionary;
+    }
+
+    public void setDictionary(HashMap<String, Object> dic){
+         dictionary = dic;
+    }
+
+    public void startNode(){}
+
+    public void stopNode(){}
+
+    public void updateNode(){}
+
+    private KevoreeModelHandlerService modelService;
+    public void setModelService(KevoreeModelHandlerService ms){
+        modelService = ms;
+    }
+    public KevoreeModelHandlerService getModelService() {
+        return modelService;
+    }
+
+    private String physicalNodeName = "";
+    public String getPhysicalNodeName() {
+        return physicalNodeName;
+    }
+    public void setPhysicalNodeName(String physicalNodeName) {
+        this.physicalNodeName = physicalNodeName;
+    }
+
+
+}
