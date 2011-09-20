b9e4ff8a2c7cdbb7ee829596d0c8cddaa3be60da
duke <fouquet.f@gmail.com>
2011-09-20 20:25:39 +0200

    big cleanup ;-)
    Former-commit-id: 3544731951c1ec24da2240f1610606b8dbd99bab

---
+++ kevoree-core/org.kevoree.framework/src/main/scala/org/kevoree/framework/deploy/PrimitiveCommandExecutionHelper.scala
@@ -0,0 +1,19 @@
+package org.kevoree.framework.deploy
+
+import org.kevoreeAdaptation.AdaptationModel
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: duke
+ * Date: 20/09/11
+ * Time: 20:19
+ * To change this template use File | Settings | File Templates.
+ */
+
+trait PrimitiveCommandExecutionHelper {
+
+  def execute(adaptionModel:AdaptationModel) : Boolean = {
+    true
+  }
+
+}
