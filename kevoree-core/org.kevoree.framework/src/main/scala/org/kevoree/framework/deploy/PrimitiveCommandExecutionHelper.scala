e121daaf2a34575f472792a1f207a46c693ed6f1
duke <fouquet.f@gmail.com>
2011-09-20 22:43:25 +0200

    improve algo phase
    Former-commit-id: e09a4dda0590be6254a9985a59c380d32062fd26

--- kevoree-core/org.kevoree.framework/src/main/scala/org/kevoree/framework/deploy/PrimitiveCommandExecutionHelper.scala
+++ kevoree-core/org.kevoree.framework/src/main/scala/org/kevoree/framework/deploy/PrimitiveCommandExecutionHelper.scala
@@ -1,19 +1,68 @@
 package org.kevoree.framework.deploy
 
-import org.kevoreeAdaptation.AdaptationModel
+import org.kevoreeAdaptation.{ParallelStep, AdaptationModel}
+import org.kevoree.framework.{PrimitiveCommand, AbstractNodeType}
+import scala.collection.JavaConversions._
+import org.slf4j.LoggerFactory
 
 /**
  * Created by IntelliJ IDEA.
  * User: duke
  * Date: 20/09/11
  * Time: 20:19
- * To change this template use File | Settings | File Templates.
  */
 
 trait PrimitiveCommandExecutionHelper {
 
-  def execute(adaptionModel:AdaptationModel) : Boolean = {
-    true
+  var logger = LoggerFactory.getLogger(this.getClass)
+
+  def execute(adaptionModel: AdaptationModel, nodeInstance: AbstractNodeType): Boolean = {
+    executeStep(adaptionModel.getOrderedPrimitiveSet, nodeInstance)
   }
+
+  private def executeStep(step: ParallelStep, nodeInstance: AbstractNodeType): Boolean = {
+    val phase = new KevoreeParDeployPhase
+    val populateResult = step.getAdaptations.forall {
+      adapt =>
+        val primitive = nodeInstance.getPrimitive(adapt)
+        if (primitive != null) {
+          logger.debug("Populate primitive => "+primitive)
+          phase.populate(primitive)
+          true
+        } else {
+          logger.debug("Error while searching primitive => "+adapt)
+          false
+        }
+    }
+    if (populateResult) {
+      phase.runPhase()
+      val subResult = executeStep(step.getNextStep, nodeInstance)
+      if(!subResult){
+        phase.rollBack()
+        false
+      } else {true}
+    } else {
+      logger.debug("Primitive mapping error")
+      false
+    }
+  }
+
+  class KevoreeParDeployPhase {
+    var primitives: List[PrimitiveCommand] = List()
+
+    def populate(cmd: PrimitiveCommand) {
+      primitives = primitives ++ List(cmd)
+    }
+
+    def runPhase(): Boolean = {
+      true
+    }
+
+    def rollBack(){
+
+    }
+
+  }
+
 
 }
