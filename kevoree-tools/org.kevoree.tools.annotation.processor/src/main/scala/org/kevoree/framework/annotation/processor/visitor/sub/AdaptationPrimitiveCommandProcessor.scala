086a5a359bc613094d8bc223b876bbc4cb31d679
Erwan Daubert <erwan.daubert@gmail.com>
2011-09-21 13:08:52 +0200

    add PrimitiveCommand annotation stuff
    Former-commit-id: cf7372d62adbddbf3341bcbffc893f32307e55ee

---
+++ kevoree-tools/org.kevoree.tools.annotation.processor/src/main/scala/org/kevoree/framework/annotation/processor/visitor/sub/AdaptationPrimitiveCommandProcessor.scala
@@ -0,0 +1,11 @@
+package org.kevoree.framework.annotation.processor.visitor.sub
+
+/**
+ * User: Erwan Daubert - erwan.daubert@gmail.com
+ * Date: 21/09/11
+ * Time: 09:30
+ */
+
+trait AdaptationPrimitiveCommandProcessor {
+
+}
