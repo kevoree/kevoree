8dda386fa172f3f3732272caee506f35b76fdfee
duke <fouquet.f@gmail.com>
2011-09-22 15:22:03 +0200

    fix editor & model
    Former-commit-id: 2509496c642df48625bb9ef1c40ec60a15101366

--- kevoree-tools/org.kevoree.tools.ui.editor/src/main/scala/org/kevoree/tools/ui/editor/command/UpdatePhysicalNode.scala
+++ kevoree-tools/org.kevoree.tools.ui.editor/src/main/scala/org/kevoree/tools/ui/editor/command/UpdatePhysicalNode.scala
@@ -35,45 +35,21 @@ class UpdatePhysicalNode extends Command {
     targetCNode = c
   }
 
-  def getCurrentPhysName: String = {
-    kernel.getModelHandler.getActualModel.getPhysicalNodes.find(physNode => physNode.getHosts.contains(targetCNode)) match {
-      case None => ""
-      case Some(physNode) => physNode.getName
-    }
-  }
-
-
   def execute(p: AnyRef) {
     p match {
-      case physNodeName: String if (physNodeName != null && physNodeName != "") => {
+      case physNodeName: String if (physNodeName != null && physNodeName.split(':').size == 2) => {
+
+        val hostNode = physNodeName.split(':')(0)
+        val hostType = physNodeName.split(':')(1)
 
         val model = kernel.getModelHandler.getActualModel
         //CLEAN PREVIOUS RELATIONSHIP
-        model.getPhysicalNodes.foreach {
-          physNode =>
-            if (physNode.getHosts.contains(targetCNode)) {
-              physNode.getHosts.remove(targetCNode)
-            }
-        }
-
-        val physNode = model.getPhysicalNodes.find(physNode => physNode.getName == physNodeName) match {
-          case Some(previousPhysNode) => previousPhysNode
-          case None => {
-            val newPhysNode = KevoreeFactory.eINSTANCE.createPhysicalNode()
-            newPhysNode.setName(physNodeName)
-            model.getPhysicalNodes.add(newPhysNode)
-            newPhysNode
+        model.getNodes.find(n => n.getName == hostNode && n.getTypeDefinition.getName == hostType) match {
+          case Some(host)=> {
+            if(host.getHosts.contains(targetCNode)){host.getHosts.clear()}
+            host.getHosts.add(targetCNode)
           }
-        }
-        if (!physNode.getHosts.contains(targetCNode)) {
-          physNode.getHosts.add(targetCNode)
-        }
-        //CLEAN UNUSED PHYSNODE
-        //WARNING COULD LEAD MAJOR PB
-        val toClean = model.getPhysicalNodes.filter(physNode => physNode.getHosts.isEmpty).toList ++ List() //FORCE CLONE
-        toClean.foreach {
-          toClean =>
-            model.getPhysicalNodes.remove(toClean)
+          case None => println("Error : Node host not linked => "+physNodeName)
         }
 
       }
