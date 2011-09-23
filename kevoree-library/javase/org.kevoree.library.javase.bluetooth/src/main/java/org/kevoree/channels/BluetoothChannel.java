f8a5f50fde4243e4f82d4f13b5d50ab1d512aaac
duke <fouquet.f@gmail.com>
2011-09-23 14:27:15 +0200

    fix ui for hosted nodes

---
+++ kevoree-library/javase/org.kevoree.library.javase.bluetooth/src/main/java/org/kevoree/channels/BluetoothChannel.java
@@ -0,0 +1,30 @@
+package org.kevoree.channels;
+
+import org.kevoree.annotation.ChannelTypeFragment;
+import org.kevoree.annotation.Library;
+import org.kevoree.framework.AbstractChannelFragment;
+import org.kevoree.framework.ChannelFragmentSender;
+import org.kevoree.framework.message.Message;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: duke
+ * Date: 23/09/11
+ * Time: 14:12
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Library(name = "Kevoree-Android-JavaSE")
+@ChannelTypeFragment
+public class BluetoothChannel extends AbstractChannelFragment {
+
+    @Override
+    public Object dispatch(Message message) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public ChannelFragmentSender createSender(String s, String s1) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
