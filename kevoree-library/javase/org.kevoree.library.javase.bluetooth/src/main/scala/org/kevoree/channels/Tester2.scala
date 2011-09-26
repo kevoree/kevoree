716c8cefd60104cf18f78fbd64d1f3c4fb0dba51
duke <fouquet.f@gmail.com>
2011-09-26 15:45:34 +0200

    change meta model inheritance

--- kevoree-library/javase/org.kevoree.library.javase.bluetooth/src/main/scala/org/kevoree/channels/Tester2.scala
+++ kevoree-library/javase/org.kevoree.library.javase.bluetooth/src/main/scala/org/kevoree/channels/Tester2.scala
@@ -1,5 +1,10 @@
 package org.kevoree.channels
 
+import javax.bluetooth.LocalDevice
+import com.intel.bluetooth.RemoteDeviceHelper
+import javax.microedition.io.{StreamConnection, Connector}
+import java.io.PrintStream
+
 /**
  * Created by IntelliJ IDEA.
  * User: duke
@@ -11,6 +16,20 @@ package org.kevoree.channels
 object Tester2 extends App {
 
   val discovery = new BluetoothDiscovery
-  println(discovery.getRemoteDevice("KevoreeSensor1"))
+
+  discovery.getRemoteDevice("KevoreeSensor1") match {
+    case Some(device)=> {
+
+      val conn = Connector.open("btspp://"+device.getBluetoothAddress+":1");
+
+      val sender = new PrintStream(conn.asInstanceOf[StreamConnection].openOutputStream())
+      sender.println("hello")
+
+      conn.close()
+
+    }
+    case None => println("Not found")
+  }
+
 
 }
