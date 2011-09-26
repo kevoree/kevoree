5b5d81df74fd1f264407c90ee87063f756891561
duke <fouquet.f@gmail.com>
2011-09-26 15:45:34 +0200

    change meta model inheritance
    Former-commit-id: 1bdd70cd22cf0d76a578d8c7c5bbb4abf9662974

---
+++ kevoree-tools/org.kevoree.tools.annotation.processor/src/main/scala/org/kevoree/framework/annotation/processor/visitor/sub/TypeDefinitionProcessor.scala
@@ -0,0 +1,15 @@
+package org.kevoree.framework.annotation.processor.visitor.sub
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: duke
+ * Date: 26/09/11
+ * Time: 15:29
+ * To change this template use File | Settings | File Templates.
+ */
+
+trait TypeDefinitionProcessor {
+
+  def defineAsSuperType
+
+}
