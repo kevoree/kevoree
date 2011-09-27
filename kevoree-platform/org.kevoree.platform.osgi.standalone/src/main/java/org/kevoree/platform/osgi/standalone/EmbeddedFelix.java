95eea9f3f9d37db3fedc4e7e6fb000754c54896f
Erwan Daubert <erwan.daubert@gmail.com>
2011-09-27 16:04:23 +0200

    put the felix cache on tmp dir
    Former-commit-id: f25f73e6785768b60ddef27af22ad7657270a373

--- kevoree-platform/org.kevoree.platform.osgi.standalone/src/main/java/org/kevoree/platform/osgi/standalone/EmbeddedFelix.java
+++ kevoree-platform/org.kevoree.platform.osgi.standalone/src/main/java/org/kevoree/platform/osgi/standalone/EmbeddedFelix.java
@@ -32,114 +32,127 @@ import java.util.Map;
 import java.util.Properties;
 
 /**
- *
  * @author ffouquet
  */
 public class EmbeddedFelix {
 
 	private static final Logger logger = LoggerFactory.getLogger(EmbeddedFelix.class);
 
-    private Felix m_fwk;
+	private Felix m_fwk;
 
-    public Felix getM_fwk() {
-        return m_fwk;
-    }
+	public Felix getM_fwk () {
+		return m_fwk;
+	}
 
-    public void run() {
+	public void run () {
 
-        String felix_base = System.getProperty("osgi.base");
-        if (felix_base == null) {
-            felix_base = ".";//this.getClass().getClassLoader().getResource(".").getPath();
-            logger.debug("Init Felix Default path => " + felix_base);
-        }
+		String felix_base = System.getProperty("osgi.base");
+		if (felix_base == null) {
+			File f = null;
+			try {
+				f = File.createTempFile("kevoreeTemp", "cache");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			if (f != null && f.exists()) {
+				f.delete();
+				f.mkdirs();
+				// TODO deleteOnExit
+				felix_base = f.getAbsolutePath();
+			} else {
+				felix_base = ".";//this.getClass().getClassLoader().getResource(".").getPath();
+			}
+			logger.debug("Init Felix Default path => " + felix_base);
+		}
 
-        String node_name = System.getProperty("node.name");
-        if (node_name == null || node_name.equals("")) {
-            node_name = "KEVOREEDefaultNodeName";
+		String node_name = System.getProperty("node.name");
+		if (node_name == null || node_name.equals("")) {
+			node_name = "KEVOREEDefaultNodeName";
 			System.setProperty("node.name", node_name);
-        }
+		}
 
-        File cacheDir = new File(felix_base + "/" + "felixCache_" + node_name);
-        Map<String, Object> configProps = new HashMap<String, Object>();
-        if (cacheDir.exists()) {
-         	cacheDir.mkdirs();
-        }
+		File cacheDir = new File(felix_base + "/" + "felixCache_" + node_name);
+		Map<String, Object> configProps = new HashMap<String, Object>();
+		if (cacheDir.exists()) {
+			cacheDir.mkdirs();
+		}
 		configProps.put(Constants.FRAMEWORK_STORAGE, cacheDir.getAbsolutePath());
 
 
-        /* Look for a free port for remote shell */
-        //int remoteShellPort = AvailablePortFinder.getNextAvailable();
+		/* Look for a free port for remote shell */
+		//int remoteShellPort = AvailablePortFinder.getNextAvailable();
 
-        //System.setProperty("osgi.shell.telnet.port", remoteShellPort + "");
-        //System.out.println("Remote Shell Port => " + remoteShellPort);
-
-
-        configProps.put(Constants.FRAMEWORK_SYSTEMPACKAGES_EXTRA, SysPackageConstants.getProperty() + ",sun.misc");
-        configProps.put(Constants.FRAMEWORK_STORAGE_CLEAN, Constants.FRAMEWORK_STORAGE_CLEAN_ONFIRSTINIT);
-        configProps.put(FelixConstants.SYSTEMBUNDLE_ACTIVATORS_PROP, EmbeddedActivators.getActivators());
-
-        Runtime.getRuntime().addShutdownHook(new Thread("Felix Shutdown Hook") {
-
-            public void run() {
-                try {
-                    logger.debug("Stopping OSGi Embedded Framework");
-                    if (m_fwk != null) {
-                        EmbeddedActivators.getBootstrapActivator().stop(m_fwk.getBundleContext());
-                        m_fwk.stop();
-                        m_fwk.waitForStop(0);
-                    }
-                } catch (Exception ex) {
-                    logger.warn("Error stopping framework: ", ex);
-                }
-            }
-        });
+		//System.setProperty("osgi.shell.telnet.port", remoteShellPort + "");
+		//System.out.println("Remote Shell Port => " + remoteShellPort);
 
 
-        try {
-            m_fwk = new Felix(configProps);
-            m_fwk.init();
-            // (10) Start the framework.
-            m_fwk.start();
+		configProps.put(Constants.FRAMEWORK_SYSTEMPACKAGES_EXTRA, SysPackageConstants.getProperty() + ",sun.misc");
+		configProps.put(Constants.FRAMEWORK_STORAGE_CLEAN, Constants.FRAMEWORK_STORAGE_CLEAN_ONFIRSTINIT);
+		configProps.put(FelixConstants.SYSTEMBUNDLE_ACTIVATORS_PROP, EmbeddedActivators.getActivators());
 
-            printWelcome();
+		Runtime.getRuntime().addShutdownHook(new Thread("Felix Shutdown Hook") {
 
-            logger.debug("Felix Embedded started");
-            // (11) Wait for framework to stop to exit the VM.
-            //m_fwk.waitForStop(0);
-            //System.exit(0)
-        } catch (Exception ex) {
+			public void run () {
+				try {
+					logger.debug("Stopping OSGi Embedded Framework");
+					if (m_fwk != null) {
+						EmbeddedActivators.getBootstrapActivator().stop(m_fwk.getBundleContext());
+						m_fwk.stop();
+						m_fwk.waitForStop(0);
+					}
+				} catch (Exception ex) {
+					logger.warn("Error stopping framework: ", ex);
+				}
+			}
+		});
+
+
+		try {
+			m_fwk = new Felix(configProps);
+			m_fwk.init();
+			// (10) Start the framework.
+			m_fwk.start();
+
+			printWelcome();
+
+			logger.debug("Felix Embedded started");
+			// (11) Wait for framework to stop to exit the VM.
+			//m_fwk.waitForStop(0);
+			//System.exit(0)
+		} catch (Exception ex) {
 //            System.err.println("Could not create framework: " + ex);
 //            ex.printStackTrace();
 			logger.error("Could not create framework: ", ex);
-            System.exit(0);
-        }
+			System.exit(0);
+		}
 
-    }
+	}
 
-    private void printWelcome() {
-        String welcomeMessage = "Kevoree OSGi Standalone";
+	private void printWelcome () {
+		String welcomeMessage = "Kevoree OSGi Standalone";
 
-        InputStream is = getClass().getResourceAsStream("/META-INF/maven/org.kevoree.platform/org.kevoree.platform.osgi.standalone/pom.properties");
-        if (is != null) {
-            try {
-                Properties p = new Properties();
+		InputStream is = getClass().getResourceAsStream(
+				"/META-INF/maven/org.kevoree.platform/org.kevoree.platform.osgi.standalone/pom.properties");
+		if (is != null) {
+			try {
+				Properties p = new Properties();
 
-                p.load(is);
+				p.load(is);
 
-                String version = p.getProperty("version");
-                if (version != null) {
-                    welcomeMessage += " - " + version;
-                }
+				String version = p.getProperty("version");
+				if (version != null) {
+					welcomeMessage += " - " + version;
+				}
 
-                is.close();
+				is.close();
 
-            } catch (IOException ex) {
-                logger.error(null, ex);
-            }
+			} catch (IOException ex) {
+				logger.error(null, ex);
+			}
 
-        }
+		}
 
-        System.out.println(welcomeMessage);
+		System.out.println(welcomeMessage);
 
-    }
+	}
 }
