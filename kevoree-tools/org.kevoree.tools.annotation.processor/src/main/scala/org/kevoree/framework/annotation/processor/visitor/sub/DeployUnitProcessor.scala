6ff4fdb22ede964d78c79089f81bacbba9f5a135
duke <fouquet.f@gmail.com>
2011-09-29 16:33:40 +0200

    fun group
    Former-commit-id: df190b5ae27763423ddc32712b6810c1d302cfb7

--- kevoree-tools/org.kevoree.tools.annotation.processor/src/main/scala/org/kevoree/framework/annotation/processor/visitor/sub/DeployUnitProcessor.scala
+++ kevoree-tools/org.kevoree.tools.annotation.processor/src/main/scala/org/kevoree/framework/annotation/processor/visitor/sub/DeployUnitProcessor.scala
@@ -20,93 +20,133 @@ package org.kevoree.framework.annotation.processor.visitor.sub
 
 import com.sun.mirror.apt.AnnotationProcessorEnvironment
 import com.sun.mirror.declaration.TypeDeclaration
-import org.kevoree.KevoreeFactory
-import org.kevoree.ContainerRoot
-import org.kevoree.NodeType
-import org.kevoree.TypeDefinition
 import scala.collection.JavaConversions._
+import org.kevoree._
 
 trait DeployUnitProcessor {
 
-  def processDeployUnit(typeDef : TypeDefinition,classdef : TypeDeclaration,env : AnnotationProcessorEnvironment)={
-    val root : ContainerRoot = typeDef.eContainer.asInstanceOf[ContainerRoot]
+  def processDeployUnit(typeDef: TypeDefinition, classdef: TypeDeclaration, env: AnnotationProcessorEnvironment) = {
+    val root: ContainerRoot = typeDef.eContainer.asInstanceOf[ContainerRoot]
 
     /* CREATE COMPONENT TYPE DEPLOY UNIT IF NEEDED */
-    val unitName = env.getOptions.find({op => op._1.contains("kevoree.lib.id")}).getOrElse{("key=","")}._1.split('=').toList.get(1)
-    val groupName = env.getOptions.find({op => op._1.contains("kevoree.lib.group")}).getOrElse{("key=","")}._1.split('=').toList.get(1)
-    val version = env.getOptions.find({op => op._1.contains("kevoree.lib.version")}).getOrElse{("key=","")}._1.split('=').toList.get(1)
-    val tag = env.getOptions.find({op => op._1.contains("kevoree.lib.tag")}).getOrElse{("key=","")}._1.split('=').toList.get(1)
+    val unitName = env.getOptions.find({
+      op => op._1.contains("kevoree.lib.id")
+    }).getOrElse {
+      ("key=", "")
+    }._1.split('=').toList.get(1)
+    val groupName = env.getOptions.find({
+      op => op._1.contains("kevoree.lib.group")
+    }).getOrElse {
+      ("key=", "")
+    }._1.split('=').toList.get(1)
+    val version = env.getOptions.find({
+      op => op._1.contains("kevoree.lib.version")
+    }).getOrElse {
+      ("key=", "")
+    }._1.split('=').toList.get(1)
+    val tag = env.getOptions.find({
+      op => op._1.contains("kevoree.lib.tag")
+    }).getOrElse {
+      ("key=", "")
+    }._1.split('=').toList.get(1)
 
-    val repositories = env.getOptions.find({op => op._1.contains("repositories")}).getOrElse{("key=","")}._1.split('=').toList.get(1)
-    val repositoriesList : List[String] = repositories.split(";").filter(r=> r != null && r != "").toList
+    val repositories = env.getOptions.find({
+      op => op._1.contains("repositories")
+    }).getOrElse {
+      ("key=", "")
+    }._1.split('=').toList.get(1)
+    val repositoriesList: List[String] = repositories.split(";").filter(r => r != null && r != "").toList
 
-    val tRepositories = env.getOptions.find({op => op._1.contains("otherRepositories")}).getOrElse{("key=","")}._1.split('=').toList.get(1)
-    val tRepositoriesList : List[String] = tRepositories.split(";").filter(r=> r != null && r != "").toList
+    val tRepositories = env.getOptions.find({
+      op => op._1.contains("otherRepositories")
+    }).getOrElse {
+      ("key=", "")
+    }._1.split('=').toList.get(1)
+    val tRepositoriesList: List[String] = tRepositories.split(";").filter(r => r != null && r != "").toList
 
-    val nodeTypeNames = env.getOptions.find({op => op._1.contains("nodeTypeNames")}).getOrElse{("key=","")}._1.split('=').toList.get(1)
-    val nodeTypeNameList : List[String] = nodeTypeNames.split(";").filter(r=> r != null && r != "").toList
+    val nodeTypeNames = env.getOptions.find({
+      op => op._1.contains("nodeTypeNames")
+    }).getOrElse {
+      ("key=", "")
+    }._1.split('=').toList.get(1)
+    val nodeTypeNamesS: List[String] = nodeTypeNames.split(",").filter(r => r != null && r != "").toList
 
-    val ctdeployunit = root.getDeployUnits.find({du => du.getUnitName == unitName && du.getGroupName == groupName && du.getVersion == version }) match {
-      case None => {
-          val newdeploy = KevoreeFactory.eINSTANCE.createDeployUnit
-          newdeploy.setUnitName(unitName)
-          newdeploy.setGroupName(groupName)
-          newdeploy.setVersion(version)
-          newdeploy.setHashcode(tag)
-          
-          /* ROOT ADD NODE TYPE IF NECESSARY */
-          nodeTypeNameList.foreach{nodeTypeName =>
-            root.getTypeDefinitions.filter(p=> p.isInstanceOf[NodeType]).find(nt => nt.getName == nodeTypeName) match {
-              case Some(existingNodeType)=>newdeploy.setTargetNodeType(existingNodeType.asInstanceOf[NodeType])
+    var deployUnits : List[DeployUnit] = List()
+    nodeTypeNamesS.foreach {
+      nodeTypeName =>
+
+        val ctdeployunit = root.getDeployUnits.find({
+          du => du.getUnitName == unitName && du.getGroupName == groupName && du.getVersion == version && du.getTargetNodeType.getName == nodeTypeName
+        }) match {
+          case None => {
+            val newdeploy = KevoreeFactory.eINSTANCE.createDeployUnit
+            newdeploy.setUnitName(unitName)
+            newdeploy.setGroupName(groupName)
+            newdeploy.setVersion(version)
+            newdeploy.setHashcode(tag)
+
+            /* ROOT ADD NODE TYPE IF NECESSARY */
+            //nodeTypeNameList.foreach {
+            //nodeTypeName =>
+            root.getTypeDefinitions.filter(p => p.isInstanceOf[NodeType]).find(nt => nt.getName == nodeTypeName) match {
+              case Some(existingNodeType) => newdeploy.setTargetNodeType(existingNodeType.asInstanceOf[NodeType])
               case None => {
-                  val nodeType = KevoreeFactory.eINSTANCE.createNodeType
-                  nodeType.setName(nodeTypeName)
-                  root.getTypeDefinitions.add(nodeType)
-                  newdeploy.setTargetNodeType(nodeType)
+                val nodeType = KevoreeFactory.eINSTANCE.createNodeType
+                nodeType.setName(nodeTypeName)
+                root.getTypeDefinitions.add(nodeType)
+                newdeploy.setTargetNodeType(nodeType)
               }
             }
-          }
+            // }
 
-          root.getDeployUnits.add(newdeploy)
-          newdeploy
+            root.getDeployUnits.add(newdeploy)
+            deployUnits = deployUnits ++ List(newdeploy)
+            newdeploy
+          }
+          case Some(fdu) => fdu.setHashcode(tag); fdu
         }
-      case Some(fdu)=> fdu.setHashcode(tag);fdu
+        typeDef.getDeployUnits.add(ctdeployunit)
+
+
     }
-    typeDef.getDeployUnits.add(ctdeployunit)
+
+
+
 
     /* ADD DEPLOY UNIT to RepositoryList */
-    repositoriesList.foreach{repoUrl=>
-      if(repoUrl != ""){
-        val repo = root.getRepositories.find(r => r.getUrl == repoUrl) match {
-          case None => {
+    repositoriesList.foreach {
+      repoUrl =>
+        if (repoUrl != "") {
+          val repo = root.getRepositories.find(r => r.getUrl == repoUrl) match {
+            case None => {
               val newrepo = KevoreeFactory.eINSTANCE.createRepository
               newrepo.setUrl(repoUrl)
               root.getRepositories.add(newrepo)
               newrepo
             }
-          case Some(e)=> e
+            case Some(e) => e
+          }
+          repo.getUnits.addAll(deployUnits)
         }
-        repo.getUnits.add(ctdeployunit)
-      }
     }
-    
-    tRepositoriesList.foreach{rRepoUrl=>
-      if(rRepoUrl != ""){
-        root.getRepositories.find(r => r.getUrl == rRepoUrl) match {
-          case None => {
+
+    tRepositoriesList.foreach {
+      rRepoUrl =>
+        if (rRepoUrl != "") {
+          root.getRepositories.find(r => r.getUrl == rRepoUrl) match {
+            case None => {
               val newrepo = KevoreeFactory.eINSTANCE.createRepository
               newrepo.setUrl(rRepoUrl)
               root.getRepositories.add(newrepo)
               newrepo
             }
-          case Some(e)=>
+            case Some(e) =>
+          }
         }
-      }
     }
 
 
   }
-
 
 
 }
