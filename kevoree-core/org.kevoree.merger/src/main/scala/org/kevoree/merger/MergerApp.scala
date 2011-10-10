d0fd8d19fe71a0db6a5a2216882c651ff7cd0222
duke <fouquet.f@gmail.com>
2011-10-10 14:27:55 +0200

    fix go to 1.4.0-SNAPSHOT
    Former-commit-id: 8cc3e509d6b8b7c8b33eba5b77200554ad1d9d15

--- kevoree-core/org.kevoree.merger/src/main/scala/org/kevoree/merger/MergerApp.scala
+++ kevoree-core/org.kevoree.merger/src/main/scala/org/kevoree/merger/MergerApp.scala
@@ -30,21 +30,30 @@ object MergerApp {
 
     val merger = new RootMerger
     val emtpyModel = KevoreeFactory.eINSTANCE.createContainerRoot
+
+    val model1 = KevoreeXmiHelper.load("/Users/duke/Desktop/oldlib.kev")
+    val model2 = KevoreeXmiHelper.load("/Users/duke/Desktop/newlib.kev")
+
+
+    /*
     val model1 = KevoreeXmiHelper.load("/Users/duke/Documents/dev/dukeboard/kevoree/kevoree-library/javase/org.kevoree.library.javase.restNode/target/classes/KEV-INF/lib.kev")
     val model2 = KevoreeXmiHelper.load("/Users/duke/Documents/dev/dukeboard/kevoree/kevoree-library/javase/org.kevoree.library.javase.javaseNode/target/classes/KEV-INF/lib.kev")
     val model3 = KevoreeXmiHelper.load("/Users/duke/Documents/dev/dukeboard/kevoree/kevoree-library/javase/org.kevoree.library.javase.fakeDomo/target/generated-sources/kevoree/KEV-INF/lib.kev")
     val model4 = KevoreeXmiHelper.load("/Users/duke/Documents/dev/dukeboard/kevoree/kevoree-library/javase/org.kevoree.library.javase.fakeDomo/target/generated-sources/kevoree/KEV-INF/lib2.kev")
-
+     */
 
     println("%%%%%%%%%%%%%%%%%%%%%%%%%% 1")
     merger.merge(emtpyModel, model1)
     println("%%%%%%%%%%%%%%%%%%%%%%%%%% 2")
     merger.merge(emtpyModel, model2)
     println("%%%%%%%%%%%%%%%%%%%%%%%%%% 3")
-    merger.merge(emtpyModel, model3)
+    //merger.merge(emtpyModel, model3)
     println("%%%%%%%%%%%%%%%%%%%%%%%%%% 4")
-    merger.merge(emtpyModel, model4)
+   // merger.merge(emtpyModel, model4)
     println("!!!!!!!!!!!!!!!!!!!!!!!!!! end")
+
+
+
     println(KevoreeXmiHelper.saveToString(emtpyModel,true))
 
 
