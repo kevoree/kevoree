197e0e3be9522356d65d0dd86ed5f80c0be9238f
duke <fouquet.f@gmail.com>
2011-10-14 14:33:25 +0200

    fix web server

--- kevoree-library/javase/org.kevoree.library.javase.webserver/src/main/scala/org/kevoree/library/javase/webserver/ServerBootstrap.scala
+++ kevoree-library/javase/org.kevoree.library.javase.webserver/src/main/scala/org/kevoree/library/javase/webserver/ServerBootstrap.scala
@@ -4,20 +4,19 @@ import akka.config.Supervision._
 import akka.config.Supervision.{SupervisorConfig, Permanent}
 import akka.actor._
 import cc.spray.can.{ServerConfig, HttpServer}
+import org.kevoree.framework.MessagePort
 
-class ServerBootstrap {
+class ServerBootstrap(request : MessagePort) {
 
   var rootService : RootService = _
 
   def startServer(port : Int){
-
-    var config = ServerConfig("0.0.0.0",port)
-
+    val config = ServerConfig("0.0.0.0",port)
     Supervisor(
         SupervisorConfig(
           OneForOneStrategy(List(classOf[Exception]), 3, 100),
           List(
-            Supervise(Actor.actorOf(new RootService("spray-root-service")), Permanent),
+            Supervise(Actor.actorOf(new RootService("spray-root-service",request,this,config.timeoutTimeout)), Permanent),
             Supervise(Actor.actorOf(new HttpServer(config)), Permanent)
           )
         )
@@ -25,7 +24,20 @@ class ServerBootstrap {
   }
 
   def stop(){
+    responseActor ! PoisonPill
     Actor.registry.actors.foreach(_ ! PoisonPill)
+  }
+
+  private var responseActor : akka.actor.ActorRef = _
+  
+  def setResponseActor(a : akka.actor.ActorRef) {
+    responseActor = a
+  }
+  
+  def responseHandler(param : AnyRef){
+    if (responseActor != null ){
+      responseActor ! param
+    }
   }
 
 }
