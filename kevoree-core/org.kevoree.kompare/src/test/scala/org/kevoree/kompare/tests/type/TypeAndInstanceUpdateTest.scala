9d40d9377285f48188bb2a6a247937166e0a2f18
Erwan Daubert <erwan.daubert@gmail.com>
2011-10-14 16:56:34 +0200

    fix kompare test
    Former-commit-id: 6d9be805ce8a4d4509e707c6ce7e77218cd78e0b

--- kevoree-core/org.kevoree.kompare/src/test/scala/org/kevoree/kompare/tests/type/TypeAndInstanceUpdateTest.scala
+++ kevoree-core/org.kevoree.kompare/src/test/scala/org/kevoree/kompare/tests/type/TypeAndInstanceUpdateTest.scala
@@ -34,18 +34,35 @@ class TypeAndInstanceUpdateTest extends KompareSuite {
   @Test def testNoTypeInstall() = {
     val kompareModel = component.kompare(model("test_type/updateBaseModel.art2")/*.setLowerHashCode*/, model("test_type/update2ComponentOneBindingRemove.art2"), "duke")
 
-    kompareModel.print
+//    kompareModel.print
 
-    println(kompareModel.getAdaptations.size)
+//    println(kompareModel.getAdaptations.size)
 
-    kompareModel shouldContainSize (JavaSePrimitive.UpdateDeployUnit, 1)
-    kompareModel shouldContain (JavaSePrimitive.UpdateType, "ComponentPrimitiveTypeService")
-    kompareModel shouldContain (JavaSePrimitive.UpdateType, "ComponentA")
+//    kompareModel shouldContainSize (JavaSePrimitive.UpdateDeployUnit, 1)
+    kompareModel shouldContainSize (JavaSePrimitive.RemoveDeployUnit, 1)
+    kompareModel shouldContainSize (JavaSePrimitive.AddDeployUnit, 1)
+//    kompareModel shouldContain (JavaSePrimitive.UpdateType, "ComponentPrimitiveTypeService")
+    kompareModel shouldContain (JavaSePrimitive.RemoveType, "ComponentPrimitiveTypeService")
+    kompareModel shouldContain (JavaSePrimitive.AddType, "ComponentPrimitiveTypeService")
+//    kompareModel shouldContain (JavaSePrimitive.UpdateType, "ComponentA")
+    kompareModel shouldContain (JavaSePrimitive.RemoveType, "ComponentA")
+    kompareModel shouldContain (JavaSePrimitive.AddType, "ComponentA")
 
-    kompareModel shouldContain (JavaSePrimitive.UpdateInstance, "ComponentPrimitiveTypeService-193784848")
-    kompareModel shouldContain (JavaSePrimitive.UpdateInstance, "ComponentA-1649555745")
-    kompareModel shouldContainSize (JavaSePrimitive.UpdateBinding, 2)
-    kompareModel shouldContainSize (JavaSePrimitive.RemoveBinding, 1)
+//    kompareModel shouldContain (JavaSePrimitive.UpdateInstance, "ComponentPrimitiveTypeService-193784848")
+    kompareModel shouldContain (JavaSePrimitive.StopInstance, "ComponentPrimitiveTypeService-193784848")
+    kompareModel shouldContain (JavaSePrimitive.RemoveInstance, "ComponentPrimitiveTypeService-193784848")
+    kompareModel shouldContain (JavaSePrimitive.AddInstance, "ComponentPrimitiveTypeService-193784848")
+    kompareModel shouldContain (JavaSePrimitive.UpdateDictionaryInstance, "ComponentPrimitiveTypeService-193784848")
+    kompareModel shouldContain (JavaSePrimitive.AddInstance, "ComponentPrimitiveTypeService-193784848")
+//    kompareModel shouldContain (JavaSePrimitive.UpdateInstance, "ComponentA-1649555745")
+    kompareModel shouldContain (JavaSePrimitive.StopInstance, "ComponentA-1649555745")
+    kompareModel shouldContain (JavaSePrimitive.RemoveInstance, "ComponentA-1649555745")
+    kompareModel shouldContain (JavaSePrimitive.AddInstance, "ComponentA-1649555745")
+    kompareModel shouldContain (JavaSePrimitive.UpdateDictionaryInstance, "ComponentA-1649555745")
+    kompareModel shouldContain (JavaSePrimitive.AddInstance, "ComponentA-1649555745")
+
+    kompareModel shouldContainSize (JavaSePrimitive.AddBinding, 2)
+    kompareModel shouldContainSize (JavaSePrimitive.RemoveBinding, 3)
 
     //kompareModel.verifySize(8)
 
