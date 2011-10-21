940054fcf1d119bb8bbca0b58beaca09e4751dfe
Erwan Daubert <erwan.daubert@gmail.com>
2011-10-21 15:16:15 +0200

    fix log and javase dependencies

--- kevoree-library/arduino/org.kevoree.library.arduino.nodeType/src/main/java/org/kevoree/library/arduinoNodeType/ArduinoDeploy.java
+++ kevoree-library/arduino/org.kevoree.library.arduino.nodeType/src/main/java/org/kevoree/library/arduinoNodeType/ArduinoDeploy.java
@@ -20,70 +20,73 @@ import java.util.List;
 public class ArduinoDeploy {
 	private static final Logger logger = LoggerFactory.getLogger(ArduinoDeploy.class);
 
-    protected ArrayList<String> baseCmd;
-    /**
-     * @parameter
-     */
-    private String baudrate;
+	protected ArrayList<String> baseCmd;
+	/**
+	 * @parameter
+	 */
+	private String baudrate;
 
-    public void prepareCommands() {
+	public void prepareCommands () {
 
-        /*
-        String binPrefix = System.getProperty("arduino.home") + "/hardware/tools/avr/bin";
-        if (binPrefix != null && !binPrefix.endsWith(File.separator)) {
-            binPrefix += File.separator;
-        } */
+		/*
+				String binPrefix = System.getProperty("arduino.home") + "/hardware/tools/avr/bin";
+				if (binPrefix != null && !binPrefix.endsWith(File.separator)) {
+					binPrefix += File.separator;
+				} */
 
-        baseCmd = new ArrayList<String>();
-        baseCmd.add(ArduinoToolChainExecutables.getAVRDUDE());
-        String confPath = /*System.getProperty("arduino.home")*/ ArduinoResourceHelper.getArduinoHome()+ "/hardware/tools/avr/etc/avrdude.conf";
-        if (confPath != null) {
-            baseCmd.add("-C");
-            baseCmd.add(confPath);
-        }
-    }
+		baseCmd = new ArrayList<String>();
+		baseCmd.add(ArduinoToolChainExecutables.getAVRDUDE());
+		String confPath = /*System.getProperty("arduino.home")*/
+				ArduinoResourceHelper.getArduinoHome() + File.separator + "hardware" + File.separator + "tools"
+						+ File.separator + "avr" + File.separator + "etc" + File.separator + "avrdude.conf";
+		if (!confPath.startsWith(File.separator + "hardware")) {
+			baseCmd.add("-C");
+			baseCmd.add(confPath);
+		}
+	}
 
-    public final void uploadSketch(Sketch sketch, Target target, String portName) {
+	public final void uploadSketch (Sketch sketch, Target target, String portName) {
 
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {}
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+		}
 
-        File hexFile = sketch.getFlash(target);
-        if (hexFile == null) {
-            logger.warn("No upload data found");
-            throw new IllegalStateException();
-        }
+		File hexFile = sketch.getFlash(target);
+		if (hexFile == null) {
+			logger.warn("No upload data found");
+			throw new IllegalStateException();
+		}
 
-        ArrayList<String> cmd = new ArrayList<String>();
-        cmd.addAll(baseCmd);
-        cmd.add("-q");
-        cmd.add("-q");
-        cmd.add("-c");
+		ArrayList<String> cmd = new ArrayList<String>();
+		cmd.addAll(baseCmd);
+		cmd.add("-q");
+		cmd.add("-q");
+		cmd.add("-c");
 
-        if(target.getKey().equals("mega2560")){
-           cmd.add("stk500v2");
-        } else {
-            cmd.add("stk500v1");
-        }
+		if (target.getKey().equals("mega2560")) {
+			cmd.add("stk500v2");
+		} else {
+			cmd.add("stk500v1");
+		}
 
-        cmd.add("-p");
-        cmd.add(target.getMCU());
-        cmd.add("-P");
-        cmd.add(portName);
-        cmd.add("-b");
-        cmd.add(baudrate != null ? baudrate : target.getUploadSpeed());
-        cmd.add("-D");
-        cmd.add("-Uflash:w:" + hexFile.getPath() + ":i");
-        execute(cmd);
-    }
+		cmd.add("-p");
+		cmd.add(target.getMCU());
+		cmd.add("-P");
+		cmd.add(portName);
+		cmd.add("-b");
+		cmd.add(baudrate != null ? baudrate : target.getUploadSpeed());
+		cmd.add("-D");
+		cmd.add("-Uflash:w:" + hexFile.getPath() + ":i");
+		execute(cmd);
+	}
 
-    protected final int execute(List<String> cmds) {
-        String finalCommand = "";
-        for (String cmd : cmds) {
-            finalCommand = finalCommand + cmd + " ";
-        }
-        ArduinoCommandExec.execute(finalCommand);
-        return 0;
-    }
+	protected final int execute (List<String> cmds) {
+		String finalCommand = "";
+		for (String cmd : cmds) {
+			finalCommand = finalCommand + cmd + " ";
+		}
+		ArduinoCommandExec.execute(finalCommand);
+		return 0;
+	}
 }
