ddb038a2fa8492b74dfa621a88ca8da02417ea7e
duke <fouquet.f@gmail.com>
2011-10-27 09:14:05 +0200

    improve editor
    Former-commit-id: d6fe0713778d20602c3077b3bf769327846e1a1f

--- kevoree-tools/org.kevoree.tools.ui.editor/src/main/scala/org/kevoree/tools/ui/editor/KevoreeTypeEditorSourceList.scala
+++ kevoree-tools/org.kevoree.tools.ui.editor/src/main/scala/org/kevoree/tools/ui/editor/KevoreeTypeEditorSourceList.scala
@@ -1,5 +1,9 @@
 package org.kevoree.tools.ui.editor
 
+import javax.swing.JSplitPane
+import org.kevoree.TypeDefinition
+import com.explodingpixels.macwidgets._
+
 /**
  * Created by IntelliJ IDEA.
  * User: duke
@@ -8,6 +12,45 @@ package org.kevoree.tools.ui.editor
  * To change this template use File | Settings | File Templates.
  */
 
-class KevoreeTypeEditorSourceList {
+class KevoreeTypeEditorSourceList(pane: JSplitPane, kernel: KevoreeUIKernel, typeDef: TypeDefinition) {
+
+  val model = new SourceListModel()
+  val sourceList = new SourceList(model)
+
+  def getComponent = {
+    sourceList.getComponent
+  }
+
+
+  var controlBar = new SourceListControlBar();
+  sourceList.installSourceListControlBar(controlBar)
+  controlBar.installDraggableWidgetOnSplitPane(pane)
+  sourceList.setColorScheme(new SourceListDarkColorScheme());
+  sourceList.useIAppStyleScrollBars()
+
+  def refresh() : Unit = {
+    val categoryDic = new SourceListCategory("Dictionary") {
+      var itemCount = 0
+
+      def setItemCount(i: Int) {
+        itemCount = i
+      }
+
+      override def getItemCount: Int = itemCount
+    }
+    model.addCategory(categoryDic)
+    typeDef.getDictionaryType.map {
+      dic =>
+        dic.getAttributes.foreach {
+          dicAtt =>
+            val item = new SourceListItem(dicAtt.getName)
+            model.addItemToCategory(item, categoryDic);
+            categoryDic.setItemCount(categoryDic.getItemCount + 1)
+        }
+    }
+
+
+  }
+
 
 }
