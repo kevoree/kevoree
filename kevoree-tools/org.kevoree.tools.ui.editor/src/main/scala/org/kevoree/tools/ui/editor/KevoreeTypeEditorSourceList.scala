8da7fa75a64e4dc42c7c6ea6119c1e2d9be3327a
duke <fouquet.f@gmail.com>
2011-10-27 08:48:29 +0200

    fix editor

---
+++ kevoree-tools/org.kevoree.tools.ui.editor/src/main/scala/org/kevoree/tools/ui/editor/KevoreeTypeEditorSourceList.scala
@@ -0,0 +1,13 @@
+package org.kevoree.tools.ui.editor
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: duke
+ * Date: 27/10/11
+ * Time: 08:48
+ * To change this template use File | Settings | File Templates.
+ */
+
+class KevoreeTypeEditorSourceList {
+
+}
