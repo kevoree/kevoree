12111c424430f312afb9404e311b895665a20fdc
Grégory NAIN <gregory.nain@gmail.com>
2011-10-27 15:27:52 +0200

    Adding archetype projects

---
+++ kevoree-library/archetypes/org.kevoree.library.archetype.javase.helloworld/src/main/resources/archetype-resources/src/main/java/org/kevoree/library/javase/helloworld/HelloProductionListener.java
@@ -0,0 +1,13 @@
+package org.kevoree.library.javase.helloworld.${package};
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: gnain
+ * Date: 27/10/11
+ * Time: 14:36
+ * To change this template use File | Settings | File Templates.
+ */
+public interface HelloProductionListener {
+
+    public void helloProduced(String helloValue);
+}
