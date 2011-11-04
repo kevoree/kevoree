b873173ecb7dae26e15df93c054b8797ff61ea2b
duke <fouquet.f@gmail.com>
2011-11-04 16:49:06 +0100

    kinect

--- kevoree-library/javase/org.kevoree.library.javase.kinect/src/main/java/org/kevoree/library/javase/kinect/Kinect.java
+++ kevoree-library/javase/org.kevoree.library.javase.kinect/src/main/java/org/kevoree/library/javase/kinect/Kinect.java
@@ -57,18 +57,12 @@ public class Kinect extends AbstractComponentType {
 
 		if (instance == null) {
             String path = KinectNativeLibraryLoader.configure();
-			NativeLibrary.addSearchPath("freenect", path);
-            /*if(KinectNativeLibraryLoader.isMac()){
-                
-                for(Object key : System.getProperties().keySet()){
-                   System.out.println(key+"=>"+System.getProperty(key.toString()));
-                }
-                
-
-                System.setProperty("DYLD_LIBRARY_PATH",path);
+            if(KinectNativeLibraryLoader.isMac()){
+                logger.debug("OSX load usb lib");
                 NativeLibrary.addSearchPath("usb", path);
-            }*/
-
+                NativeLibrary.getInstance("usb");
+            }
+            NativeLibrary.addSearchPath("freenect", path);
 			instance = NativeLibrary.getInstance("freenect");
 			nbComponent++;
 			Native.register(Freenect.class, instance);
