07a0d3e77dbc7c30acbe7968c07f5a5d5d021e6c
Erwan Daubert <erwan.daubert@gmail.com>
2011-11-07 18:29:25 +0100

    update socket channel

--- kevoree-library/javase/org.kevoree.library.javase.socketChannels/src/main/java/org/kevoree/library/socketChannel/SocketMessage.java
+++ kevoree-library/javase/org.kevoree.library.javase.socketChannels/src/main/java/org/kevoree/library/socketChannel/SocketMessage.java
@@ -1,6 +1,12 @@
 package org.kevoree.library.socketChannel;
 
 
+import org.kevoree.framework.message.Message;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.List;
+
 /**
  * Created by IntelliJ IDEA.
  * User: jed
@@ -8,21 +14,37 @@ package org.kevoree.library.socketChannel;
  * Time: 15:41
  * To change this template use File | Settings | File Templates.
  */
-public class SocketMessage extends org.kevoree.framework.message.Message{
+public class SocketMessage extends Message implements Serializable {
 
-    private   String uuid;
+	private String uuid;
 
-    public String getUuid(){
+	public String getUuid () {
 
-        return uuid;
-    }
+		return uuid;
+	}
 
-    public void setUuid(String uuid){
+	public void setUuid (String uuid) {
 
-        this.uuid =uuid;
-    }
+		this.uuid = uuid;
+	}
 
 
+	private void writeObject (java.io.ObjectOutputStream out) throws IOException {
+		out.writeObject(getContent());
+		out.writeObject(getPassedNodes());
+		out.writeBoolean(getInOut());
+		out.writeLong(getTimeout());
+		out.writeBytes(getDestNodeName());
+
+	}
+
+	private void readObject (java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+		setContent(in.readObject());
+		setPassedNodes(((List <String>) in.readObject()));
+		setInOut(in.readBoolean());
+		setTimeout(in.readLong());
+		setDestNodeName(in.readUTF());
+	}
 
 
 }
