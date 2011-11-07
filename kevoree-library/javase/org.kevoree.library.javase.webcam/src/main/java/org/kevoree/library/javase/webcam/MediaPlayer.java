d29eb54736edcc437728635b979ff203a8481c5c
Erwan Daubert <erwan.daubert@gmail.com>
2011-11-07 17:39:11 +0100

    add some port on audio and media player

--- kevoree-library/javase/org.kevoree.library.javase.webcam/src/main/java/org/kevoree/library/javase/webcam/MediaPlayer.java
+++ kevoree-library/javase/org.kevoree.library.javase.webcam/src/main/java/org/kevoree/library/javase/webcam/MediaPlayer.java
@@ -1,37 +1,24 @@
 package org.kevoree.library.javase.webcam;
 
 import org.kevoree.annotation.*;
-import org.kevoree.extra.vlcj.VLCNativeLibraryLoader;
-import org.kevoree.framework.AbstractComponentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import uk.co.caprica.vlcj.binding.LibVlc;
-import uk.co.caprica.vlcj.player.MediaPlayerFactory;
 import uk.co.caprica.vlcj.player.embedded.DefaultFullScreenStrategy;
 import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
-import uk.co.caprica.vlcj.player.embedded.FullScreenStrategy;
-import uk.co.caprica.vlcj.player.embedded.videosurface.CanvasVideoSurface;
-import uk.co.caprica.vlcj.player.embedded.videosurface.VideoSurfaceAdapter;
-import uk.co.caprica.vlcj.player.embedded.videosurface.mac.MacVideoSurfaceAdapter;
-import uk.co.caprica.vlcj.player.headless.HeadlessMediaPlayer;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Arrays;
 
 /**
  * Created by IntelliJ IDEA.
  * User: duke
  * Date: 06/11/11
  * Time: 21:06
- * To change this template use File | Settings | File Templates.
+ *
  */
-@Provides({
-        @ProvidedPort(name = "media", type = PortType.MESSAGE)
-})
 @Library(name = "JavaSE")
 @ComponentType
-public class MediaPlayer extends AbstractComponentType {
+public class MediaPlayer extends Player {
 
     private JFrame frame;
     private EmbeddedMediaPlayer mediaPlayer;
@@ -40,7 +27,7 @@ public class MediaPlayer extends AbstractComponentType {
     @Start
     public void start() throws Exception {
         frame = new JFrame("Kevoree Frame");
-        DefaultFullScreenStrategy full = new DefaultFullScreenStrategy(frame);
+//        DefaultFullScreenStrategy full = new DefaultFullScreenStrategy(frame);
         mediaPlayer = MediaPlayerHelper.getInstance().getFactory(this.getName()).newEmbeddedMediaPlayer(new DefaultFullScreenStrategy(frame));
         Canvas c = new Canvas();
         c.setBackground(Color.black);
@@ -65,19 +52,6 @@ public class MediaPlayer extends AbstractComponentType {
     @Update
     public void update() throws Exception {
 
-    }
-
-    @Port(name = "media")
-    public void triggerMedia(final Object o) {
-        if (o != null) {
-            SwingUtilities.invokeLater(new Runnable() {
-                @Override
-                public void run() {
-                    logger.debug("Run media mrl=" + o.toString());
-                    mediaPlayer.playMedia(o.toString());
-                }
-            });
-        }
     }
 
 }
