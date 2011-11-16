6acf7872c10e051399a5be5782cd493ac642436d
duke <fouquet.f@gmail.com>
2011-11-16 11:23:35 +0100

    todo fix broken pipe refactore socket channel

--- kevoree-library/javase/org.kevoree.library.javase.socketChannels/src/main/java/org/kevoree/library/socketChannel/DeadMessageQueueThread.java
+++ kevoree-library/javase/org.kevoree.library.javase.socketChannels/src/main/java/org/kevoree/library/socketChannel/DeadMessageQueueThread.java
@@ -10,6 +10,8 @@ import java.net.Socket;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Semaphore;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,90 +23,95 @@ import java.util.concurrent.BlockingQueue;
 public class DeadMessageQueueThread extends Thread {
 
     private boolean alive = true;
-
     private SocketChannel parentChannel;
+
+    public void setTimer(Integer timer) {
+        this.timer = timer;
+    }
+
+    private Integer timer;
+    private BlockingQueue<Message> queue_node_dead = new LinkedBlockingQueue<Message>();
+    private Integer maximum_deadqueue_size;
+    private final Semaphore deadMessageSemaphore = new java.util.concurrent.Semaphore(1);
 
     public void stopProcess() {
         alive = false;
+        queue_node_dead.clear();
     }
 
-    private BlockingQueue<Message> queue_node_dead = null;
+    public void addToDeadQueue(Message m) {
 
-    public void addToDeadQueue(Message m ){
+        if (queue_node_dead.size() < maximum_deadqueue_size) {
+            if ("true".equals(parentChannel.getDictionary().get("replay"))) {
+                queue_node_dead.add(m);
+                deadMessageSemaphore.release();
+            }
+
+        } else {
+            logger.error("The maximum number of message buffer is reached");
+        }
+
+
         queue_node_dead.add(m);
     }
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public DeadMessageQueueThread(SocketChannel parent){
-        parentChannel =   parent;
+    public DeadMessageQueueThread(SocketChannel parent, Integer _timer, Integer _maximum_deadqueue_size) {
+        parentChannel = parent;
+        timer = _timer;
+        maximum_deadqueue_size = _maximum_deadqueue_size;
     }
 
 
     public void run() {
         Message current = null;
-        Socket client_consumer = null;
         int size, i;
-        Queue<Message> queue_tmp;
+        Queue<Message> stepFailMsgQueue;
         while (alive) {
             size = queue_node_dead.size();
             if (size > 0) {
-                queue_tmp = new LinkedList<Message>();
+                stepFailMsgQueue = new LinkedList<Message>();
                 for (i = 0; i < size; i++) {
                     current = queue_node_dead.peek();
                     queue_node_dead.remove(current);
                     try {
                         logger.debug("Sending backup message to " + current.getDestNodeName() + " port <"
-                                + parsePortNumber(current.getDestNodeName()) + "> ");
+                                + parentChannel.parsePortNumber(current.getDestNodeName()) + "> ");
 
-                        String host = getAddress(current.getDestNodeName());
-                        int port = parsePortNumber(current.getDestNodeName());
-                        if (clientSockets.containsKey(host)) {
-                            client_consumer = clientSockets.get(host);
-                        } else {
-                            client_consumer = new Socket(host, port);
-                            clientSockets.put(host, client_consumer);
-                        }
+                        String host = parentChannel.getAddress(current.getDestNodeName());
+                        int port = parentChannel.parsePortNumber(current.getDestNodeName());
+                        Socket client_consumer = parentChannel.getOrCreateSocket(host, port);
                         /* adding the current node */
-                        if (!current.getPassedNodes().contains(getNodeName())) {
-                            current.getPassedNodes().add(getNodeName());
+                        if (!current.getPassedNodes().contains(parentChannel.getNodeName())) {
+                            current.getPassedNodes().add(parentChannel.getNodeName());
                         }
                         OutputStream os = client_consumer.getOutputStream();
                         ObjectOutputStream oos = new ObjectOutputStream(os);
-
-
-                        // RichJSONObject obj = new RichJSONObject(current);
                         oos.writeObject(current);
                         oos.flush();
-//							client_consumer.close();
                         queue_node_dead.remove(current);
                     } catch (Exception e) {
-                        queue_tmp.add(current);
+                        stepFailMsgQueue.add(current);
                         if (alive) {
                             logger.warn("Unable to send message to  " + current.getDestNodeName() + e);
                         }
-
-                        // remove the cache socket because the destination node maybe broken and the pipe need to create again
-                        if (client_consumer != null) {
-                            clientSockets.remove(getAddress(current.getDestNodeName()));
-                        }
-
                         try {
                             Thread.sleep(timer);
                         } catch (Exception e2) {
                         }
                     }
                 }  // for
-                if (queue_tmp.size() > 0) {
-                    logger.debug("Undo queue " + queue_tmp.size());
+                if (stepFailMsgQueue.size() > 0) {
+                    logger.debug("Undo queue " + stepFailMsgQueue.size());
                     /* backup message connection refused */
-                    queue_node_dead.addAll(queue_tmp);
-                    queue_tmp.clear();
+                    queue_node_dead.addAll(stepFailMsgQueue);
+                    stepFailMsgQueue.clear();
                 }
             } else {
                 try {
                     logger.debug("acquire");
-                    sem2.acquire();
+                    deadMessageSemaphore.acquire();
                 } catch (InterruptedException e) {
                     // ignore
                 }
