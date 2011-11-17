4c340e542217534dcfe2f921cc9c8c4c80215fee
duke <fouquet.f@gmail.com>
2011-11-17 14:28:50 +0100

    fix NioChannel performance

---
+++ kevoree-library/javase/org.kevoree.library.javase.nioChannels/src/main/scala/org/kevoree/library/nioChannel/MessageListener.scala
@@ -0,0 +1,39 @@
+package org.kevoree.library.nioChannel
+
+import actors.DaemonActor
+import org.jboss.netty.channel.Channel
+import org.kevoree.framework.message.Message
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: duke
+ * Date: 17/11/11
+ * Time: 11:14
+ * To change this template use File | Settings | File Templates.
+ */
+
+class MessageListener(host: String, port: Int, channel: Channel, msgQ: MessageQueue) extends DaemonActor {
+
+  case class STOP()
+  
+  def stopProcess = { this ! STOP() }
+  
+  def act() {
+    loop {
+      react {
+        case STOP()=> exit() //TODO RECOVERY OF LOST MESSAGE
+        case msg: Message => {
+          try {
+            channel.write(msg)
+          } catch {
+            case _@e => {
+              msgQ.invalidChannel(host,port)
+            }
+          }
+
+        }
+      }
+    }
+  }
+
+}
