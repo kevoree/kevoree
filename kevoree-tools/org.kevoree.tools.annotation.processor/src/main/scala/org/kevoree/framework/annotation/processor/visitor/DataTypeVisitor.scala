fb06219ee48fa8e52e04557a9ae0dbf187107ce9
duke <fouquet.f@gmail.com>
2011-11-22 23:18:36 +0100

    Fix editor Fix Array parsing in processor Fix latexEditor
    Former-commit-id: efff6aff23fa0767bc01412ac38bbde28bc241b8

--- kevoree-tools/org.kevoree.tools.annotation.processor/src/main/scala/org/kevoree/framework/annotation/processor/visitor/DataTypeVisitor.scala
+++ kevoree-tools/org.kevoree.tools.annotation.processor/src/main/scala/org/kevoree/framework/annotation/processor/visitor/DataTypeVisitor.scala
@@ -24,6 +24,7 @@ import org.kevoree.framework.annotation.processor.LocalUtility
 import javax.lang.model.util.SimpleTypeVisitor6
 import javax.lang.model.`type`._
 import javax.lang.model.element.Element
+import java.util.StringTokenizer
 
 
 class DataTypeVisitor extends SimpleTypeVisitor6[Any, Any] {
@@ -41,13 +42,24 @@ class DataTypeVisitor extends SimpleTypeVisitor6[Any, Any] {
     p1.getTypeArguments.foreach {
       tm =>
         val dtv = new DataTypeVisitor();
-        tm.accept(dtv,tm);
+        tm.accept(dtv, tm);
         dataType.addGenericTypes(LocalUtility.getOraddDataType(dtv.getDataType()));
     }
   }
 
+  override def visitArray(p1: _root_.javax.lang.model.`type`.ArrayType, p: Any) {
+    var name = p1.toString
+    name = name.replace("[]", "")
+    val sb = new StringBuffer()
+    sb.append("Array[")
+    sb.append(name.substring(0, 1).toUpperCase);
+    sb.append(name.substring(1).toLowerCase);
+    sb.append("]");
+    dataType.setName(sb.toString)
+  }
 
   override def visitPrimitive(p1: _root_.javax.lang.model.`type`.PrimitiveType, p: Any) {
+
     p1.getKind match {
       case TypeKind.BOOLEAN => dataType.setName("scala.Boolean")
       case TypeKind.BYTE => dataType.setName("scala.Byte")
@@ -57,6 +69,9 @@ class DataTypeVisitor extends SimpleTypeVisitor6[Any, Any] {
       case TypeKind.INT => dataType.setName("scala.Int")
       case TypeKind.LONG => dataType.setName("scala.Long")
       case TypeKind.SHORT => dataType.setName("scala.Short")
+      case TypeKind.ARRAY => {
+        dataType.setName(p1.toString)
+      }
     }
   }
 
