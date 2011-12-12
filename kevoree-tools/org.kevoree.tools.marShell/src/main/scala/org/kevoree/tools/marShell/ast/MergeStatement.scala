d07d6fe7ee6d192682be19d8f45b9fd8786874ac
duke <fouquet.f@gmail.com>
2011-12-12 15:50:44 +0100

    Add Merge statement

---
+++ kevoree-tools/org.kevoree.tools.marShell/src/main/scala/org/kevoree/tools/marShell/ast/MergeStatement.scala
@@ -0,0 +1,11 @@
+package org.kevoree.tools.marShell.ast
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: duke
+ * Date: 12/12/11
+ * Time: 14:49
+ * To change this template use File | Settings | File Templates.
+ */
+
+case class MergeStatement(url : String) extends Statment {}
