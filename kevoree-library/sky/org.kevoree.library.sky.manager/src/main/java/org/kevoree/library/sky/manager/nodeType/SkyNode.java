0716218e0f4458050c650301864816f17795840d
Erwan Daubert <Erwan.Daubert@gmail.com>
2011-12-12 11:17:31 +0100

    add abstract on SkyNode

--- kevoree-library/sky/org.kevoree.library.sky.manager/src/main/java/org/kevoree/library/sky/manager/nodeType/SkyNode.java
+++ kevoree-library/sky/org.kevoree.library.sky.manager/src/main/java/org/kevoree/library/sky/manager/nodeType/SkyNode.java
@@ -3,10 +3,7 @@ package org.kevoree.library.sky.manager.nodeType;
 import org.kevoree.AdaptationPrimitiveType;
 import org.kevoree.ContainerNode;
 import org.kevoree.ContainerRoot;
-import org.kevoree.annotation.NodeType;
-import org.kevoree.annotation.PrimitiveCommands;
-import org.kevoree.annotation.Start;
-import org.kevoree.annotation.Stop;
+import org.kevoree.annotation.*;
 import org.kevoree.framework.PrimitiveCommand;
 import org.kevoree.library.defaultNodeTypes.JavaSENode;
 import org.kevoree.library.sky.manager.Helper;
@@ -32,16 +29,20 @@ import scala.Some;
  */
 
 @PrimitiveCommands(value = {}, values = {SkyNode.REMOVE_NODE, SkyNode.ADD_NODE})
+@DictionaryType({
+		@DictionaryAttribute(name = "role", defaultValue = "host", vals = {"host", "container", "host/container"},
+				optional = false)
+})
 @NodeType
-public class SkyNode extends JavaSENode {
+public abstract class SkyNode extends JavaSENode {
 	private static final Logger logger = LoggerFactory.getLogger(SkyNode.class);
 
 	protected static final String REMOVE_NODE = "RemoveNode";
 	protected static final String ADD_NODE = "AddNode";
 
-	public KevoreeNodeRunner createKevoreeNodeRunner(String nodeName, String bootStrapModel) {
+	public abstract KevoreeNodeRunner createKevoreeNodeRunner (String nodeName, String bootStrapModel) ;/*{
 		return null;
-	}
+	}*/
 
 
 	@Start
@@ -64,74 +65,134 @@ public class SkyNode extends JavaSENode {
 		KevoreeNodeManager.stop();
 	}
 
+	private boolean isHost () {
+		String role = this.getDictionary().get("role").toString();
+		return role.contains("host");
+	}
+
+	private boolean isContainer () {
+		String role = this.getDictionary().get("role").toString();
+		return role.contains("container");
+	}
+
 	@Override
 	public AdaptationModel kompare (ContainerRoot current, ContainerRoot target) {
 		logger.debug("starting kompare...");
-		AdaptationPrimitiveType removeNodeType = null;
-		AdaptationPrimitiveType addNodeType = null;
-//		AdaptationPrimitiveType updateNodeType = null;
-		// looking for managed AdaptationPrimitiveType
-		for (AdaptationPrimitiveType primitiveType : current.getAdaptationPrimitiveTypesForJ()) {
-			if (primitiveType.getName().equals(REMOVE_NODE)) {
-				removeNodeType = primitiveType;
-			} else if (primitiveType.getName().equals(ADD_NODE)) {
-				addNodeType = primitiveType;
-			} /*else if (primitiveType.getName().equals(UPDATE_NODE)) {
-				updateNodeType = primitiveType;
-			}*/
-		}
-
-		if (removeNodeType == null || addNodeType == null /*|| updateNodeType == null*/) {
-			for (AdaptationPrimitiveType primitiveType : target.getAdaptationPrimitiveTypesForJ()) {
-				if (primitiveType.getName().equals(REMOVE_NODE)) {
-					removeNodeType = primitiveType;
-				} else if (primitiveType.getName().equals(ADD_NODE)) {
-					addNodeType = primitiveType;
-				} /*else if (primitiveType.getName().equals(UPDATE_NODE)) {
-					updateNodeType = primitiveType;
-				}*/
-			}
-		}
-		if (removeNodeType == null) {
-			logger.warn("there is no adaptation primitive for " + REMOVE_NODE);
-		}
-		if (addNodeType == null) {
-			logger.warn("there is no adaptation primitive for " + ADD_NODE);
-		}
-		/*if (updateNodeType == null) {
-			logger.warn("there is no adaptation primitive for " + UPDATE_NODE);
-		}*/
 
 		AdaptationModel adaptationModel = KevoreeAdaptationFactory.eINSTANCE().createAdaptationModel();
 		ParallelStep step = KevoreeAdaptationFactory.eINSTANCE().createParallelStep();
 		adaptationModel.setOrderedPrimitiveSet(new Some<ParallelStep>(step));
 
-		// find all containerNode to remove
-		for (ContainerNode node : current.getNodesForJ()) {
-			if (node.getName().equals(this.getNodeName())) {
-				for (ContainerNode subNode : node.getHostsForJ()) {
-					boolean found = false;
-					for (ContainerNode node1 : target.getNodesForJ()) {
-						if (node1.getName().equals(this.getNodeName())) {
-							for (ContainerNode subNode1 : node1.getHostsForJ()) {
-								if (subNode.getName().equals(subNode1.getName())) {
-									found = true;
-									break;
+		if (isHost()) {
+			AdaptationPrimitiveType removeNodeType = null;
+			AdaptationPrimitiveType addNodeType = null;
+			//		AdaptationPrimitiveType updateNodeType = null;
+			// looking for managed AdaptationPrimitiveType
+			for (AdaptationPrimitiveType primitiveType : current.getAdaptationPrimitiveTypesForJ()) {
+				if (primitiveType.getName().equals(REMOVE_NODE)) {
+					removeNodeType = primitiveType;
+				} else if (primitiveType.getName().equals(ADD_NODE)) {
+					addNodeType = primitiveType;
+				} /*else if (primitiveType.getName().equals(UPDATE_NODE)) {
+				updateNodeType = primitiveType;
+			}*/
+			}
+
+			if (removeNodeType == null || addNodeType == null /*|| updateNodeType == null*/) {
+				for (AdaptationPrimitiveType primitiveType : target.getAdaptationPrimitiveTypesForJ()) {
+					if (primitiveType.getName().equals(REMOVE_NODE)) {
+						removeNodeType = primitiveType;
+					} else if (primitiveType.getName().equals(ADD_NODE)) {
+						addNodeType = primitiveType;
+					} /*else if (primitiveType.getName().equals(UPDATE_NODE)) {
+					updateNodeType = primitiveType;
+				}*/
+				}
+			}
+			if (removeNodeType == null) {
+				logger.warn("there is no adaptation primitive for " + REMOVE_NODE);
+			}
+			if (addNodeType == null) {
+				logger.warn("there is no adaptation primitive for " + ADD_NODE);
+			}
+			/*if (updateNodeType == null) {
+			 logger.warn("there is no adaptation primitive for " + UPDATE_NODE);
+		 }*/
+
+
+			// find all containerNode to remove
+			for (ContainerNode node : current.getNodesForJ()) {
+				if (node.getName().equals(this.getNodeName())) {
+					for (ContainerNode subNode : node.getHostsForJ()) {
+						boolean found = false;
+						for (ContainerNode node1 : target.getNodesForJ()) {
+							if (node1.getName().equals(this.getNodeName())) {
+								for (ContainerNode subNode1 : node1.getHostsForJ()) {
+									if (subNode.getName().equals(subNode1.getName())) {
+										found = true;
+										break;
+									}
+								}
+								if (!found) {
+									// create RemoveNode command
+									logger.debug(
+											"add a " + REMOVE_NODE + " adaptation primitive with " + subNode.getName()
+													+ " as parameter");
+									AdaptationPrimitive command = KevoreeAdaptationFactory.eINSTANCE()
+											.createAdaptationPrimitive();
+									command.setPrimitiveType(removeNodeType);
+									command.setRef(subNode);
+									ParallelStep subStep = KevoreeAdaptationFactory.eINSTANCE().createParallelStep();
+									subStep.addAdaptations(command);
+									adaptationModel.addAdaptations(command);
+									step.setNextStep(new Some<ParallelStep>(subStep));
+									step = subStep;
 								}
 							}
-							if (!found) {
-								// create RemoveNode command
-								logger.debug("add a " + REMOVE_NODE + " adaptation primitive with " + subNode.getName()
-										+ " as parameter");
-								AdaptationPrimitive command = KevoreeAdaptationFactory.eINSTANCE()
-										.createAdaptationPrimitive();
-								command.setPrimitiveType(removeNodeType);
-								command.setRef(subNode);
-								ParallelStep subStep = KevoreeAdaptationFactory.eINSTANCE().createParallelStep();
-								subStep.addAdaptations(command);
-								adaptationModel.addAdaptations(command);
-								step.setNextStep(new Some<ParallelStep>(subStep));
-								step = subStep;
+						}
+					}
+				}
+			}
+
+			// find all containerNode to add
+			for (ContainerNode node : target.getNodesForJ()) {
+				if (node.getName().equals(this.getNodeName())) {
+					for (ContainerNode subNode : node.getHostsForJ()) {
+						boolean found = false;
+						for (ContainerNode node1 : current.getNodesForJ()) {
+							if (node1.getName().equals(this.getNodeName())) {
+								for (ContainerNode subNode1 : node1.getHostsForJ()) {
+									if (subNode.getName().equals(subNode1.getName())) {
+										found = true;
+										// create UpdateNode command
+										/*logger.debug("add a " + UPDATE_NODE + " adaptation primitive with " + subNode.getName()
+											+ " as parameter");
+											AdaptationPrimitive command = KevoreeAdaptationFactory.eINSTANCE()
+											.createAdaptationPrimitive();
+											command.setPrimitiveType(updateNodeType);
+											command.setRef(subNode);
+											ParallelStep subStep = KevoreeAdaptationFactory.eINSTANCE().createParallelStep();
+											subStep.addAdaptations(command);
+											adaptationModel.addAdaptations(command);
+											step.setNextStep(new Some<ParallelStep>(subStep));
+											step = subStep;*/
+										break;
+									}
+								}
+								if (!found) {
+									// create AddNode command
+									logger.debug("add a " + ADD_NODE + " adaptation primitive with " + subNode.getName()
+											+ " as parameter");
+									AdaptationPrimitive command = KevoreeAdaptationFactory.eINSTANCE()
+											.createAdaptationPrimitive();
+									command.setPrimitiveType(addNodeType);
+									command.setRef(subNode);
+									ParallelStep subStep = KevoreeAdaptationFactory.eINSTANCE().createParallelStep();
+									subStep.addAdaptations(command);
+									adaptationModel.addAdaptations(command);
+									step.setNextStep(new Some<ParallelStep>(subStep));
+									step = subStep;
+								}
 							}
 						}
 					}
@@ -139,60 +200,16 @@ public class SkyNode extends JavaSENode {
 			}
 		}
 
-		// find all containerNode to add
-		for (ContainerNode node : target.getNodesForJ()) {
-			if (node.getName().equals(this.getNodeName())) {
-				for (ContainerNode subNode : node.getHostsForJ()) {
-					boolean found = false;
-					for (ContainerNode node1 : current.getNodesForJ()) {
-						if (node1.getName().equals(this.getNodeName())) {
-							for (ContainerNode subNode1 : node1.getHostsForJ()) {
-								if (subNode.getName().equals(subNode1.getName())) {
-									found = true;
-									// create UpdateNode command
-									/*logger.debug("add a " + UPDATE_NODE + " adaptation primitive with " + subNode.getName()
-																		+ " as parameter");
-																AdaptationPrimitive command = KevoreeAdaptationFactory.eINSTANCE()
-																		.createAdaptationPrimitive();
-																command.setPrimitiveType(updateNodeType);
-																command.setRef(subNode);
-																ParallelStep subStep = KevoreeAdaptationFactory.eINSTANCE().createParallelStep();
-																subStep.addAdaptations(command);
-																adaptationModel.addAdaptations(command);
-																step.setNextStep(new Some<ParallelStep>(subStep));
-																step = subStep;*/
-									break;
-								}
-							}
-							if (!found) {
-								// create AddNode command
-								logger.debug("add a " + ADD_NODE + " adaptation primitive with " + subNode.getName()
-										+ " as parameter");
-								AdaptationPrimitive command = KevoreeAdaptationFactory.eINSTANCE()
-										.createAdaptationPrimitive();
-								command.setPrimitiveType(addNodeType);
-								command.setRef(subNode);
-								ParallelStep subStep = KevoreeAdaptationFactory.eINSTANCE().createParallelStep();
-								subStep.addAdaptations(command);
-								adaptationModel.addAdaptations(command);
-								step.setNextStep(new Some<ParallelStep>(subStep));
-								step = subStep;
-							}
-						}
-					}
-				}
-			}
+		if (isContainer()) {
+			AdaptationModel superModel = super.kompare(current, target);
+			adaptationModel.addAllAdaptations(superModel.getAdaptations());
+			step.setNextStep(superModel.getOrderedPrimitiveSet());
+			logger.debug("Kompare model contain " + adaptationModel.getAdaptations().size() + " primitives");
+
 		}
-
-		AdaptationModel superModel = super.kompare(current, target);
-		adaptationModel.addAllAdaptations(superModel.getAdaptations());
-		step.setNextStep(superModel.getOrderedPrimitiveSet());
-		logger.debug("Kompare model contain " + adaptationModel.getAdaptations().size() + " primitives");
-
-		for (AdaptationPrimitive p : adaptationModel.getAdaptationsForJ()) {
+		/*for (AdaptationPrimitive p : adaptationModel.getAdaptationsForJ()) {
 			logger.debug("primitive " + p.getPrimitiveType().getName());
-		}
-
+		}*/
 
 		return adaptationModel;
 	}
