d701b92f6f430399bab33ba4fb40a89d6f6af321
JeD <jedartois@gmail.com>
2011-12-13 11:49:58 +0100

    add listen on all interfaces

--- kevoree-library/javase/org.kevoree.library.javase.restAutoDiscovery/src/main/java/org/kevoree/library/jmdnsrest/JmDNSRestGroup.java
+++ kevoree-library/javase/org.kevoree.library.javase.restAutoDiscovery/src/main/java/org/kevoree/library/jmdnsrest/JmDNSRestGroup.java
@@ -11,9 +11,15 @@ import org.kevoree.library.rest.ServerBootstrap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.awt.*;
 import java.io.*;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,22 +30,23 @@ import java.net.URLConnection;
 
 
 @DictionaryType({
-        @DictionaryAttribute(name = "port", defaultValue = "8000", optional = true , fragmentDependant = true),
-        @DictionaryAttribute(name = "timer", defaultValue = "5000", optional = true, fragmentDependant = true)   // gap between request scan of the network
+        @DictionaryAttribute(name = "port", defaultValue = "8000", optional = false , fragmentDependant = true),
+        @DictionaryAttribute(name = "timer", defaultValue = "5000", optional = false, fragmentDependant = true)   // gap between request scan of the network
 })
 @GroupType
 @Library(name="JavaSE")
 public class JmDNSRestGroup extends RestGroup implements  Runnable{
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
-    private JmDnsComponent jmdns = null ;
+    private ArrayList<JmDnsComponent> jmdns = null ;
     private  Thread thread = null;
     private  boolean  alive;
-    private int timer=2000;
+    private int timer;
+
     @Start
     public void startJmdnsGroup() {
         super.startRestGroup();
-        jmdns = new JmDnsComponent(this.getNodeName(),this.getName() ,Integer.parseInt(this.getDictionary().get("port").toString()),this.getModelService(),"JmDNSRestGroup");
+        jmdns = new ArrayList<JmDnsComponent>();
         thread = new Thread(this);
         updateDico();
         alive = true;
@@ -48,10 +55,11 @@ public class JmDNSRestGroup extends RestGroup implements  Runnable{
 
     @Stop
     public void stopJmdnsGroup() {
-
         alive = false;
         thread.interrupt();
-        jmdns.close();
+        for(JmDnsComponent _jmdns : jmdns){
+            _jmdns.close();
+        }
         jmdns = null;
         super.stopRestGroup();
     }
@@ -64,15 +72,56 @@ public class JmDNSRestGroup extends RestGroup implements  Runnable{
         try{
             timer = Integer.parseInt(getDictionary().get("timer").toString());
         } catch (Exception e){
-         timer = 8000;
+            timer = 9000;
         }
+    }
+
+
+    public ArrayList<InetAddress> getIps(){
+        ArrayList<InetAddress> ips = new ArrayList<InetAddress>();
+        try
+        {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface interfaceN = (NetworkInterface)interfaces.nextElement();
+                Enumeration<InetAddress> ienum = interfaceN.getInetAddresses();
+                while (ienum.hasMoreElements()) {
+                    InetAddress ia = ienum.nextElement();
+                    if(!ia.getHostAddress().toString().startsWith("127")){
+                        ips.add(ia);
+                    }
+
+                }
+            }
+        }
+        catch(Exception e){
+            logger.error("pas de carte reseau " + e);
+        }
+
+        return ips;
     }
 
     @Override
     public void run() {
+        ArrayList<InetAddress> ips =    getIps();
+        ContainerRoot model = getModelService().getLastModel();
+        for(InetAddress ip : ips)
+        {
+            KevoreePlatformHelper.updateNodeLinkProp(model,this.getNodeName(),this.getNodeName(),org.kevoree.framework.Constants.KEVOREE_PLATFORM_REMOTE_NODE_IP(),ip.getHostName(),"LAN", 100);
+            KevoreePlatformHelper.updateNodeLinkProp(model,this.getNodeName(),this.getNodeName(), org.kevoree.framework.Constants.KEVOREE_MODEL_PORT(),this.getDictionary().get("port").toString(), "LAN", 100);
+        }
+        getModelService().updateModel(model);
+
+        for(InetAddress ip : ips)
+        {
+            jmdns.add(new JmDnsComponent(this.getNodeName(),this.getName() ,Integer.parseInt(this.getDictionary().get("port").toString()),this.getModelService(),"JmDNSRestGroup",ip));
+        }
         while(alive)
         {
-            jmdns.requestUpdateList();
+            for(JmDnsComponent _jmdns : jmdns){
+                _jmdns.requestUpdateList(timer);
+            }
             try {
                 Thread.sleep(timer);
             } catch (InterruptedException e) {
