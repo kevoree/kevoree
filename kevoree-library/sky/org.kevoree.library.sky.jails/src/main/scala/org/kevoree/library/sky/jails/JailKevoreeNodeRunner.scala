b486b7d3244425f04607909f78be44163006a9b5
Erwan Daubert <Erwan.Daubert@gmail.com>
2012-01-20 17:00:02 +0100

    generalize ssh configuration for all futur kloud nodes

--- kevoree-library/sky/org.kevoree.library.sky.jails/src/main/scala/org/kevoree/library/sky/jails/JailKevoreeNodeRunner.scala
+++ kevoree-library/sky/org.kevoree.library.sky.jails/src/main/scala/org/kevoree/library/sky/jails/JailKevoreeNodeRunner.scala
@@ -132,36 +132,8 @@ class JailKevoreeNodeRunner (nodeName: String, bootStrapModel: String, inet: Str
 
             // specify limitation on jail such as CPU, RAM
             if (specifyConstraints()) {
-              // copy SSH Public Key if available
-              // for each SSHRestGroup, We copy the Public SSH key on jail
-              logger.debug("try to copy SSH Public keys into the jail...")
-              model.getGroups.filter(group => group.getTypeDefinition.getName == "SSHRestGroup" &&
-                (group.getSubNodes.find(node => node.getName == nodeName) match {
-                  case None => false
-                  case Some(node) => true
-                })).foreach {
-                group =>
-                  val keyOption = KevoreePropertyHelper
-                    .getPropertyForGroup(model, group.getName, SSHRestGroup.SSH_PUBLIC_KEY)
-                  if (keyOption.isDefined) {
-                    logger.debug("try to copy SSH Public keys from {} into the jail...", group.getName)
-                    // check if .ssh is available and create it else
-                    if (!new File(jailPath + File.separator + "root" + File.separator + ".ssh" + File.separator +
-                      "authorized_keys").exists()) {
-                      new File(jailPath + File.separator + "root" + File.separator + ".ssh").mkdirs()
-                    }
-                    addStringToFile(keyOption.get.toString,
-                                     jailPath + File.separator + "root" + File.separator + ".ssh" + File.separator +
-                                       "authorized_keys")
-                    // configure sshd: BASIC CONFIGURATION USING TEMPLATE
-                    copyFileFromStream(this.getClass.getClassLoader.getResourceAsStream("sshd_config"),
-                                        jailPath + File.separator + "etc" + File.separator + "ssh" + File.separator +
-                                          "sshd_config")
-                    replaceStringIntoFile("<ip_address>", newIp,
-                                           jailPath + File.separator + "etc" + File.separator + "ssh" + File.separator +
-                                             "sshd_config")
-                  }
-              }
+              // configure ssh access
+              configureSSH(model, jailPath, newIp)
               // launch the jail
               resultActor.starting()
               logger.debug("running {} onestart {}", Array[AnyRef](ezjailAdmin, nodeName))
@@ -585,107 +557,6 @@ class JailKevoreeNodeRunner (nodeName: String, bootStrapModel: String, inet: Str
     } else {
       logger.debug("Unable to find {}", inputFile)
       false
-    }
-  }
-
-  private def copyFileFromStream (inputStream: InputStream, outputFile: String): Boolean = {
-    logger.debug("trying to copy a stream into {}", outputFile)
-    if (inputStream != null) {
-      try {
-        if (new File(outputFile).exists()) {
-          new File(outputFile).delete()
-        }
-        val reader = new DataInputStream(inputStream)
-        val writer = new DataOutputStream(new FileOutputStream(new File(outputFile)))
-
-        val bytes = new Array[Byte](2048)
-        var length = reader.read(bytes)
-        while (length != -1) {
-          writer.write(bytes, 0, length)
-          length = reader.read(bytes)
-
-        }
-        writer.flush()
-        writer.close()
-        reader.close()
-        true
-      } catch {
-        case _@e => logger.error("Unable to copy a stream into {}", outputFile, e); false
-      }
-    } else {
-      logger.debug("The stream is undefined")
-      false
-    }
-  }
-
-  private def addStringToFile (data: String, outputFile: String) {
-    logger.debug("trying to add \"{}\" into {}", data, outputFile)
-    val stringBuilder = new StringBuilder
-    stringBuilder append data + "\n"
-    if (new File(outputFile).exists()) {
-      val reader = new DataInputStream(new FileInputStream(new File(outputFile)))
-      val writer = new ByteArrayOutputStream()
-
-      val bytes = new Array[Byte](2048)
-      var length = reader.read(bytes)
-      while (length != -1) {
-        writer.write(bytes, 0, length)
-        length = reader.read(bytes)
-
-      }
-      writer.flush()
-      writer.close()
-      reader.close()
-      stringBuilder append new String(writer.toByteArray)
-    }
-
-    copyStringToFile(stringBuilder.toString(), outputFile)
-    logger.debug("adding \"{}\" into {} is done", data, outputFile)
-  }
-
-  private def copyStringToFile (data: String, outputFile: String) {
-    logger.debug("trying to copy \"{}\" to {}", data, outputFile)
-    try {
-      if (new File(outputFile).exists()) {
-        new File(outputFile).delete()
-      }
-      val writer = new DataOutputStream(new FileOutputStream(new File(outputFile)))
-
-      writer.write(data.getBytes)
-      writer.flush()
-      writer.close()
-      logger.debug("copying \"{}\" into {} uis done", data, outputFile)
-    } catch {
-      case _@e => logger.error("Unable to copy \"{}\" on {}", Array[AnyRef](data, outputFile), e); false
-    }
-  }
-
-  private def replaceStringIntoFile (dataToReplace: String, newData: String, file: String) {
-    logger.debug("trying to replace \"{}\" by \"{}\" into {}", Array[AnyRef](dataToReplace, newData, file))
-    if (new File(file).exists()) {
-      val stringBuilder = new StringBuilder
-      val reader = new DataInputStream(new FileInputStream(new File(file)))
-      val writer = new ByteArrayOutputStream()
-
-      val bytes = new Array[Byte](2048)
-      var length = reader.read(bytes)
-      while (length != -1) {
-        writer.write(bytes, 0, length)
-        length = reader.read(bytes)
-
-      }
-      writer.flush()
-      writer.close()
-      reader.close()
-      stringBuilder append new String(writer.toByteArray)
-      stringBuilder
-        .replace(stringBuilder.indexOf(dataToReplace), stringBuilder.indexOf(dataToReplace) + dataToReplace.length(),
-                  newData)
-
-      copyStringToFile(stringBuilder.toString(), file)
-      logger.debug("replacing \"{}\" by \"{}\" into {} is done", Array[AnyRef](dataToReplace, newData, file))
-    } else {
-      logger.debug("The file {} doesn't exist, anything can be replace.", file)
     }
   }
 
