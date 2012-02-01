c2a817c96ec75379e09c7af85333b52d0042eafc
Erwan Daubert <Erwan.Daubert@gmail.com>
2012-02-01 09:01:16 +0100

    use NodeFragment

---
+++ kevoree-tools/org.kevoree.tools.annotation.api/src/main/java/org/kevoree/annotation/NodeFragment.java
@@ -0,0 +1,32 @@
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kevoree.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * User: Erwan Daubert - erwan.daubert@gmail.com
+ * Date: 30/01/12
+ * Time: 17:46
+ *
+ * @author Erwan Daubert
+ * @version 1.0
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface NodeFragment {
+}
