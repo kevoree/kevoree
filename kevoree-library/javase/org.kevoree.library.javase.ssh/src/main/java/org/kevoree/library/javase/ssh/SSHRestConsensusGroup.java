0939e4311927253953ba661ecd40585c55bb291e
Erwan Daubert <Erwan.Daubert@gmail.com>
2012-02-13 11:51:22 +0100

    add Consensus prototype group

---
+++ kevoree-library/javase/org.kevoree.library.javase.ssh/src/main/java/org/kevoree/library/javase/ssh/SSHRestConsensusGroup.java
@@ -0,0 +1,18 @@
+package org.kevoree.library.javase.ssh;
+
+import org.kevoree.annotation.GroupType;
+import org.kevoree.annotation.Library;
+import org.kevoree.library.rest.RestConsensusGroup;
+
+/**
+ * User: Erwan Daubert - erwan.daubert@gmail.com
+ * Date: 13/02/12
+ * Time: 10:48
+ *
+ * @author Erwan Daubert
+ * @version 1.0
+ */
+@GroupType
+@Library(name = "JavaSE")
+public class SSHRestConsensusGroup extends RestConsensusGroup {
+}
