797468dccd95bdfb794dad85202e50af168b9fcd
Erwan Daubert <Erwan.Daubert@gmail.com>
2012-02-21 21:05:01 +0100

    add Kloud and miniKloud buttons on the editor

---
+++ kevoree-tools/org.kevoree.tools.ui.editor/src/main/scala/org/kevoree/tools/ui/editor/form/KillWatchDog.scala
@@ -0,0 +1,28 @@
+package org.kevoree.tools.ui.editor.form
+
+import actors.{TIMEOUT, DaemonActor}
+
+/**
+ * User: Erwan Daubert - erwan.daubert@gmail.com
+ * Date: 27/09/11
+ * Time: 10:53
+ *
+ * @author Erwan Daubert
+ * @version 1.0
+ */
+
+class KillWatchDog (process: Process, timout: Int) extends DaemonActor {
+
+  case class STOP ()
+
+  def stop () {
+    this ! STOP()
+  }
+
+  def act () {
+    reactWithin(timout) {
+      case TIMEOUT => process.destroy()
+      case STOP() => this.exit()
+    }
+  }
+}
