9c6f9b25642e8d89f9a4331bc8c19f3daa5e0599
François Fouquet <fouquet.f@gmail.com>
2012-03-06 14:54:48 +0100

    Fix MErger Bug Improve KCL for getResource As Stream (transitive dependency)
    Former-commit-id: 2520a7f605e1522664842a36ffff2f2acd7dab43

---
+++ kevoree-core/org.kevoree.merger/src/main/scala/org/kevoree/merger/resolver/PortTypeRefResolver.scala
@@ -0,0 +1,73 @@
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kevoree.merger.resolver
+
+import org.slf4j.LoggerFactory
+import org.kevoree.{ComponentType, ComponentInstance, ContainerRoot}
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: duke
+ * Date: 06/03/12
+ * Time: 10:28
+ */
+
+trait PortTypeRefResolver {
+  private val logger = LoggerFactory.getLogger(this.getClass)
+
+  def resolvePortTypeRef(model: ContainerRoot) {
+    import org.kevoree.framework.aspects.KevoreeAspects._
+    model.getAllInstances.foreach {
+      instance =>
+      //WE ASSUME THAT TYPE DEFINITION IS ALREADY RESOLVED
+        if (instance.isInstanceOf[ComponentInstance]) {
+          val componentInstance: ComponentInstance = instance.asInstanceOf[ComponentInstance]
+          componentInstance.getProvided.foreach {
+            pport =>
+              val ct = instance.getTypeDefinition.asInstanceOf[ComponentType]
+              ct.getProvided.foreach {
+                pp =>
+                  if (pp.getName == pport.getPortTypeRef.getName) {
+                    pport.setPortTypeRef(pp)
+                  }
+              }
+              ct.getRequired.foreach {
+                rp =>
+                  if (rp.getName == pport.getPortTypeRef.getName) {
+                    pport.setPortTypeRef(rp)
+                  }
+              }
+          }
+          componentInstance.getRequired.foreach {
+            rport =>
+              val ct = instance.getTypeDefinition.asInstanceOf[ComponentType]
+              ct.getProvided.foreach {
+                pp =>
+                  if (pp.getName == rport.getPortTypeRef.getName) {
+                    rport.setPortTypeRef(pp)
+                  }
+              }
+              ct.getRequired.foreach {
+                rp =>
+                  if (rp.getName == rport.getPortTypeRef.getName) {
+                    rport.setPortTypeRef(rp)
+                  }
+              }
+          }
+        }
+
+    }
+  }
+
+}
