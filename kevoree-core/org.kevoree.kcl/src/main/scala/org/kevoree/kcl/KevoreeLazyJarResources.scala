f418c32e9b98203ab77c11cec457273fa5a073c7
François Fouquet <fouquet.f@gmail.com>
2012-03-06 16:16:24 +0100

    minor fix
    Former-commit-id: 44f03beaa2bab88d2ddb43442853f098602d9245

--- kevoree-core/org.kevoree.kcl/src/main/scala/org/kevoree/kcl/KevoreeLazyJarResources.scala
+++ kevoree-core/org.kevoree.kcl/src/main/scala/org/kevoree/kcl/KevoreeLazyJarResources.scala
@@ -107,7 +107,7 @@ class KevoreeLazyJarResources extends ClasspathResources {
 
   def loadJar(jarStream: InputStream, baseurl: URL) {
 
-    logger.debug("Load From " + jarStream.toString)
+    //logger.debug("Load From " + jarStream.toString)
 
     var bis: BufferedInputStream = null;
     var jis: JarInputStream = null;
