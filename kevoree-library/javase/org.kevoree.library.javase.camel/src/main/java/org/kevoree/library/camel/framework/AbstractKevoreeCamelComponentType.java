a9f49358fe4f323426df7b4e78605289d442b958
François Fouquet <fouquet.f@gmail.com>
2012-03-07 13:35:40 +0100

    Initiate Camel Framework

---
+++ kevoree-library/javase/org.kevoree.library.javase.camel/src/main/java/org/kevoree/library/camel/framework/AbstractKevoreeCamelComponentType.java
@@ -0,0 +1,60 @@
+package org.kevoree.library.camel.framework;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.kevoree.annotation.*;
+import org.kevoree.framework.AbstractComponentType;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: duke
+ * Date: 07/03/12
+ * Time: 11:50
+ */
+
+@Library(name = "JavaSE")
+@ComponentFragment
+public abstract class AbstractKevoreeCamelComponentType extends AbstractComponentType {
+
+    private CamelContext context = null;
+
+    public CamelContext getContext() {
+        return context;
+    }
+
+    public CamelContext buildCamelContext(){
+        return new DefaultCamelContext();
+    }
+
+    @Start
+    public void start() throws Exception {
+        context = buildCamelContext();
+        context.setClassResolver(new ClassLoaderClassResolver(this.getClass().getClassLoader()));
+        RouteBuilder rb = new RouteBuilder() {
+            public void configure() {
+                buildRoutes(this);
+            }
+        };
+        context.addRoutes(rb);
+        context.start();
+    }
+
+    @Stop
+    public void stop() throws Exception {
+        if (context != null) {
+            context.stop();
+        }
+        context = null;
+    }
+
+    @Update
+    public void update() throws Exception {
+        stop();
+        start();
+    }
+
+    protected abstract void buildRoutes(RouteBuilder rb);
+
+
+}
---
+++ kevoree-library/javase/org.kevoree.library.javase.camel/src/main/java/org/kevoree/library/camel/framework/ClassLoaderClassResolver.java
@@ -0,0 +1,84 @@
+package org.kevoree.library.camel.framework;
+
+import org.apache.camel.spi.ClassResolver;
+import org.kevoree.library.camel.TestCamel;
+
+import java.io.InputStream;
+import java.net.URL;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: duke
+ * Date: 07/03/12
+ * Time: 11:34
+ */
+public class ClassLoaderClassResolver implements ClassResolver {
+
+    private ClassLoader cl = null;
+    public ClassLoaderClassResolver(ClassLoader c){
+        cl = c;
+    }
+    
+    @Override
+    public Class<?> resolveClass(String s) {
+        try {
+            return cl.loadClass(s);
+        } catch (ClassNotFoundException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public <T> Class<T> resolveClass(String s, Class<T> tClass) {
+        return null;
+    }
+
+    @Override
+    public Class<?> resolveClass(String s, ClassLoader classLoader) {
+        try {
+            return cl.loadClass(s);
+        } catch (ClassNotFoundException e) {
+            try {
+                return TestCamel.class.getClassLoader().loadClass(s);
+            } catch (ClassNotFoundException ee) {
+                return null;
+            }
+        }
+    }
+
+    @Override
+    public <T> Class<T> resolveClass(String s, Class<T> tClass, ClassLoader classLoader) {
+        return null;
+    }
+
+    @Override
+    public Class<?> resolveMandatoryClass(String s) throws ClassNotFoundException {
+        return cl.loadClass(s);
+    }
+
+    @Override
+    public <T> Class<T> resolveMandatoryClass(String s, Class<T> tClass) throws ClassNotFoundException {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Class<?> resolveMandatoryClass(String s, ClassLoader classLoader) throws ClassNotFoundException {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public <T> Class<T> resolveMandatoryClass(String s, Class<T> tClass, ClassLoader classLoader) throws ClassNotFoundException {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public InputStream loadResourceAsStream(String s) {
+        return cl.getResourceAsStream(s);
+    }
+
+    @Override
+    public URL loadResourceAsURL(String s) {
+        return cl.getResource(s);
+    }
+
+}
