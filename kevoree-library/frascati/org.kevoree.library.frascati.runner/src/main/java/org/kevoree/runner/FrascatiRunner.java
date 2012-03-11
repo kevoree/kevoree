d39a631f82fcdae155a7f120c36c495180a83e4b
barais@irisa.fr <barais@irisa.fr>
2012-03-11 21:25:30 +0100

    Last Update

--- kevoree-library/frascati/org.kevoree.library.frascati.runner/src/main/java/org/kevoree/runner/FrascatiRunner.java
+++ kevoree-library/frascati/org.kevoree.library.frascati.runner/src/main/java/org/kevoree/runner/FrascatiRunner.java
@@ -13,9 +13,11 @@ public class FrascatiRunner {
 
     public static void main( String[] args ) throws Exception {
 
-        System.setProperty("node.bootstrap", FrascatiRunner.class.getClassLoader().getResource("fbootstrap.kev").getPath());
+        System.setProperty("node.bootstrap", FrascatiRunner.class.getClassLoader().getResource("SecondBootStrap.kev").getPath());
         System.setProperty("node.name", "node0");
         System.setProperty("node.log.level","DEBUG");
+        System.setProperty("node.update.timeout","100000");
+
         App.main(args);
 
     }
