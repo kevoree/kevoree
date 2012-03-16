00d7066c67a790af4d16a97fa7461ff5f2e910f6
François Fouquet <fouquet.f@gmail.com>
2012-03-16 11:14:36 +0100

    add KCL improve

--- kevoree-core/org.kevoree.kcl/src/main/java/org/xeustechnologies/jcl/AbstractClassLoader.java
+++ kevoree-core/org.kevoree.kcl/src/main/java/org/xeustechnologies/jcl/AbstractClassLoader.java
@@ -35,11 +35,7 @@ import org.xeustechnologies.jcl.utils.Utils;
 @SuppressWarnings("unchecked")
 public abstract class AbstractClassLoader extends ClassLoader {
 
-
-
-
     protected final List<ProxyClassLoader> loaders = new ArrayList<ProxyClassLoader>();
-
     private final ProxyClassLoader systemLoader = new SystemLoader();
     private final ProxyClassLoader parentLoader = new ParentLoader();
     private final ProxyClassLoader currentLoader = new CurrentLoader();
