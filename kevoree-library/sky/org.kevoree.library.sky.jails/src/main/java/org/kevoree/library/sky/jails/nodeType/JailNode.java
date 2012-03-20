ddc42d042ce395fc9be27ce7731d5500507442bb
Erwan Daubert <Erwan.Daubert@gmail.com>
2012-03-20 19:55:31 +0100

    fix jailNode to take into accound dictionary update + fix kloud configuration

--- kevoree-library/sky/org.kevoree.library.sky.jails/src/main/java/org/kevoree/library/sky/jails/nodeType/JailNode.java
+++ kevoree-library/sky/org.kevoree.library.sky.jails/src/main/java/org/kevoree/library/sky/jails/nodeType/JailNode.java
@@ -1,7 +1,9 @@
 package org.kevoree.library.sky.jails.nodeType;
 
-import org.kevoree.ContainerRoot;
-import org.kevoree.annotation.*;
+import org.kevoree.annotation.DictionaryAttribute;
+import org.kevoree.annotation.DictionaryType;
+import org.kevoree.annotation.Library;
+import org.kevoree.annotation.NodeType;
 import org.kevoree.library.sky.jails.JailKevoreeNodeRunner;
 import org.kevoree.library.sky.manager.KevoreeNodeRunner;
 import org.kevoree.library.sky.manager.nodeType.IaaSNode;
@@ -21,29 +23,31 @@ import org.slf4j.LoggerFactory;
 @DictionaryType({
 		@DictionaryAttribute(name = "inet", defaultValue = "alc0", optional = false),
 		@DictionaryAttribute(name = "subnet", defaultValue = "10.0.0.0", optional = false),
-		@DictionaryAttribute(name = "mask", defaultValue = "24", vals={"8", "16", "24"}, optional = false),
+		@DictionaryAttribute(name = "mask", defaultValue = "24", vals = {"8", "16", "24"}, optional = false),
 		@DictionaryAttribute(name = "flavor", defaultValue = "kevjail", optional = false)
 })
 @NodeType
 public class JailNode extends IaaSNode {
 	private static final Logger logger = LoggerFactory.getLogger(JailNode.class);
 
-	/*@Start
 	@Override
-	public void startNode () {
-		super.startNode();
+	public KevoreeNodeRunner createKevoreeNodeRunner (String nodeName) {
+		return new JailKevoreeNodeRunner(this);
 	}
 
-	@Stop
-	@Override
-	public void stopNode () {
-		logger.debug("stopping node {}", this.getNodeName());
-		super.stopNode();
-	}*/
+	public String getNetworkInterface () {
+		return this.getDictionary().get("inet").toString();
+	}
 
-    @Override
-    public KevoreeNodeRunner createKevoreeNodeRunner(String nodeName) {
-        return new JailKevoreeNodeRunner(nodeName, this.getDictionary().get("inet").toString(),
-      				this.getDictionary().get("subnet").toString(), this.getDictionary().get("mask").toString(), this.getDictionary().get("flavor").toString(),this);
-    }
+	public String getNetwork () {
+		return this.getDictionary().get("subnet").toString();
+	}
+
+	public String getMask () {
+		return this.getDictionary().get("mask").toString();
+	}
+
+	public String getFlavor () {
+		return this.getDictionary().get("flavor").toString();
+	}
 }
