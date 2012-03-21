6f24acc17005ec2066004c3154de4322771873fc
barais@irisa.fr <barais@irisa.fr>
2012-03-21 11:34:53 +0100

    last

---
+++ kevoree-library/javase/org.kevoree.library.javase.webserver.markdown2html/src/main/java/org/kevoree/library/javase/webserver/markdown2htmlwar/client/MainWindows.java
@@ -0,0 +1,55 @@
+package org.kevoree.library.javase.webserver.markdown2htmlwar.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.RichTextArea;
+import com.google.gwt.user.client.ui.Widget;
+
+	 class MainWindows extends Composite {
+
+	private static MainWindowsUiBinder uiBinder = GWT
+			.create(MainWindowsUiBinder.class);
+	@UiField RichTextArea richTextArea;
+	@UiField HTML html;
+
+	private final MarkDown2HtmlServiceAsync serv = GWT.create(MarkDown2HtmlService.class);
+
+	
+	interface MainWindowsUiBinder extends UiBinder<Widget, MainWindows> {
+	}
+
+	public MainWindows() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+
+	public MainWindows(String firstName) {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+
+
+	@UiHandler("richTextArea")
+	void onRichTextAreaKeyPress(KeyPressEvent event) {
+		serv.markdown2html(richTextArea.getText(), new AsyncCallback<String>() {
+			
+			@Override
+			public void onSuccess(String arg0) {
+				html.setHTML(arg0);	
+			}
+			
+			@Override
+			public void onFailure(Throwable arg0) {
+				Window.alert("toto");
+				
+			}
+		});
+	}
+}
