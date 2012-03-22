a5e759130e442c8835e34759b4c1d4f17f422f50
François Fouquet <fouquet.f@gmail.com>
2012-03-22 14:48:20 +0100

    minor fix War Manager

--- kevoree-library/javase/org.kevoree.library.javase.webserver.gitblit/src/main/java/org/kevoree/library/javase/webserver/gitblit/GitBlitServer.java
+++ kevoree-library/javase/org.kevoree.library.javase.webserver.gitblit/src/main/java/org/kevoree/library/javase/webserver/gitblit/GitBlitServer.java
@@ -2,6 +2,7 @@ package org.kevoree.library.javase.webserver.gitblit;
 
 import org.kevoree.annotation.*;
 import org.kevoree.framework.AbstractComponentType;
+import org.kevoree.framework.WebInfHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,9 +20,8 @@ import java.util.UUID;
 @Library(name = "JavaSE")
 @DictionaryType({
         @DictionaryAttribute(name = "port", defaultValue = "8080"),
-        @DictionaryAttribute(name = "home", defaultValue = "", optional = true)
+        @DictionaryAttribute(name = "gitHome", defaultValue = "", optional = true)
 })
-
 @ComponentType
 public class GitBlitServer extends AbstractComponentType {
 
@@ -30,23 +30,37 @@ public class GitBlitServer extends AbstractComponentType {
 
     @Start
     public void startServer() throws ClassNotFoundException, NoSuchMethodException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException, InvocationTargetException, InstantiationException, IOException {
-
         Map args = new HashMap();
-        //args.put("preferredClassLoader",KevWebappClassLoader.class.getName());
-        //args.put("preferredClassLoader",KevWebappClassLoader.class.getName());
-        KevLauncher.initLogger(args);
         args.put("ajp13Port", "-1");
-
         java.io.File tempWarDir = java.io.File.createTempFile("-t-", "-t-");
         tempWarDir.delete();
         tempWarDir.mkdirs();
-
         org.kevoree.framework.FileNIOHelper.unzipToTempDir(this.getClass().getClassLoader().getResourceAsStream("gitblit-0.8.2.war"), tempWarDir, java.util.Arrays.asList(".filtered"), java.util.Arrays.asList(".filtered"));
+        
+        HashMap<String,Object> dico = getDictionary();
+        
+        if(this.getDictionary().get("gitHome") != null && !this.getDictionary().get("gitHome").equals("")){
+            File gitHome = new File(dico.get("gitHome").toString());
+            if(!gitHome.exists()){
+                gitHome.mkdirs();
+            }
+            dico.put("git.repositoriesFolder",gitHome.getAbsolutePath());
+        } else {
+            File tempUserDir = new File(System.getProperty("java.io.tmpdir")+File.separator+"gitBlitHome"+getName());
+            tempUserDir.mkdirs();
+            dico.put("git.repositoriesFolder",tempUserDir.getAbsolutePath());
+        }
+        
+        if(!dico.get("git.repositoriesFolder").toString().endsWith(File.separator)){
+           dico.put("git.repositoriesFolder",dico.get("git.repositoriesFolder")+File.separator);
+        }
+        
+
+        WebInfHelper.setWebInfParams(new File(tempWarDir+File.separator+"WEB-INF"),dico);
         args.put("webroot", tempWarDir.getAbsolutePath());
         args.put("httpPort",this.getDictionary().get("port"));
         winstone = new KevLauncher(args);
     }
-
 
     @Stop
     public void stopServer() {
