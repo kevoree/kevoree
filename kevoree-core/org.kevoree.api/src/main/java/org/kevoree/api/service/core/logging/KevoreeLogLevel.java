a1f295d487ed17b96d3085eaa2c6976b82ac8e0a
François Fouquet <fouquet.f@gmail.com>
2012-03-23 13:47:37 +0100

    a dd LogService API

---
+++ kevoree-core/org.kevoree.api/src/main/java/org/kevoree/api/service/core/logging/KevoreeLogLevel.java
@@ -0,0 +1,13 @@
+package org.kevoree.api.service.core.logging;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: duke
+ * Date: 23/03/12
+ * Time: 13:42
+ */
+public enum KevoreeLogLevel {
+
+    ERROR, WARN, INFO, DEBUG, FINE;
+
+}
