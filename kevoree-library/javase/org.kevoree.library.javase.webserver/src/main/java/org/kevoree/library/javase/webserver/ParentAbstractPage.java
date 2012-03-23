d962250824c3cb9126400ca8483d89625f6cc158
Erwan Daubert <Erwan.Daubert@gmail.com>
2012-03-23 16:32:50 +0100

    fix ParentPage

--- kevoree-library/javase/org.kevoree.library.javase.webserver/src/main/java/org/kevoree/library/javase/webserver/ParentAbstractPage.java
+++ kevoree-library/javase/org.kevoree.library.javase.webserver/src/main/java/org/kevoree/library/javase/webserver/ParentAbstractPage.java
@@ -4,7 +4,6 @@ import org.kevoree.annotation.ComponentFragment;
 import org.kevoree.annotation.Library;
 import org.kevoree.annotation.Start;
 import org.kevoree.annotation.Update;
-import scala.Option;
 
 /**
  * User: Erwan Daubert - erwan.daubert@gmail.com
@@ -21,22 +20,14 @@ public class ParentAbstractPage extends AbstractPage {
 	@Override
 	@Start
 	public void startPage () {
-		handler.initRegex(this.getDictionary().get("urlpattern").toString() + "**");
-		logger.debug("Parent abstract page start with pattern = {}", this.getDictionary().get("urlpattern").toString() + "**");
+		this.getDictionary().put("urlpattern", this.getDictionary().get("urlpattern").toString() + "**");
+		logger.debug("Parent abstract page start with pattern = {}", this.getDictionary().get("urlpattern").toString());
+		super.startPage();
 	}
 
 	@Override
 	@Update
 	public void updatePage () {
-		handler.initRegex(this.getDictionary().get("urlpattern").toString() + "**");
-	}
-
-	public String getLastParam (String url) {
-		Option<String> result = handler.getLastParam(url, this.getDictionary().get("urlpattern").toString() + "**");
-		if (result.isDefined()) {
-			return result.get();
-		} else {
-			return null;
-		}
+		startPage();
 	}
 }
