35ce20c6e0725f6f4f2528fb83254b59fa894f24
JeD <jedartois@gmail.com>
2012-03-27 11:56:37 +0200

    improve transformation primitive 3 byte to 1 byte

--- kevoree-library/arduino/org.kevoree.library.arduino.nodeType/src/main/resources/templates/KevFrameworkHeaders.c
+++ kevoree-library/arduino/org.kevoree.library.arduino.nodeType/src/main/resources/templates/KevFrameworkHeaders.c
@@ -1,18 +1,19 @@
-
 #include <QueueList.h>
-#include <avr/pgmspace.h> 
+#include <avr/pgmspace.h>
 #include <avr/wdt.h>
-#define UDI_C 0 
-#define AIN_C 1 
-#define RIN_C 2 
-#define ABI_C 3 
-#define RBI_C 4 
-#define kprint(x) KevSerialPrint_P(PSTR(x)) 
-#define kprintln(x) KevSerialPrintln_P(PSTR(x)) 
+
+#define kprint(x) KevSerialPrint_P(PSTR(x))
+#define kprintln(x) KevSerialPrintln_P(PSTR(x))
 #define KNOINLINE __attribute__((noinline))
 #define BUFFERSIZE 100
-
 #define MAX_UNTYPED_DICTIONARY 20
+
+//transformation to modify the runtime    example : $4{0:T1:period=100}
+const char UDI_C = '0';
+const char AIN_C = '1';
+const char RIN_C = '2';
+const char ABI_C = '3';
+const char RBI_C = '4';
 
 int serialIndex = 0;
 char inBytes[BUFFERSIZE];
@@ -20,7 +21,7 @@ const char startBAdminChar = '{';
 const char endAdminChar = '}';
 const char startAdminChar = '$';
 const char sepAdminChar = '/';
-
+const char delimitation = ':';
 
 #define MSGMAXSIZE 15
 #define MSGBUFFERSIZE 4
@@ -30,5 +31,3 @@ int currentMsgBufIndex = -1;
 int currentMsgIndex = -1;
 boolean messageInProgress = false;
 boolean instanceNameRead = false;
-
-
--- kevoree-library/arduino/org.kevoree.library.arduino.nodeType/src/main/resources/templates/KevScriptParser.c
+++ kevoree-library/arduino/org.kevoree.library.arduino.nodeType/src/main/resources/templates/KevScriptParser.c
@@ -1,83 +1,107 @@
 
-char * insID;
-char * typeID;
-char * params;
-char * chID;
-char * portID;
+char *insID;
+char *typeID;
+char *params;
+char *chID;
+char *portID;
 const char delims[] = ":";
-boolean parseAndSaveAdminMsg()
+boolean
+parseAndSaveAdminMsg ()
 {
-	if( inBytes[0]=='u' && inBytes[1]=='d' && inBytes[2]=='i' && inBytes[3]==':' ){
-		insID = strtok(&inBytes[4], delims);
-		params = strtok(NULL, delims);
-		saveUDI_CMD(insID,params);
-		return true;
-	}
-	if( inBytes[0]=='a' && inBytes[1]=='i' && inBytes[2]=='n' && inBytes[3]==':' ){
-		insID = strtok(&inBytes[4], delims);
-		typeID = strtok(NULL, delims);
-		params = strtok(NULL, delims);
-		saveAIN_CMD(insID,getIDFromType(typeID),params);
-		return true;
-	}
-	if( inBytes[0]=='r' && inBytes[1]=='i' && inBytes[2]=='n' && inBytes[3]==':' ){
-		insID = strtok(&inBytes[4], delims);
-		saveRIN_CMD(insID);
-		return true;
-	}
-	if( inBytes[0]=='a' && inBytes[1]=='b' && inBytes[2]=='i' && inBytes[3]==':' ){
-		insID = strtok(&inBytes[4], delims);
-		chID = strtok(NULL, delims);
-		portID = strtok(NULL, delims);
-		saveBI_CMD(true,insID,chID,getIDFromPortName(portID));
-		return true;
-	}
-	if( inBytes[0]=='r' && inBytes[1]=='b' && inBytes[2]=='i' && inBytes[3]==':' ){
-		insID = strtok(&inBytes[4], delims);
-		chID = strtok(NULL, delims);
-		portID = strtok(NULL, delims);
-		saveBI_CMD(false,insID,chID,getIDFromPortName(portID));
-		return true;
-	}
-	return false;
+  //  UDI_C
+  if (inBytes[0] == UDI_C && inBytes[1] == delimitation)
+    {
+      insID = strtok (&inBytes[2], delims);
+      params = strtok (NULL, delims);
+      saveUDI_CMD (insID, params);
+      return true;
+    }
+  //  AIN_C
+  if (inBytes[0] == AIN_C && inBytes[1] == delimitation)
+    {
+      insID = strtok (&inBytes[2], delims);
+      typeID = strtok (NULL, delims);
+      params = strtok (NULL, delims);
+      saveAIN_CMD (insID, getIDFromType (typeID), params);
+      return true;
+    }
+  //RIN_C
+  if (inBytes[0] == RIN_C && inBytes[1] == delimitation)
+    {
+      insID = strtok (&inBytes[2], delims);
+      saveRIN_CMD (insID);
+      return true;
+    }
+  //ABI_C
+  if (inBytes[0] == ABI_C && inBytes[1] == delimitation)
+    {
+      insID = strtok (&inBytes[2], delims);
+      chID = strtok (NULL, delims);
+      portID = strtok (NULL, delims);
+      saveBI_CMD (true, insID, chID, getIDFromPortName (portID));
+      return true;
+    }
+  //RBI_C
+  if (inBytes[0] == RBI_C && inBytes[1] == delimitation)
+    {
+      insID = strtok (&inBytes[2], delims);
+      chID = strtok (NULL, delims);
+      portID = strtok (NULL, delims);
+      saveBI_CMD (false, insID, chID, getIDFromPortName (portID));
+      return true;
+    }
+  return false;
 }
 
-int parse_array_int(int size,char *dico, int val[])
+int
+parse_array_int (int size, char *dico, int val[])
 {
 #define MAX_SIZE_INT 6
-	int count=0,i=0,j=0;
-	char parsing[MAX_SIZE_INT];
-	// init array val
-	for(i=0;i<size;i++)
+  int count = 0, i = 0, j = 0;
+  char parsing[MAX_SIZE_INT];
+  // init array val
+  for (i = 0; i < size; i++)
+    {
+      val[i] = 0;
+    }
+  if ((int) strlen (dico) > 1)
+    {
+      for (i = 0; i < (int) strlen (dico) + 1; i++)
 	{
-		val[i] = 0;
-	}
-	if((int)strlen(dico) > 1)
-	{
-		for(i=0;i<(int)strlen(dico)+1;i++)
+	  if (dico[i] != ';' && i != strlen (dico))
+	    {
+	      if (j < MAX_SIZE_INT)
 		{
-			if(dico[i] != ';' && i !=strlen(dico))
-			{
-				if(j <MAX_SIZE_INT)	{ parsing[j] = dico[i];	j++;}	else {strcpy(parsing,"0");}
-			}
-			else
-			{
-				val[count] = atoi(parsing);
-				memset(parsing, 0, sizeof(parsing));
-				count++;
-				if(count > size){		/* the dico is bigger */	return -2;	}
-				j=0;
-			}
+		  parsing[j] = dico[i];
+		  j++;
 		}
+	      else
+		{
+		  strcpy (parsing, "0");
+		}
+	    }
+	  else
+	    {
+	      val[count] = atoi (parsing);
+	      memset (parsing, 0, sizeof (parsing));
+	      count++;
+	      if (count > size)
+		{		/* the dico is bigger */
+		  return -2;
+		}
+	      j = 0;
+	    }
 	}
-	else
-	{
-		/* the dico is empty */
-		return -1;
-	}
+    }
+  else
+    {
+      /* the dico is empty */
+      return -1;
+    }
 }
 
-int parse_IntList4(char *dico, int val[])
+int
+parse_IntList4 (char *dico, int val[])
 {
-	return parse_array_int(4,dico,val);
+  return parse_array_int (4, dico, val);
 }
