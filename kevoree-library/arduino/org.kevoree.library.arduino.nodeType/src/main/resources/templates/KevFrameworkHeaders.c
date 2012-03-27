115fc2e30a4406f2a47d3b0e4d83c1dd7e91eb4a
JeD <jedartois@gmail.com>
2012-03-27 10:39:49 +0200

    clean and fix push model

--- kevoree-library/arduino/org.kevoree.library.arduino.nodeType/src/main/resources/templates/KevFrameworkHeaders.c
+++ kevoree-library/arduino/org.kevoree.library.arduino.nodeType/src/main/resources/templates/KevFrameworkHeaders.c
@@ -1,34 +1,34 @@
 
-     #include <QueueList.h>
-     #include <avr/pgmspace.h> 
-     #include <avr/wdt.h>
-     #define UDI_C 0 
-     #define AIN_C 1 
-     #define RIN_C 2 
-     #define ABI_C 3 
-     #define RBI_C 4 
-     #define kprint(x) KevSerialPrint_P(PSTR(x)) 
-     #define kprintln(x) KevSerialPrintln_P(PSTR(x)) 
-     #define KNOINLINE __attribute__((noinline))
-     #define BUFFERSIZE 100
+#include <QueueList.h>
+#include <avr/pgmspace.h> 
+#include <avr/wdt.h>
+#define UDI_C 0 
+#define AIN_C 1 
+#define RIN_C 2 
+#define ABI_C 3 
+#define RBI_C 4 
+#define kprint(x) KevSerialPrint_P(PSTR(x)) 
+#define kprintln(x) KevSerialPrintln_P(PSTR(x)) 
+#define KNOINLINE __attribute__((noinline))
+#define BUFFERSIZE 100
 
-     #define MAX_UNTYPED_DICTIONARY 20
+#define MAX_UNTYPED_DICTIONARY 20
 
-     int serialIndex = 0;
-     char inBytes[BUFFERSIZE];
-     const char startBAdminChar = '{';
-     const char endAdminChar = '}';
-     const char startAdminChar = '$';
-     const char sepAdminChar = '/';
+int serialIndex = 0;
+char inBytes[BUFFERSIZE];
+const char startBAdminChar = '{';
+const char endAdminChar = '}';
+const char startAdminChar = '$';
+const char sepAdminChar = '/';
 
 
-     #define MSGMAXSIZE 15
-     #define MSGBUFFERSIZE 4
-     int currentInstanceID = -1;
-     char msgBytes[MSGBUFFERSIZE][MSGMAXSIZE];
-     int currentMsgBufIndex = -1;
-     int currentMsgIndex = -1;
-     boolean messageInProgress = false;
-     boolean instanceNameRead = false;
+#define MSGMAXSIZE 15
+#define MSGBUFFERSIZE 4
+int currentInstanceID = -1;
+char msgBytes[MSGBUFFERSIZE][MSGMAXSIZE];
+int currentMsgBufIndex = -1;
+int currentMsgIndex = -1;
+boolean messageInProgress = false;
+boolean instanceNameRead = false;
 
 
