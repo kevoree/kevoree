72a5cdd6a8b5cfff942e27416eef24ed225ffea2
JeD <jedartois@gmail.com>
2012-03-28 18:19:12 +0200

    Initial commit push model arduino

--- kevoree-library/arduino/org.kevoree.library.arduino.nodeType/src/main/resources/templates/KevScriptPersistenceStorageEEPROM.c
+++ kevoree-library/arduino/org.kevoree.library.arduino.nodeType/src/main/resources/templates/KevScriptPersistenceStorageEEPROM.c
@@ -17,3 +17,20 @@ save2Memory (byte b)
   EEPROM.write (eepromIndex, b);
   eepromIndex++;
 }
+
+int
+eepromReadInt (int address)
+{
+  int value = 0x0000;
+  value = value | (EEPROM.read (address) << 8);
+  value = value | EEPROM.read (address + 1);
+  return value;
+}
+
+void
+save2MemoryInt (int value)
+{
+  EEPROM.write (eepromIndex, (value >> 8) & 0xFF);
+  EEPROM.write (eepromIndex + 1, value & 0xFF);
+  eepromIndex = eepromIndex + 2;
+}
