67688411625e021b8671e2acec8fbcfc480653c4
JeD <jedartois@gmail.com>
2012-04-02 10:30:57 +0200

    no msg

---
+++ kevoree-library/arduino/org.kevoree.library.arduino.nodeType/src/main/scala/org/kevoree/library/arduinoNodeType/Tester.scala
@@ -0,0 +1,23 @@
+package org.kevoree.library.arduinoNodeType
+
+import org.kevoree.framework.KevoreeXmiHelper
+
+/**
+ * Created by jed
+ * User: jedartois@gmail.com
+ * Date: 30/03/12
+ * Time: 14:25
+ */
+
+object Tester extends  App {
+
+  val model = KevoreeXmiHelper.load("/home/jed/Desktop/model.kev")
+
+
+
+  ArduinoModelGetHelper.getCurrentModel(model, "node0","/dev/ttyACM0")
+
+
+  
+  
+}
