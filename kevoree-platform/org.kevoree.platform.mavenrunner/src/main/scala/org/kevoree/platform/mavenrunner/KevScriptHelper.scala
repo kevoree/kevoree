9619f4ef7a69fcbe7a613d9b796dce11043e0afa
François Fouquet <fouquet.f@gmail.com>
2012-04-12 00:54:02 +0200

    init kev runner
    improve TJWS
    
    
    Former-commit-id: 4476454f787c3af0f26db8ebdbcd6e6987597d40

---
+++ kevoree-platform/org.kevoree.platform.mavenrunner/src/main/scala/org/kevoree/platform/mavenrunner/KevScriptHelper.scala
@@ -0,0 +1,66 @@
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kevoree.platform.mavenrunner
+
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.{BufferedReader, FileReader, File}
+import org.kevoree.tools.marShell.KevScriptOfflineEngine
+import org.kevoree.{ContainerRoot, KevoreeFactory}
+
+/**
+ * User: Erwan Daubert - erwan.daubert@gmail.com
+ * Date: 16/02/12
+ * Time: 17:24
+ *
+ * @author Erwan Daubert
+ * @version 1.0
+ */
+
+object KevScriptHelper extends App {
+
+  def generate(scriptFile: File): ContainerRoot = {
+    val kevEngine = new KevScriptOfflineEngine(KevoreeFactory.createContainerRoot)
+    kevEngine.addVariable("kevoree.version", KevoreeFactory.getVersion)
+    kevEngine.append(loadScript(scriptFile))
+    kevEngine.interpret()
+  }
+
+
+  private def loadScript(file: File): String = {
+    val fileReader = new BufferedReader(new FileReader(file))
+    val scriptBuilder = new StringBuilder
+    var line = fileReader.readLine()
+
+    while (line != null) {
+      scriptBuilder append line + "\n"
+      line = fileReader.readLine()
+    }
+
+    fileReader.close()
+    scriptBuilder.toString()
+  }
+}
