fff3f2529239ba3139fe6ab68eb7352d29f52312
Erwan Daubert <Erwan.Daubert@gmail.com>
2012-04-19 16:38:24 +0200

    fix AetherUtil to manage LATEST version
    Former-commit-id: 19af0bbff16b82dbba5eb08fb4c7d1e99aca2f4c

--- kevoree-tools/org.kevoree.tools.aether.framework/src/main/scala/org/kevoree/tools/aether/framework/AetherUtil.scala
+++ kevoree-tools/org.kevoree.tools.aether.framework/src/main/scala/org/kevoree/tools/aether/framework/AetherUtil.scala
@@ -11,6 +11,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.kevoree.tools.aether.framework
 
@@ -58,6 +71,8 @@ object AetherUtil extends TempFileCacheManager {
   def resolveKevoreeArtifact (unitName: String, groupName: String, version: String): File = {
     if (version.endsWith("SNAPSHOT")) {
       resolveMavenArtifact(unitName, groupName, version, List("http://maven.kevoree.org/snapshots/"))
+    } else if (version.equals("LATEST")) {
+      resolveMavenArtifact(unitName, groupName, version, List("http://maven.kevoree.org/release/", "http://maven.kevoree.org/snapshots/"))
     } else {
       resolveMavenArtifact(unitName, groupName, version, List("http://maven.kevoree.org/release/"))
     }
