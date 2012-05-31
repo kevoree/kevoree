14fdb2915b08f8046b52ef650e50ec374493a494
François Fouquet <fouquet.f@gmail.com>
2012-05-31 22:21:15 +0200

    minor fix

--- kevoree-tools/org.kevoree.tools.aether.framework/src/main/scala/org/kevoree/tools/aether/framework/JCLContextHandler.scala
+++ kevoree-tools/org.kevoree.tools.aether.framework/src/main/scala/org/kevoree/tools/aether/framework/JCLContextHandler.scala
@@ -11,6 +11,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.kevoree.tools.aether.framework
 
 /**
@@ -249,25 +262,28 @@ class JCLContextHandler extends DaemonActor with KevoreeClassLoaderHandler {
           }
 
           val toRemoveKCL = kcl_cache.get(key)
-
-          //GET
-          var rootGroup = Thread.currentThread().getThreadGroup
-          var parentGroup: ThreadGroup = null
-          while ( {
-            parentGroup = rootGroup.getParent; parentGroup
-          } != null) {
-            rootGroup = parentGroup
-          }
-          val numThreads = rootGroup.activeCount()
-          val listOfThreads = new Array[Thread](numThreads)
-          rootGroup.enumerate(listOfThreads)
-          for (i <- 0 until numThreads) {
-            val tloop = listOfThreads(i)
-            if (tloop.getContextClassLoader == toRemoveKCL) {
-              logger.warn("Change Thread " + tloop.getName + " currentClassLoader to avoid memory leak")
-              tloop.setContextClassLoader(getClass.getClassLoader)
+          try {
+            var rootGroup = Thread.currentThread().getThreadGroup
+            var parentGroup: ThreadGroup = null
+            while ( {
+              parentGroup = rootGroup.getParent
+              parentGroup
+            } != null) {
+              rootGroup = parentGroup
             }
+            val numThreads = rootGroup.activeCount()
+            val listOfThreads = new Array[Thread](numThreads)
+            rootGroup.enumerate(listOfThreads)
+            for (i <- 0 until numThreads) {
+              val tloop = listOfThreads(i)
+              if (tloop != null && tloop.getContextClassLoader == toRemoveKCL) {
+                logger.warn("Change Thread " + tloop.getName + " currentClassLoader to avoid memory leak")
+                tloop.setContextClassLoader(getClass.getClassLoader)
+              }
 
+            }
+          } catch {
+            case _ =>
           }
           toRemoveKCL.unload()
           kcl_cache.remove(key)
