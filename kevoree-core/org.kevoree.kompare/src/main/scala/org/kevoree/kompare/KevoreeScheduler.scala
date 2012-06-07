e4ebe07b58b1d3ca7ad3b36c5d595364b6de141e
François Fouquet <fouquet.f@gmail.com>
2012-06-07 10:07:29 +0200

    improve marshell to better implement inner block

--- kevoree-core/org.kevoree.kompare/src/main/scala/org/kevoree/kompare/KevoreeScheduler.scala
+++ kevoree-core/org.kevoree.kompare/src/main/scala/org/kevoree/kompare/KevoreeScheduler.scala
@@ -11,6 +11,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.kevoree.kompare
 
 import scheduling.SchedulingWithTopologicalOrderAlgo
@@ -28,7 +41,7 @@ import org.slf4j.LoggerFactory
 
 trait KevoreeScheduler {
 
-  def plan (adaptionModel: AdaptationModel, nodeName: String): AdaptationModel = {
+  def plan(adaptionModel: AdaptationModel, nodeName: String): AdaptationModel = {
     if (!adaptionModel.getAdaptations.isEmpty) {
 
       val scheduling = new SchedulingWithTopologicalOrderAlgo
