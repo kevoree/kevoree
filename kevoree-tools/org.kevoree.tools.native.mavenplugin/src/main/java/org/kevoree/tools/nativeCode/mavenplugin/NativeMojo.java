8b143fa33d05ddc525a90d29287890c5946c31a2
François Fouquet <fouquet.f@gmail.com>
2012-08-08 15:13:22 +0200

    init native framework

---
+++ kevoree-tools/org.kevoree.tools.native.mavenplugin/src/main/java/org/kevoree/tools/nativeCode/mavenplugin/NativeMojo.java
@@ -0,0 +1,106 @@
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.kevoree.tools.nativeCode.mavenplugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ffouque
+ * @author <a href="mailto:ffouquet@irisa.fr">Fouquet François</a>
+ * @version $Id$
+ * @goal generate
+ * @phase generate-sources
+ * @requiresDependencyResolution compile
+ */
+public class NativeMojo extends AbstractMojo {
+
+    /**
+     * The directory root under which processor-generated source files will be placed; files are placed in
+     * subdirectories based on package namespace. This is equivalent to the <code>-s</code> argument for apt.
+     *
+     * @parameter default-value="${project.build.directory}/generated-sources/kevoree"
+     */
+    private File sourceOutputDirectory;
+
+    /**
+     *
+     * @parameter default-value="${basedir}/src/main"
+     */
+    private File inputCFile;
+
+
+    /**
+     * POM
+     *
+     * @parameter expression="${project}"
+     * @readonly
+     * @required
+     */
+    protected MavenProject project;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+        if(inputCFile == null || !(inputCFile.exists())){
+            getLog().warn("InputDir null => ");
+        } else {
+            List<File> componentFiles = scanForCcomponentFile(inputCFile);
+            for(File f : componentFiles){
+                getLog().info("File found =>"+f.getAbsolutePath());
+            }
+        }
+
+
+
+        //TODO
+    }
+
+    /* Check file header */
+    private List<File> scanForCcomponentFile(File modelDir) {
+        List<File> models = new ArrayList();
+        for (File f : modelDir.listFiles()) {
+            if(f.isDirectory()){
+                scanForCcomponentFile(f);
+            } else {
+                if (f.getName().endsWith(".c") || f.getName().endsWith(".cpp")) {
+                    models.add(f);
+                }
+            }
+        }
+        return models;
+    }
+}
