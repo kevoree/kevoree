66a4f4ef3589b05923377e524e6bc59891144b62
François Fouquet <fouquet.f@gmail.com>
2012-08-22 18:52:54 +0200

    etherpad

--- kevoree-corelibrary/javase/org.kevoree.library.javase.etherpad/src/main/java/org/kevoree/library/javase/etherpad/EtherPad.java
+++ kevoree-corelibrary/javase/org.kevoree.library.javase.etherpad/src/main/java/org/kevoree/library/javase/etherpad/EtherPad.java
@@ -1,5 +1,7 @@
 package org.kevoree.library.javase.etherpad;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.kevoree.annotation.ComponentType;
 import org.kevoree.annotation.DictionaryAttribute;
 import org.kevoree.annotation.DictionaryType;
@@ -7,8 +9,14 @@ import org.kevoree.framework.FileNIOHelper;
 import org.kevoree.library.javase.nodejs.AbstractNodeJSComponentType;
 
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 /**
  * Created with IntelliJ IDEA.
@@ -23,20 +31,76 @@ import java.util.Random;
 public class EtherPad extends AbstractNodeJSComponentType {
     @Override
     public String getMainFile() {
-        return "node_modules"+File.separator+"ep_etherpad-lite"+File.separator+"node"+File.separator+"server.js";
+        if(System.getProperty("os.name").toLowerCase().contains("win")){
+            return "node"+File.separator+"server.js";
+        } else {
+            return "node_modules"+File.separator+"ep_etherpad-lite"+File.separator+"node"+File.separator+"server.js";
+        }
     }
 
     @Override
     public String getMainDir() {
         File tempDir = createTempDir();
-        FileNIOHelper.unzipToTempDir(getClass().getClassLoader().getResourceAsStream("etherpad-lite.zip"),tempDir,new ArrayList<String>(),new ArrayList<String>());
-         logger.info("Extract EtherPad to dir : "+tempDir.getAbsolutePath());
+        try {
+            org.apache.commons.io.FileUtils.cleanDirectory(tempDir);
+        } catch (IOException e) {
+            logger.error("Error while cleaning dir");
+        }
+        if(System.getProperty("os.name").toLowerCase().contains("win")){
+            logger.info("Windows detected, downloading EtherPad-Win, Please wait");
+            try {
+                File outF = new File(tempDir,"etherpad-lite.zip");
+                org.apache.commons.io.FileUtils.forceMkdir(tempDir);
+                org.apache.commons.io.FileUtils.copyURLToFile(new URL("http://etherpad.org/etherpad-lite-win.zip"),outF);
+                logger.info("Download in {}",outF.getAbsolutePath());
+                unzip(outF,tempDir);
+                outF.delete();
+                org.apache.commons.io.FileUtils.moveDirectory(new File(tempDir.getAbsolutePath()+File.separator+"etherpad-lite-win"),new File(tempDir.getAbsolutePath()+File.separator+"etherpad-lite"));
+            } catch (Exception e) {
+                logger.error("Error while preparing etherpad lite windows ",e);
+            }
+        } else {
+            FileNIOHelper.unzipToTempDir(getClass().getClassLoader().getResourceAsStream("etherpad-lite.zip"), tempDir, new ArrayList<String>(), new ArrayList<String>());
+        }
+        logger.info("Extract EtherPad to dir : "+tempDir.getAbsolutePath());
         try {
             copyparam(tempDir);
         } catch (IOException e) {
             logger.error("Error while update param",e);
         }
         return tempDir.getAbsolutePath()+File.separator+"etherpad-lite";
+    }
+
+    public static List<File> unzip(File zipFile, File targetDir) throws IOException {
+        List<File> files = new ArrayList<File>();
+        ZipFile zip = new ZipFile(zipFile);
+        try {
+            zip = new ZipFile(zipFile);
+            for (ZipEntry entry : Collections.list(zip.entries())) {
+                if(entry.isDirectory()){
+
+                } else {
+                    InputStream input = zip.getInputStream(entry);
+                    try {
+                        if (!targetDir.exists()) targetDir.mkdirs();
+                        String cleanName = entry.getName();
+                        if(entry.getName().contains("?")){
+                            cleanName = cleanName.substring(0,cleanName.indexOf("?"));
+                            System.out.println("Clean "+cleanName);
+                        }
+                        File target = new File(targetDir,cleanName);
+                        FileUtils.copyInputStreamToFile(input, target);
+                        files.add(target);
+                    } finally {
+                        IOUtils.closeQuietly(input);
+                    }
+                }
+
+            }
+            return files;
+        } finally {
+            zip.close();
+        }
     }
 
     public File createTempDir() {
