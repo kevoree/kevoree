4fc7344d7d2ea4a55c540cb9ae48b9d94a9aa099
François Fouquet <fouquet.f@gmail.com>
2012-09-05 16:45:23 +0200

    cleanup

--- kevoree-corelibrary/javase/org.kevoree.library.javase.etherpad/src/main/java/org/kevoree/library/javase/etherpad/EtherPad.java
+++ kevoree-corelibrary/javase/org.kevoree.library.javase.etherpad/src/main/java/org/kevoree/library/javase/etherpad/EtherPad.java
@@ -103,18 +103,6 @@ public class EtherPad extends AbstractNodeJSComponentType {
         }
     }
 
-    public File createTempDir() {
-        final String baseTempPath = System.getProperty("java.io.tmpdir");
-        Random rand = new Random();
-        int randomInt = 1 + rand.nextInt();
-        File tempDir = new File(baseTempPath + File.separator + "tempDir" + randomInt);
-        if (tempDir.exists() == false) {
-            tempDir.mkdir();
-        }
-        tempDir.deleteOnExit();
-        return tempDir;
-    }
-
     public void copyparam(File target) throws IOException {
         Reader reader = new InputStreamReader(EtherPad.class.getClassLoader().getResourceAsStream("settings.json"));
         BufferedReader bread = new BufferedReader(reader);
