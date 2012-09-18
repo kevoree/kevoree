53003cf83d0860577755380720b7b4af45be83ad
Erwan Daubert <Erwan.Daubert@gmail.com>
2012-09-18 19:49:00 +0200

    fix miniKloud

--- kevoree-tools/org.kevoree.tools.ui.editor/src/main/scala/org/kevoree/tools/ui/editor/kloud/MiniKloudForm.scala
+++ kevoree-tools/org.kevoree.tools.ui.editor/src/main/scala/org/kevoree/tools/ui/editor/kloud/MiniKloudForm.scala
@@ -11,32 +11,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/**
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.kevoree.tools.ui.editor.kloud
 
 import java.io._
@@ -218,8 +192,8 @@ class MiniKloudForm(editor: KevoreeEditor, button: AbstractButton) {
         kevEngine.append("merge 'mvn:org.kevoree.corelibrary.model/org.kevoree.library.model.sky/{kevoree.version}'")
         kevEngine.append("addNode {minicloudNodeName}: MiniCloudNode {role='host', port='7000'}")
 
-        // add all user nodes as child of the minicloud node
-        editor.getPanel.getKernel.getModelHandler.getActualModel.getNodes.foreach {
+        // add all JavaSE (or inherited) user nodes as child of the minicloud node
+        nodes.foreach {
           node =>
             kevEngine.addVariable("nodeName", node.getName)
             kevEngine.append("addChild {nodeName}@editor_node")
