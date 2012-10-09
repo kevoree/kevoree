be507619e77c191589ed872fbd57a8a3c599336c
François Fouquet <fouquet.f@gmail.com>
2012-10-09 09:01:10 +0200

    cleanup

--- kevoree-corelibrary/sky/org.kevoree.library.sky.api/src/main/java/org/kevoree/library/sky/api/nodeType/AbstractIaaSNode.java
+++ kevoree-corelibrary/sky/org.kevoree.library.sky.api/src/main/java/org/kevoree/library/sky/api/nodeType/AbstractIaaSNode.java
@@ -1,5 +1,7 @@
 package org.kevoree.library.sky.api.nodeType;
 
+import org.kevoree.annotation.NodeFragment;
+
 /**
  * User: Erwan Daubert - erwan.daubert@gmail.com
  * Date: 03/10/12
@@ -8,5 +10,6 @@ package org.kevoree.library.sky.api.nodeType;
  * @author Erwan Daubert
  * @version 1.0
  */
+@NodeFragment
 public abstract class AbstractIaaSNode extends AbstractHostNode implements IaaSNode {
 }
