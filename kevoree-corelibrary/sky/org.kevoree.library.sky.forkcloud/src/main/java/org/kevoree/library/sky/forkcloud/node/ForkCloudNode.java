1e92ad50b21941394a74f2db7c071bd636336663
François Fouquet <fouquet.f@gmail.com>
2012-10-09 07:28:10 +0200

    m

--- kevoree-corelibrary/sky/org.kevoree.library.sky.forkcloud/src/main/java/org/kevoree/library/sky/forkcloud/node/ForkCloudNode.java
+++ kevoree-corelibrary/sky/org.kevoree.library.sky.forkcloud/src/main/java/org/kevoree/library/sky/forkcloud/node/ForkCloudNode.java
@@ -1,9 +1,9 @@
 package org.kevoree.library.sky.forkcloud.node;
 
-import com.sun.akuma.Daemon;
 import org.kevoree.annotation.Library;
 import org.kevoree.annotation.NodeType;
 import org.kevoree.library.sky.api.KevoreeNodeRunner;
+import org.kevoree.library.sky.api.nodeType.AbstractHostNode;
 import org.kevoree.library.sky.api.nodeType.IaaSNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,25 +16,11 @@ import org.slf4j.LoggerFactory;
  */
 @Library(name = "SKY")
 @NodeType
-public class ForkCloudNode extends IaaSNode {
+public class ForkCloudNode extends AbstractHostNode {
     private static final Logger logger = LoggerFactory.getLogger(ForkCloudNode.class);
-
-    Daemon d = new Daemon();
-
-    @Override
-    public void startNode() {
-        super.startNode();
-        if (d.isDaemonized()) {
-            try {
-                d.init("/var/run/kevoree/"+System.getProperty("node.name"));
-            } catch (Exception e) {
-                logger.error("Error while settings the pid file ", e);
-            }
-        }
-    }
 
     @Override
     public KevoreeNodeRunner createKevoreeNodeRunner(String nodeName) {
-        return new ForkCloudNodeRunner(nodeName, this, d);
+        return new ForkCloudNodeRunner(nodeName, this);
     }
 }
