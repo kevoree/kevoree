08d62f6942ff8031a13ac966a895d5323857ab55
JeD <jedartois@gmail.com>
2012-10-25 10:22:50 +0200

    update hello world

--- kevoree-corelibrary/native/org.kevoree.library.nativeN.HelloWorld/src/main/c/HelloWorld.c
+++ kevoree-corelibrary/native/org.kevoree.library.nativeN.HelloWorld/src/main/c/HelloWorld.c
@@ -5,12 +5,20 @@ extern void output_port(void *input);
 /* @Port(name = "input_port") */
 void input_port(void *input) {
 // USE INPUT
+	 output_port(input);
 }
 
 /*@Start*/
 int start()
 {
 	fprintf(stderr,"Component starting \n");
+
+	while(1){
+
+	 output_port((char*)"Hello World");
+
+	 sleep(1);
+	}
 
 return 0;
 }
