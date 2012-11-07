238f1a86b03b7f230b254b3593f1bbd31f4d2351
Erwan Daubert <Erwan.Daubert@gmail.com>
2012-11-07 20:38:00 +0100

    refactor

--- kevoree-corelibrary/sky/org.kevoree.library.sky.api/src/main/java/org/kevoree/library/sky/api/nodeType/AbstractIaaSNode.java
+++ kevoree-corelibrary/sky/org.kevoree.library.sky.api/src/main/java/org/kevoree/library/sky/api/nodeType/AbstractIaaSNode.java
@@ -1,6 +1,9 @@
 package org.kevoree.library.sky.api.nodeType;
 
 import org.kevoree.annotation.NodeFragment;
+import org.kevoree.api.service.core.script.KevScriptEngine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * User: Erwan Daubert - erwan.daubert@gmail.com
@@ -12,4 +15,21 @@ import org.kevoree.annotation.NodeFragment;
  */
 @NodeFragment
 public abstract class AbstractIaaSNode extends AbstractHostNode implements IaaSNode {
+	private static final Logger logger = LoggerFactory.getLogger(AbstractIaaSNode.class);
+
+	protected void updateModel (KevScriptEngine kengine) {
+		Boolean created = false;
+		for (int i = 0; i < 20; i++) {
+			try {
+				kengine.atomicInterpretDeploy();
+				created = true;
+				break;
+			} catch (Exception e) {
+				logger.warn("Error while try to update the configuration of node {}, try number {}", new String[]{getName(), i + ""});
+			}
+		}
+		if (!created) {
+			logger.error("After 20 attempt, it was not able to update the configuration of {}", getName());
+		}
+	}
 }
