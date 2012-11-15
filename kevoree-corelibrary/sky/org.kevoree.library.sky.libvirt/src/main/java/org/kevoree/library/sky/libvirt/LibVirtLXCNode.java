b15af569bec8027b9f0984a9d70c32326c74be50
François Fouquet <fouquet.f@gmail.com>
2012-11-15 09:38:37 +0100

    add lxc connection

--- kevoree-corelibrary/sky/org.kevoree.library.sky.libvirt/src/main/java/org/kevoree/library/sky/libvirt/LibVirtLXCNode.java
+++ kevoree-corelibrary/sky/org.kevoree.library.sky.libvirt/src/main/java/org/kevoree/library/sky/libvirt/LibVirtLXCNode.java
@@ -3,6 +3,10 @@ package org.kevoree.library.sky.libvirt;
 import org.kevoree.annotation.Library;
 import org.kevoree.annotation.NodeType;
 import org.kevoree.library.sky.api.KevoreeNodeRunner;
+import org.libvirt.Connect;
+import org.libvirt.LibvirtException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,6 +17,19 @@ import org.kevoree.library.sky.api.KevoreeNodeRunner;
 @Library(name = "SKY")
 @NodeType
 public class LibVirtLXCNode extends LibVirtNode {
+
+    private static final Logger logger = LoggerFactory.getLogger(LibVirtKvmNode.class);
+
+    @Override
+    public void startNode () {
+        try {
+            connection = new Connect("lxc:///", false);
+        } catch (LibvirtException e) {
+            logger.error("Unable to find the lxc daemon!", e);
+        }
+        super.startNode();
+    }
+
     @Override
     public KevoreeNodeRunner createKevoreeNodeRunner(String nodeName) {
         return new LibVirtLXCKevoreeNodeRunner(nodeName, this, connection);
