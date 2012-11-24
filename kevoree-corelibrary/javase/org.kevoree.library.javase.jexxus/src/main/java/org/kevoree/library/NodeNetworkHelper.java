a71b699ea32e359f33438478499053df5bec5678
François Fouquet <fouquet.f@gmail.com>
2012-11-24 19:10:41 +0100

    first draft of IPv4 detection. Todo => move this thing in the modelling layer

--- kevoree-corelibrary/javase/org.kevoree.library.javase.jexxus/src/main/java/org/kevoree/library/NodeNetworkHelper.java
+++ kevoree-corelibrary/javase/org.kevoree.library.javase.jexxus/src/main/java/org/kevoree/library/NodeNetworkHelper.java
@@ -9,6 +9,7 @@ import org.kevoree.framework.KevoreePlatformHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.Inet4Address;
 import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
 import java.util.Enumeration;
@@ -36,9 +37,8 @@ public class NodeNetworkHelper {
                 NetworkInterface networkInterface = networkInterfaces.nextElement();
                 if (!networkInterface.isLoopback()) {
                       for(InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()){
-                          if(interfaceAddress.getAddress().getHostAddress().length() == 13){
+                          if(interfaceAddress.getAddress() instanceof Inet4Address){
                               addresses.put(interfaceAddress.getAddress().getHostAddress(),networkInterface.getDisplayName());
-                              //System.out.println(">"+networkInterface.getDisplayName()+"->"+interfaceAddress.getAddress());
                           }
                       }
                 }
@@ -68,7 +68,6 @@ public class NodeNetworkHelper {
         if (ipObject != null && !ipObject.toString().equals("") && !ipObject.toString().equals("0.0.0.0")) {
             java.util.HashMap<String,String> addresses = new java.util.HashMap<String,String>();
             addresses.put(ipObject.toString(),"unknown");
-
             return addNetworkProperty(readWriteModel, group.getNodeName(),addresses, group.getKevScriptEngineFactory());
         } else {
             java.util.HashMap<String,String> addresses = getAddresses();
