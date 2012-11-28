ddbee26ad2aea8f3751992211d50e7b13389fc60
François Fouquet <fouquet.f@gmail.com>
2012-11-28 20:21:57 +0100

    add test

--- kevoree-platform/org.kevoree.platform.base/src/main/java/org/kevoree/platform/standalone/App.java
+++ kevoree-platform/org.kevoree.platform.base/src/main/java/org/kevoree/platform/standalone/App.java
@@ -37,8 +37,8 @@ public class App {
     public void start() throws Exception {
 
     //    System.setProperty("kevoree.log.level", "DEBUG");
-      //  System.setProperty("node.bootstrap","/Users/duke/Documents/dev/dukeboard/kevoree/kevoree-platform/org.kevoree.platform.standalone.min/boot.kevs");
-
+        System.setProperty("node.bootstrap","/Users/duke/Desktop/yop.kev");
+        System.setProperty("node.name","editor_node");
 
         //TO REMOVE
         if(System.getProperty("node.groupType") == null){
