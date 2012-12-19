2e70d6c3247b82be07194872c6c72808e65b39bd
Erwan Daubert <Erwan.Daubert@gmail.com>
2012-12-19 15:29:52 +0100

    add some logs and fix condition
    Former-commit-id: 77e91ae36fa43b7f516233a81265ef866be8ef88

--- kevoree-core/org.kevoree.basechecker/src/main/scala/org/kevoree/core/basechecker/RootChecker.scala
+++ kevoree-core/org.kevoree.basechecker/src/main/scala/org/kevoree/core/basechecker/RootChecker.scala
@@ -11,31 +11,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * 	http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.kevoree.core.basechecker
 
 import bindingchecker.BindingChecker
 import channelchecker.BoundsChecker
-import cyclechecker.{ComponentCycleChecker, NodeCycleChecker}
+import cyclechecker.ComponentCycleChecker
 import dictionaryChecker.{DictionaryNetworkPortChecker, DictionaryOptionalChecker}
 import kevoreeVersionChecker.KevoreeVersionChecker
 import namechecker.{IdChecker, NameChecker}
 import nodechecker.{NodeContainerChecker, NodeChecker}
 import org.kevoree.ContainerRoot
 import org.kevoree.api.service.core.checker.{CheckerViolation, CheckerService}
-import java.util.ArrayList
 import portchecker.PortChecker
 import org.slf4j.LoggerFactory
 import java.util
@@ -63,8 +49,6 @@ class RootChecker extends CheckerService {
           result.add(violation)
         }
       }
-
-
     })
     logger.debug("Model checked in "+(System.currentTimeMillis()-beginTime))
     result
