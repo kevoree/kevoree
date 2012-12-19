e2ce172be1d13fbf8bdcf2497700e7afac740d13
Erwan Daubert <Erwan.Daubert@gmail.com>
2012-12-19 15:27:35 +0100

    fix removeNode interpreter
    Former-commit-id: 7870a20a21181176da47c261a13b4b3231aa3e00

--- kevoree-tools/org.kevoree.tools.marShell/src/main/scala/org/kevoree/tools/marShell/interpreter/sub/KevsRemoveNodeInterpreter.scala
+++ kevoree-tools/org.kevoree.tools.marShell/src/main/scala/org/kevoree/tools/marShell/interpreter/sub/KevsRemoveNodeInterpreter.scala
@@ -11,23 +11,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package org.kevoree.tools.marShell.interpreter.sub
 
@@ -42,70 +25,79 @@ case class KevsRemoveNodeInterpreter(addN: RemoveNodeStatment) extends KevsAbstr
 
   var logger = LoggerFactory.getLogger(this.getClass)
 
+  def removeNode(targetNode: ContainerNode, context: KevsInterpreterContext): Boolean = {
+    //DELETE ALL GROUP DEPENDENCY
+    context.model.getGroups.foreach {
+      g =>
+        if (g.getSubNodes.contains(targetNode)) {
+          g.removeSubNodes(targetNode)
+        }
+    }
+
+    //DELETE ALL COMPONENT
+    targetNode.getComponents.toList.foreach(c => {
+      KevsRemoveComponentInstanceInterpreter(null).deleteComponent(targetNode, c)
+    })
+
+    //REMOVE FROM NETWORK LINK
+    context.model.getNodeNetworks.foreach {
+      nn =>
+        if (nn.getTarget.getName == addN.nodeName) {
+          context.model.removeNodeNetworks(nn)
+        } else {
+          nn.getInitBy.map {
+            initNode =>
+              if (initNode.getName == addN.nodeName) {
+                context.model.removeNodeNetworks(nn)
+              }
+          }
+        }
+    }
+
+    //CLEANUP HOST NODE
+    if (targetNode.getHost.isDefined) {
+      targetNode.getHost.get.removeHosts(targetNode)
+    }
+
+
+    //CLEANUP DICTIONARY
+    context.model.getHubs.foreach {
+      inst =>
+        inst.getDictionary.map {
+          dico =>
+            dico.getValues.filter(v => v.getTargetNode.isDefined && v.getTargetNode.get.getName == addN.nodeName).foreach {
+              value =>
+                dico.removeValues(value)
+            }
+        }
+    }
+    context.model.getGroups.foreach {
+      inst =>
+        inst.getDictionary.map {
+          dico =>
+            dico.getValues.filter(v => v.getTargetNode.isDefined && v.getTargetNode.get.getName == addN.nodeName).foreach {
+              value =>
+                dico.removeValues(value)
+            }
+        }
+    }
+
+    // DELETE hosted nodes
+    targetNode.getHosts.foreach {
+      childNode =>
+        KevsRemoveNodeInterpreter(null).removeNode(childNode, context)
+    }
+
+    //DELETE NODE
+    context.model.removeNodes(targetNode)
+    true
+  }
+
   def interpret(context: KevsInterpreterContext): Boolean = {
 
     context.model.findByQuery("nodes[" + addN.nodeName + "]", classOf[ContainerNode]) match {
       case Some(targetNode) => {
-        //DELETE ALL GROUP DEPENDENCY
-        context.model.getGroups.foreach {
-          g =>
-            if (g.getSubNodes.contains(targetNode)) {
-              g.removeSubNodes(targetNode)
-            }
-        }
-
-        //DELETE ALL COMPONENT
-        targetNode.getComponents.toList.foreach(c => {
-          KevsRemoveComponentInstanceInterpreter(null).deleteComponent(targetNode, c)
-        })
-
-        //REMOVE FROM NETWORK LINK
-        context.model.getNodeNetworks.foreach {
-          nn =>
-            if (nn.getTarget.getName == addN.nodeName) {
-              context.model.removeNodeNetworks(nn)
-            } else {
-              nn.getInitBy.map {
-                initNode =>
-                  if (initNode.getName == addN.nodeName) {
-                    context.model.removeNodeNetworks(nn)
-                  }
-              }
-            }
-
-        }
-
-        //CLEANUP HOST NODE
-        if (targetNode.getHost.isDefined) {
-          targetNode.getHost.get.removeHosts(targetNode)
-        }
-
-
-        //CLEANUP DICTIONARY
-        context.model.getHubs.foreach {
-          inst =>
-            inst.getDictionary.map {
-              dico =>
-                dico.getValues.filter(v => v.getTargetNode.isDefined && v.getTargetNode.get.getName == addN.nodeName).foreach {
-                  value =>
-                    dico.removeValues(value)
-                }
-            }
-        }
-        context.model.getGroups.foreach {
-          inst =>
-            inst.getDictionary.map {
-              dico =>
-                dico.getValues.filter(v => v.getTargetNode.isDefined && v.getTargetNode.get.getName == addN.nodeName).foreach {
-                  value =>
-                    dico.removeValues(value)
-                }
-            }
-        }
-
-        //DELETE NODE
-        context.model.removeNodes(targetNode)
-        true
+        removeNode(targetNode, context)
       }
       case None => {
         logger.error("Node Already existe")
