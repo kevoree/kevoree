f366db2ca35f6c66ec7f338a973982809e40e8cc
Erwan Daubert <Erwan.Daubert@gmail.com>
2013-01-15 12:08:46 +0100

    fix kevscript interpreter using findByQuery
    Former-commit-id: f01ff9fdc794d871e010487fce65303c0f2f2667

--- kevoree-tools/org.kevoree.tools.marShell/src/main/scala/org/kevoree/tools/marShell/interpreter/sub/KevsAddBindingInterpreter.scala
+++ kevoree-tools/org.kevoree.tools.marShell/src/main/scala/org/kevoree/tools/marShell/interpreter/sub/KevsAddBindingInterpreter.scala
@@ -11,49 +11,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/**
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/**
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package org.kevoree.tools.marShell.interpreter.sub
 
@@ -62,54 +19,55 @@ import org.kevoree.tools.marShell.interpreter.KevsAbstractInterpreter
 import org.kevoree.tools.marShell.interpreter.KevsInterpreterContext
 
 import org.slf4j.LoggerFactory
-import org.kevoree.{ComponentInstance, ContainerNode, KevoreeFactory}
+import org.kevoree.{Channel, ComponentInstance, ContainerNode, KevoreeFactory}
 
-case class KevsAddBindingInterpreter (addBinding: AddBindingStatment) extends KevsAbstractInterpreter {
+case class KevsAddBindingInterpreter(addBinding: AddBindingStatment) extends KevsAbstractInterpreter {
 
-	var logger = LoggerFactory.getLogger(this.getClass)
+  var logger = LoggerFactory.getLogger(this.getClass)
 
-	def interpret (context: KevsInterpreterContext): Boolean = {
-		addBinding.cid.nodeName match {
-			case Some(searchNodeName) => {
-				context.model.getNodes.find(n => n.getName == addBinding.cid.nodeName.get) match {
-					case Some(node) =>
-						node.getComponents.find(c => c.getName == addBinding.cid.componentInstanceName) match {
-							case Some(component) =>
-								(component.getProvided ++ component.getRequired).find(p => p.getPortTypeRef.getName == addBinding.portName) match {
-									case Some(port) => {
-										port.getBindings.find(mb => mb.getPort.getPortTypeRef.getName == addBinding.portName
-											&& mb.getPort.eContainer.eContainer.asInstanceOf[ContainerNode].getName == addBinding.cid.nodeName.get
-											&& mb.getPort.eContainer.asInstanceOf[ComponentInstance].getName == addBinding.cid.componentInstanceName
-											&& mb.getHub.getName == addBinding.bindingInstanceName) match {
-											case Some(binding) => {
-												logger
-													.warn("Binding {}.{}@{} => {} already exists",
-																 Array[AnyRef](addBinding.cid.componentInstanceName, addBinding.portName, addBinding.cid.nodeName.get, addBinding.bindingInstanceName))
-												true
-											}
-											case None => {
-												context.model.getHubs.find(c => c.getName == addBinding.bindingInstanceName) match {
-													case Some(channel) => {
-														val newbinding = KevoreeFactory.eINSTANCE.createMBinding
-														newbinding.setHub(channel)
-														newbinding.setPort(port)
-														context.model.addMBindings(newbinding)
-														true
-													}
-													case None => logger.error("Hub not found => " + addBinding.bindingInstanceName); false
-												}
-											}
-										}
-									}
-									case None => logger.error("Port not found => " + addBinding.portName); false
-								}
-							case None => logger.error("Component not found => " + addBinding.cid.componentInstanceName); false
-						}
-					case None => logger.error("Node not found => " + addBinding.cid.nodeName); false
-				}
-			}
-			case None => logger.error("NodeName is mandatory !"); false
-		}
-	}
+  def interpret(context: KevsInterpreterContext): Boolean = {
+    addBinding.cid.nodeName match {
+      case Some(searchNodeName) => {
+        context.model.findByQuery("nodes[" + addBinding.cid.nodeName.get + "]", classOf[ContainerNode]) match {
+          //.getNodes.find(n => n.getName == addBinding.cid.nodeName.get) match {
+          case Some(node) =>
+            node.findByQuery("components[" + addBinding.cid.componentInstanceName + "]", classOf[ComponentInstance]) match {
+              //getComponents.find(c => c.getName == addBinding.cid.componentInstanceName) match {
+              case Some(component) =>
+                context.model.findByQuery("hubs[" + addBinding.bindingInstanceName + "]", classOf[Channel]) match {
+                  case Some(channel) => {
+                    (component.getProvided ++ component.getRequired).find(p => p.getPortTypeRef.getName == addBinding.portName) match {
+                      case Some(port) => {
+                        port.getBindings.find(mb => mb.getPort.getPortTypeRef.getName == addBinding.portName
+                          && mb.getPort.eContainer.eContainer.asInstanceOf[ContainerNode].getName == addBinding.cid.nodeName.get
+                          && mb.getPort.eContainer.asInstanceOf[ComponentInstance].getName == addBinding.cid.componentInstanceName
+                          && mb.getHub.getName == addBinding.bindingInstanceName) match {
+                          case Some(binding) => {
+                            logger.warn("Binding {}.{}@{} => {} already exists",
+                              Array[AnyRef](addBinding.cid.componentInstanceName, addBinding.portName, addBinding.cid.nodeName.get, addBinding.bindingInstanceName))
+                            true
+                          }
+                          case None => {
+                            val newbinding = KevoreeFactory.eINSTANCE.createMBinding
+                            newbinding.setHub(channel)
+                            newbinding.setPort(port)
+                            context.model.addMBindings(newbinding)
+                            true
+                          }
+                        }
+                      }
+                      case None => logger.error("Port not found => " + addBinding.portName); false
+                    }
+                  }
+                  case None => logger.error("Hub not found => " + addBinding.bindingInstanceName); false
+                }
+              case None => logger.error("Component not found => " + addBinding.cid.componentInstanceName); false
+            }
+          case None => logger.error("Node not found => " + addBinding.cid.nodeName); false
+        }
+      }
+      case None => logger.error("NodeName is mandatory !"); false
+    }
+  }
 
 }
