f7b77d9fa7112f9fcdfd78b02d1a36c456f386b3
JeD <jedartois@gmail.com>
2013-01-22 16:26:15 +0100

    some cleaning

--- kevoree-tools/org.kevoree.tools.control.framework/src/main/java/org/kevoree/tools/control/framework/command/CreateRulesCommand.java
+++ kevoree-tools/org.kevoree.tools.control.framework/src/main/java/org/kevoree/tools/control/framework/command/CreateRulesCommand.java
@@ -1,3 +1,16 @@
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.kevoree.tools.control.framework.command;
 
 import org.kevoree.KControlModel.KControlModelFactory;
@@ -7,13 +20,10 @@ import org.kevoree.KControlModel.RuleMatcher;
 import org.kevoree.adaptation.control.api.ControlException;
 import org.kevoree.tools.control.framework.api.Command;
 import org.kevoree.tools.control.framework.api.IAccessControl;
-import sun.security.rsa.RSAPublicKeyImpl;
-
 import java.security.PublicKey;
 import java.security.interfaces.RSAPublicKey;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+
 
 /**
  * Created with IntelliJ IDEA.
