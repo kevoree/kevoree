d7d14e7d6510b96f89afbb220f4ceaeec2e67d09
JeD <jedartois@gmail.com>
2013-02-20 17:02:28 +0100

    update access control according to new metamodel
    Former-commit-id: 25b70c8018629296b7a95af6ccd43e921f066653

---
+++ kevoree-tools/org.kevoree.tools.accesscontrol.framework/src/main/java/org/kevoree/tools/accesscontrol/framework/api/ICompareAccessControl.java
@@ -0,0 +1,56 @@
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kevoree.tools.accesscontrol.framework.api;
+
+
+import org.kevoree.ContainerRoot;
+
+import org.kevoree.adaptation.accesscontrol.api.ControlException;
+import org.kevoree.adaptation.accesscontrol.api.SignedModel;
+import org.kevoreeAdaptation.AdaptationModel;
+import org.kevoreeAdaptation.AdaptationPrimitive;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jed
+ * Date: 21/01/13
+ * Time: 17:24
+ compares the  permissions to an access control
+ */
+public interface ICompareAccessControl
+{
+
+    /**
+     * Call Kompare
+     * @param nodeName
+     * @param model
+     * @param target
+     * @return  The AdaptationPrimitive refused if is empty the access is approval
+     */
+    public List<AdaptationPrimitive> approval(String currentNodeName, ContainerRoot model, SignedModel target_model) throws ControlException;
+
+
+    /**
+     * don't call Kompare
+     * @param adaptationModel
+     * @param target_model
+     * @return
+     * @throws ControlException
+     */
+    public List<AdaptationPrimitive> approval(AdaptationModel adaptationModel,SignedModel target_model) throws ControlException;
+
+
+}
---
+++ kevoree-tools/org.kevoree.tools.accesscontrol.framework/src/test/TestSignature.java
@@ -0,0 +1,50 @@
+/**
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import org.junit.Before;
+import org.junit.Test;
+import org.kevoree.tools.control.framework.utils.HelperSignature;
+
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jed
+ * Date: 22/01/13
+ * Time: 16:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestSignature {
+
+    KeyPair sign;
+    @Before
+    public void init() throws NoSuchAlgorithmException {
+
+        sign = HelperSignature.generateKeys(1024);
+
+    }
+    @Test
+    public void test_sign() throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+
+        byte [] signaturebytes =   HelperSignature.getSignature(sign.getPrivate(),"test".getBytes());
+        assertNotNull(signaturebytes);
+        assertTrue(HelperSignature.verifySignature(signaturebytes, sign.getPublic(), "test".getBytes()));
+    }
+}
