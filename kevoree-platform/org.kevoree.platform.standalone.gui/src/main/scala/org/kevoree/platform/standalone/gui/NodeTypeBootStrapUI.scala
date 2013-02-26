e4b50af1b196b7cac4dd6153c3d65681369b7be5
Erwan Daubert <Erwan.Daubert@gmail.com>
2013-02-26 08:30:41 +0100

    use node.name System property

--- kevoree-platform/org.kevoree.platform.standalone.gui/src/main/scala/org/kevoree/platform/standalone/gui/NodeTypeBootStrapUI.scala
+++ kevoree-platform/org.kevoree.platform.standalone.gui/src/main/scala/org/kevoree/platform/standalone/gui/NodeTypeBootStrapUI.scala
@@ -11,19 +11,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * 	http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.kevoree.platform.standalone.gui
 
 import java.awt.event.{ActionEvent, ActionListener}
@@ -101,8 +88,13 @@ class NodeTypeBootStrapUI(private var pkernel: ContainerRoot) extends JPanel {
           }
         }
     groupTypeComboBox = UIHelper.createJComboBox(groupTypeModel)
-
-    nodeInstancePanel = new InstanceParamPanel(getTypeDefinition(nodeTypeComboBox),"node0")
+    System.out.println(System.getProperty("node.name"))
+    if (System.getProperty("node.name") != null && System.getProperty("node.name") != "") {
+      System.out.println(System.getProperty("node.name") + "2")
+      nodeInstancePanel = new InstanceParamPanel(getTypeDefinition(nodeTypeComboBox),System.getProperty("node.name"))
+    } else {
+      nodeInstancePanel = new InstanceParamPanel(getTypeDefinition(nodeTypeComboBox),"node0")
+    }
     groupInstancePanel = new InstanceParamPanel(getTypeDefinition(groupTypeComboBox),"sync")
 
     val nodeTypeLabel = new JLabel("Node type", SwingConstants.TRAILING);
