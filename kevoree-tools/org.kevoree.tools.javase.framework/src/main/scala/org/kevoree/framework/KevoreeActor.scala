6fd5a321ed57e92b47a988c19199d9f9bc982010
Francois Fouquet <fouquet.f@gmail.com>
2013-03-18 19:04:31 +0100

    cleanup too
    Former-commit-id: 6afc258caf74fbfa9fe1d645e7efe6468ec8e51b

--- kevoree-tools/org.kevoree.tools.javase.framework/src/main/scala/org/kevoree/framework/KevoreeActor.scala
+++ kevoree-tools/org.kevoree.tools.javase.framework/src/main/scala/org/kevoree/framework/KevoreeActor.scala
@@ -18,7 +18,7 @@ package org.kevoree.framework
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * 	http://www.gnu.org/licenses/lgpl-3.0.txt
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -37,50 +37,60 @@ import scala.actors.TIMEOUT
 trait KevoreeActor extends Actor {
 
   //sealed case class ACTOR_ADMIN_MSG()
-  sealed case class STOP_ACTOR(force : Boolean = false) //extends ACTOR_ADMIN_MSG
-  sealed case class PAUSE_ACTOR() //extends ACTOR_ADMIN_MSG
-  sealed case class RESUME_ACTOR() //extends ACTOR_ADMIN_MSG
+  sealed case class STOP_ACTOR(force: Boolean = false)
+
+  //extends ACTOR_ADMIN_MSG
+  sealed case class PAUSE_ACTOR()
+
+  //extends ACTOR_ADMIN_MSG
+  sealed case class RESUME_ACTOR()
+
+  //extends ACTOR_ADMIN_MSG
 
   def stop = this ! STOP_ACTOR()
+
   def forceStop = this ! STOP_ACTOR(true)
+
   def resume = this ! RESUME_ACTOR()
+
   def pause = this ! PAUSE_ACTOR()
 
   protected var pauseState = false
+
   def isInPause = pauseState
 
-  protected def stopRequest(force : Boolean) : Nothing = {
-    if(force){
+  protected def stopRequest(force: Boolean): Nothing = {
+    if (force) {
       exit //Simply exit
     } else {
       emptyMailBox //Recusive call to the process fonction
     }
   }
 
-  private def emptyMailBox : Nothing = reactWithin(0) {
+  private def emptyMailBox: Nothing = reactWithin(0) {
 
-    case  RESUME_ACTOR() => println("Actor in stopping phase, ignore admin message")
-    case  STOP_ACTOR(_) => println("Actor in stopping phase, ignore admin message")
-    case  PAUSE_ACTOR() => println("Actor in stopping phase, ignore admin message")
+    case RESUME_ACTOR() => println("Actor in stopping phase, ignore admin message")
+    case STOP_ACTOR(_) => println("Actor in stopping phase, ignore admin message")
+    case PAUSE_ACTOR() => println("Actor in stopping phase, ignore admin message")
 
     case TIMEOUT => exit()
-    case _ @ msg => internal_process(msg);emptyMailBox
+    case _@msg => internal_process(msg); emptyMailBox
   }
 
-  def internal_process(msg : Any)
+  def internal_process(msg: Any)
 
   def act() {
     loop {
       react {
         case PAUSE_ACTOR() => {
-            pauseState = true
-            react {
-              case RESUME_ACTOR() => pauseState = false //NOTHING TO DO
-              case STOP_ACTOR(f) => pauseState = false ; stopRequest(f)
-            }
+          pauseState = true
+          react {
+            case RESUME_ACTOR() => pauseState = false //NOTHING TO DO
+            case STOP_ACTOR(f) => pauseState = false; stopRequest(f)
           }
+        }
         case STOP_ACTOR(f) => stopRequest(f)
-        case _ @ msg => internal_process(msg)
+        case _@msg => internal_process(msg)
       }
     }
   }
