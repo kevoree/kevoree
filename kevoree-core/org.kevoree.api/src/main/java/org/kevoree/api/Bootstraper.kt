1569e12dd982ea830d67d189046f2eb38997a6a5
Erwan Daubert <erwan.daubert@gmail.com>
2013-05-14 17:58:28 +0200

    fix return type which can be null
    Former-commit-id: 98c4d284ad3b087879244116ba5681157b365c1a

--- kevoree-core/org.kevoree.api/src/main/java/org/kevoree/api/Bootstraper.kt
+++ kevoree-core/org.kevoree.api/src/main/java/org/kevoree/api/Bootstraper.kt
@@ -13,20 +13,6 @@
  */
 package org.kevoree.api
 
-/**
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * 	http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import org.kevoree.api.service.core.handler.KevoreeModelHandlerService
 import org.kevoree.api.service.core.script.KevScriptEngineFactory
 import java.io.File
@@ -46,10 +32,10 @@ trait Bootstraper {
 
   fun getKevoreeClassLoaderHandler() : KevoreeClassLoaderHandler
   fun bootstrapNodeType(currentModel: ContainerRoot, nodeName: String, mservice: KevoreeModelHandlerService, kevsEngineFactory: KevScriptEngineFactory): org.kevoree.api.NodeType?
-  fun resolveArtifact(artId: String, groupId: String, version: String, repos: List<String>): File
-  fun resolveArtifact(artId: String, groupId: String, version: String, extension : String, repos: List<String>): File
-  fun resolveKevoreeArtifact(artId: String, groupId: String, version: String): File
-  fun resolveDeployUnit(du: DeployUnit): File
+  fun resolveArtifact(artId: String, groupId: String, version: String, repos: List<String>): File?
+  fun resolveArtifact(artId: String, groupId: String, version: String, extension : String, repos: List<String>): File?
+  fun resolveKevoreeArtifact(artId: String, groupId: String, version: String): File?
+  fun resolveDeployUnit(du: DeployUnit): File?
   fun close() : Unit
   fun clear() : Unit
   fun getKevoreeLogService() : KevoreeLogService
