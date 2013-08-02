3ca39a71f4a7d4b04fe909cccdd873517561c032
Erwan Daubert <erwan.daubert@gmail.com>
2013-08-02 17:15:44 +0200

    add project.version on marshell.mavenplugin
    Former-commit-id: 94b5e3325b5735aa56b3dd3c9cec5e380363235b

--- kevoree-tools/org.kevoree.tools.marshell.mavenplugin/src/main/scala/org/kevoree/tools/marshell/mavenplugin/ModelGenerator.scala
+++ kevoree-tools/org.kevoree.tools.marshell.mavenplugin/src/main/scala/org/kevoree/tools/marshell/mavenplugin/ModelGenerator.scala
@@ -11,45 +11,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * 	http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/**
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/**
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.kevoree.tools.marshell.mavenplugin
 
 import java.io.{BufferedReader, FileReader, File}
@@ -78,7 +39,9 @@ object ModelGenerator extends App {
       key => kevEngine.addVariable(key.toString, mavenSource.getProperties.get(key.toString).toString)
     }
 //    kevEngine.addVariable("kevoree.version", KevoreeFactory.getVersion)
+    kevEngine.addVariable("project.version", mavenSource.getVersion)
     kevEngine.addVariable("basedir",mavenSource.getBasedir.getAbsolutePath)
+    kevEngine.addVariable("project.build.directory", mavenSource.getBuild.getDirectory)
     kevEngine.append(loadScript(new File(scriptPath)))
     kevEngine.interpret()
   }
