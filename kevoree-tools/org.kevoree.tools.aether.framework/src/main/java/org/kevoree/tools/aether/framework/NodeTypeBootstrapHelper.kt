b2c8a227d732210d7cf600ab15b3a5491dc4030f
Erwan Daubert <erwan.daubert@gmail.com>
2013-08-12 17:11:29 +0200

    change repo management
    Former-commit-id: 286f28378d2e614772aa15a26d95558922ac3109

--- kevoree-tools/org.kevoree.tools.aether.framework/src/main/java/org/kevoree/tools/aether/framework/NodeTypeBootstrapHelper.kt
+++ kevoree-tools/org.kevoree.tools.aether.framework/src/main/java/org/kevoree/tools/aether/framework/NodeTypeBootstrapHelper.kt
@@ -24,7 +24,11 @@ open class NodeTypeBootstrapHelper: Bootstraper, KCLBootstrap {
 
     override fun resolveKevoreeArtifact(artId: String, groupId: String, version: String): File? {
         val l = ArrayList<String>()
-        l.add("http://oss.sonatype.org/content/groups/public/")
+        if(version.contains("SNAPSHOT") || version.contains("LATEST")){
+            l.add("http://oss.sonatype.org/content/groups/public/")
+        } else {
+            l.add("http://repo1.maven.org/maven2")
+        }
         return resolveArtifact(artId, groupId, version, l)
     }
 
