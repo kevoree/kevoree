9a04c0e7a4f2c9269b20444e62704e9fdacec3bd
Erwan Daubert <erwan.daubert@gmail.com>
2013-08-14 16:37:54 +0200

    avoid empty library name
    Former-commit-id: 889d6b8a6c9dc37f39fdee485455b3dbbd218dc1

--- kevoree-tools/org.kevoree.tools.annotation.processor/src/main/scala/org/kevoree/framework/annotation/processor/visitor/sub/LibraryProcessor.scala
+++ kevoree-tools/org.kevoree.tools.annotation.processor/src/main/scala/org/kevoree/framework/annotation/processor/visitor/sub/LibraryProcessor.scala
@@ -11,27 +11,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * 	http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package org.kevoree.framework.annotation.processor.visitor.sub
 
-import org.kevoree.KevoreeFactory
 import org.kevoree.ContainerRoot
 import org.kevoree.TypeDefinition
 import javax.lang.model.element.TypeElement
@@ -48,7 +30,15 @@ trait LibraryProcessor {
     if (classdef.getAnnotation(classOf[org.kevoree.annotation.Library]) != null) {
       val libannot = classdef.getAnnotation(classOf[org.kevoree.annotation.Library])
 
-      (List(libannot.name()) ++ libannot.names()).foreach {
+      var libraryNames = List[String]()
+      if (libannot.name() != null && !"".equals(libannot.name())) {
+        libraryNames = List[String](libannot.name())
+      }
+      if (libannot.names().size > 0) {
+        libraryNames = libraryNames ++ libannot.names()
+      }
+
+      libraryNames.foreach {
         libName =>
         /* CREATE LIBRARY IF NEEDED */
           root.getLibraries.find({
