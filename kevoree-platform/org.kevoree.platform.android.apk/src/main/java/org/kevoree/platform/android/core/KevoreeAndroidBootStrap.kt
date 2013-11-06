6183fd044a8d6d4b9fc7a43f71573024b99884e8
Erwan Daubert <erwan.daubert@gmail.com>
2013-11-06 14:14:44 +0100

    remove useless bootstrap library
    Former-commit-id: ac5bcc7afb6dd72e8c90252d8ed09693fc2753c7

--- kevoree-platform/org.kevoree.platform.android.apk/src/main/java/org/kevoree/platform/android/core/KevoreeAndroidBootStrap.kt
+++ kevoree-platform/org.kevoree.platform.android.apk/src/main/java/org/kevoree/platform/android/core/KevoreeAndroidBootStrap.kt
@@ -82,7 +82,7 @@ class KevoreeAndroidBootStrap {
             }
             dummyKCL.lockLinks()
 
-            bootstraper.registerManuallyDeployUnit("jgrapht-jdk1.5", "org.jgrapht", "0.7.3", dummyKCL)
+//            bootstraper.registerManuallyDeployUnit("jgrapht-jdk1.5", "org.jgrapht", "0.7.3", dummyKCL)
             bootstraper.registerManuallyDeployUnit("kotlin-runtime", "org.jetbrains.kotlin", "*", dummyKCL);
             bootstraper.registerManuallyDeployUnit("kotlin-stdlib", "org.jetbrains.kotlin", "*", dummyKCL);
             bootstraper.registerManuallyDeployUnit("jfilter-library", "fr.inria.jfilter", "1.3", dummyKCL);
