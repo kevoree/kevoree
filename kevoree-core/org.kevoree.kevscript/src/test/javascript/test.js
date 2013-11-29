4cf894b31262fe08424220a951af01d92700bba7
Maxime Tricoire <max.tricoire@gmail.com>
2013-11-29 12:30:01 +0100

    update grammar to new version
    Former-commit-id: 5c145a1a8db5c89a47a0f2de234ae5cef329be53

---
+++ kevoree-core/org.kevoree.kevscript/src/test/javascript/test.js
@@ -0,0 +1,11 @@
+var kevs = require('./../../../target/parsers/javascript/parser'),
+    fs   = require('fs'),
+    path = require('path');
+
+fs.readFile(path.resolve(__dirname, '..', '..', 'examples', 'test.kevs'), 'utf8', function (err, data) {
+  if (err) throw err;
+
+  var parser = new kevs.Parser();
+  var ast = parser.parse(data);
+  console.log(ast.toString());
+});
---
+++ kevoree-core/org.kevoree.kevscript/test-js.sh
@@ -0,0 +1,1 @@
+waxeye -g javascript target/parsers/javascript src/main/grammar/kevscript.waxeye && node src/test/javascript/test.js
