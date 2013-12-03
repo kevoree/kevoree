da61fd85ab55793d475429d466322ab3273fd5e9
Francois Fouquet <fouquet.f@gmail.com>
2013-12-03 08:24:51 +0100

    add dummy file for javadoc (TODO fix for kotlin)
    Former-commit-id: 2fd9fc03c90b10226dc3cd7ba7263e7c1d9487ef

---
+++ kevoree-core/org.kevoree.basechecker/src/main/java/Ignore.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: duke
+ * Date: 03/12/2013
+ * Time: 08:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class Ignore {
+}
