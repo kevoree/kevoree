a119a5ef90dead191736d2dc218635507b09556f
Maxime Tricoire <max.tricoire@gmail.com>
2014-01-06 16:45:36 +0100

    remove web editor component from tools
    Former-commit-id: 69a03a8f6d8da4579dc986af9ae579921aaab4f0

--- kevoree-tools/org.kevoree.tools.web.editor/src/main/resources/webapp/public/js/app/templates/node-link-tab.js
+++
@@ -1,6 +0,0 @@
-define(['jadeRuntime'], function(jade) {
-return function anonymous(locals) {
-var buf = [];
-var locals_ = (locals || {}),id = locals_.id,isActive = locals_.isActive,type = locals_.type;buf.push("<li" + (jade.attrs({ 'id':('node-link-root-'+id), 'data-node-link-id':(id), "class": [((isActive)?'active':null)] }, {"id":true,"class":true,"data-node-link-id":true})) + "><a" + (jade.attrs({ 'id':('node-link-tab-val-'+id), 'href':('#node-link-'+id), 'data-toggle':('tab') }, {"id":true,"href":true,"data-toggle":true})) + ">" + (jade.escape((jade.interp = type) == null ? '' : jade.interp)) + "</a></li>");;return buf.join("");
-};
-});
