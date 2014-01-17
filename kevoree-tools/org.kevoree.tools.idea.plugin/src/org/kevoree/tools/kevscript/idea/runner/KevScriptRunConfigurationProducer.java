2a2c847ed3cc26e54d388ff6b8e03dd55c567311
Grégory NAIN <gregory.nain@gmail.com>
2014-01-17 12:44:27 +0100

    runner
    Former-commit-id: cedfa9e0844ac362fea81d927f04e94f1e4b5370

--- kevoree-tools/org.kevoree.tools.idea.plugin/src/org/kevoree/tools/kevscript/idea/runner/KevScriptRunConfigurationProducer.java
+++ kevoree-tools/org.kevoree.tools.idea.plugin/src/org/kevoree/tools/kevscript/idea/runner/KevScriptRunConfigurationProducer.java
@@ -7,25 +7,30 @@ import com.intellij.execution.configurations.ConfigurationType;
 import com.intellij.openapi.util.Ref;
 import com.intellij.psi.PsiElement;
 
+import javax.management.Notification;
+
 /**
  * Created by gregory.nain on 17/01/2014.
  */
 public class KevScriptRunConfigurationProducer extends RunConfigurationProducer<KevScriptRunConfiguration> {
-    protected KevScriptRunConfigurationProducer(ConfigurationFactory configurationFactory) {
-        super(new KevScriptRunConfigurationFactory(new KevScriptRunConfigurationType()));
-    }
 
-    protected KevScriptRunConfigurationProducer(ConfigurationType configurationType) {
+
+    protected KevScriptRunConfigurationProducer() {
         super(new KevScriptRunConfigurationType());
     }
 
     @Override
     protected boolean setupConfigurationFromContext(KevScriptRunConfiguration kevScriptRunConfiguration, ConfigurationContext configurationContext, Ref<PsiElement> psiElementRef) {
+        configurationContext.getLocation();
+        if(psiElementRef.get().)
+
         return true;
     }
 
     @Override
     public boolean isConfigurationFromContext(KevScriptRunConfiguration kevScriptRunConfiguration, ConfigurationContext configurationContext) {
+
+
         return true;
     }
 }
