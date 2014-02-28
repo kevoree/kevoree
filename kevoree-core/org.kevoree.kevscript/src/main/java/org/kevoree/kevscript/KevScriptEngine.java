f65fffc02abcaa396f4e36110a52b869c58a2f60
Erwan Daubert <erwan.daubert@gmail.com>
2014-02-28 10:47:34 +0100

    remove hack which is handle by the new KMF snapshot (a release is coming)
    Former-commit-id: 68d50056be6ede2627a0434232e1f869ee3f3db4

--- kevoree-core/org.kevoree.kevscript/src/main/java/org/kevoree/kevscript/KevScriptEngine.java
+++ kevoree-core/org.kevoree.kevscript/src/main/java/org/kevoree/kevscript/KevScriptEngine.java
@@ -115,7 +115,7 @@ public class KevScriptEngine implements KevScriptService {
                 Repository repo = factory.createRepository();
                 StringBuilder builder = new StringBuilder();
                 for (IAST<Type> child : node.getChildren().get(0).getChildren()) {
-                    builder.append(child.childrenAsString().replace("\\", "\\\\"));
+                    builder.append(child.childrenAsString());
                 }
                 repo.setUrl(builder.toString());
                 model.addRepositories(repo);
@@ -178,14 +178,14 @@ public class KevScriptEngine implements KevScriptService {
                     //frag dep
                     builder = new StringBuilder();
                     for (IAST<Type> child : node.getChildren().get(2).getChildren()) {
-                        builder.append(child.childrenAsString().replace("\\", "\\\\"));
+                        builder.append(child.childrenAsString());
                     }
                     propToSet = builder.toString();
                     targetNodes = InstanceResolver.resolve(model, node.getChildren().get(1));
                 } else {
                     builder = new StringBuilder();
                     for (IAST<Type> child : node.getChildren().get(1).getChildren()) {
-                        builder.append(child.childrenAsString().replace("\\", "\\\\"));
+                        builder.append(child.childrenAsString());
                     }
                     propToSet = builder.toString();
                 }
